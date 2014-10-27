508f64e3aed1d5c5de680dc6cd9f44f7d1f872eb
Raphael Wintrich <raphi@waldorfweb.net>
2014-10-27 01:33:36 +0100

    save map state in local storage

---
+++ js/storage.js
@@ -0,0 +1,24 @@
+var storage = {
+		
+	prefix: '',
+		
+	setPrefix: function(prefix)
+	{
+		this.prefix = prefix+':';
+	},
+	set: function(key,val)
+	{
+		val = JSON.stringify({v:val});
+		localStorage.setItem(storage.prefix+key, val);
+	},
+	get: function(key)
+	{
+		val = localStorage.getItem(storage.prefix+key);
+		if(val != undefined)
+		{
+			val = JSON.parse(val);
+			return val.v;
+		}
+		return val;
+	}
+};
