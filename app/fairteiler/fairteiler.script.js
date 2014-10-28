5333b0c690b2d867e1e8ec06274f51e8f158473b
Raphael Wintrich <raphi@waldorfweb.net>
2014-10-28 15:46:10 +0100

    fairteiler css fix

--- app/fairteiler/fairteiler.script.js
+++ app/fairteiler/fairteiler.script.js
@@ -1,5 +1,5 @@
 $(function(){
-	$("#wallpost-attach-image").before('<label style="margin:0 15px 0 0;padding:0;cursor:pointer;"><img style="margin:0;padding:0;position:relative;top:10px;" src="img/facebook_share.png" alt="Auf Facebook teilen" /><input style="position:relative;top:3px;" type="checkbox" name="fbshare" id="fbshare" value="1" /></label>');
+	$("#wallpost-attach-image").before('<label id="fbshare-wrapper" style="margin:0 15px 0 0;padding:0;cursor:pointer;"><img style="margin:0;padding:0;position:relative;top:10px;" src="img/facebook_share.png" alt="Auf Facebook teilen" /><input style="position:relative;top:3px;" type="checkbox" name="fbshare" id="fbshare" value="1" /></label>');
 	$("#wall-submit").bind('mousedown',function(){
 		$("#ft-public-link").trigger('click');
 	});
