d13776ba94fb41d238c93b810e116161e1dd6edd
Raphael Wintrich <raphi@waldorfweb.net>
2014-11-12 12:11:42 +0100

    socket check script update

--- sh/fs.checksocket.sh
+++ sh/fs.checksocket.sh
@@ -7,9 +7,6 @@ then
 else
 	echo "Socket NOT running! Restarting..."
 	cd /var/www/lmr-prod/www
-	php run.php mails stop > /var/www/lmr-prod/log/fs_mails_socket.log
-	sleep 3
-	kill $(ps aux | grep '[p]hp run.php mails' | awk '{print $2}')
 	php run.php mails > /var/www/lmr-prod/log/fs_mails_socket.log
 fi
 
