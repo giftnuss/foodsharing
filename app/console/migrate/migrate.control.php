355d945e45bfd22a6699a6e3a9065772351bdd80
Matthias Larisch <mail@matthias-larisch.de>
2014-12-12 01:44:08 +0100

    fs import: organisations/company data into data field, correctly handle chats for already existing users

--- app/console/migrate/migrate.control.php
+++ app/console/migrate/migrate.control.php
@@ -90,13 +90,16 @@ class MigrateControl extends ConsoleControl
   {
     $this->model->update("UPDATE fs_foodsaver SET fs_id = NULL");
     $this->model->begin_transaction();
-    if($result = $this->fs_db->query("SELECT p.zipcode as plz, c.name as stadt, p.lat as lat, p.lng as lon, p.picture as fs_picture, u.email as email, p.firstname as name, p.lastname as nachname, p.street as anschrift, p.phone as telefon, p.mobile as handy, if(p.gender=false,1,2) as geschlecht, p.birthdate as geb_datum, u.id as fs_id, u.created as anmeldedatum, 1 as active, if (u.last_login is null, current_date(), u.last_login) as last_login, u.password as fs_password FROM users u LEFT JOIN profiles p ON u.id = p.user_id LEFT JOIN cities c ON p.city_id = c.id WHERE u.deleted = 0 AND p.deleted = 0")) {
+    if($result = $this->fs_db->query("SELECT p.zipcode as plz, c.name as stadt, p.lat as lat, p.lng as lon, p.picture as fs_picture, u.email as email, p.firstname as name, p.lastname as nachname, p.street as anschrift, p.phone as telefon, p.mobile as handy, if(p.gender=false,1,2) as geschlecht, p.birthdate as geb_datum, u.id as fs_id, u.created as anmeldedatum, 1 as active, if (u.last_login is null, current_date(), u.last_login) as last_login, u.password as fs_password, p.type as profile_type, p.organisation_name as organisation_name, p.organisation_fax as organisation_fax, p.organisation_email as organisation_email, p.organisation_vr as organisation_vr, p.organisation_logo as organisation_logo, p.company_name as company_name, p.company_fax as company_fax, p.company_email as company_email, p.company_hrg as company_hrg, p.company_logo as company_logo FROM users u LEFT JOIN profiles p ON u.id = p.user_id LEFT JOIN cities c ON p.city_id = c.id WHERE u.deleted = 0 AND p.deleted = 0 AND u.role in (2, 3, 4)")) {
       $bar = $this->progressbar($result->num_rows);
       $cur_user_cnt = 0;
       while($row = $result->fetch_object()) {
         $cur_user_cnt++;
         $bar->update($cur_user_cnt);
         $photo = $this->fetch_and_convert_image($row->fs_picture);
+        $org_photo = $this->fetch_and_convert_image($row->organisation_logo);
+        $comp_photo = $this->fetch_and_convert_image($row->company_logo);
+        $custom_data = serialize($row);
         $plz = $row->plz;
         if(strlen($plz) > 5 || strlen($plz) < 4) {
           $plz = "";
@@ -121,8 +124,14 @@ class MigrateControl extends ConsoleControl
 					$handy = $row->handy;
 
 			  $stadt = substr($row->stadt, 0, 50);
+        $optional_data = "";
+        $utype = 0;
+        if($row->profile_type == 1 || $row->profile_type == 2) {
+          $optional_data = $custom_data;
+          $utype = 1;
+        }
 
-        $this->model->insert("INSERT IGNORE INTO fs_foodsaver (plz, stadt, lat, lon, photo, email, name, nachname, anschrift, telefon, handy, geschlecht, geb_datum, fs_id, anmeldedatum, active, data, about_me_public, token, last_login, fs_password) VALUES ("
+        $this->model->insert("INSERT INTO fs_foodsaver (plz, stadt, lat, lon, photo, email, name, nachname, anschrift, telefon, handy, geschlecht, geb_datum, fs_id, anmeldedatum, active, data, about_me_public, token, last_login, fs_password, type) VALUES ("
         ."'".$this->model->safe($plz)."', "
         ."'".$this->model->safe($stadt)."', "
         ."'".$lat."', "
@@ -138,9 +147,10 @@ class MigrateControl extends ConsoleControl
         ."'".$this->model->safe($row->geb_datum)."', "
         .$row->fs_id.", "
         ."'".$this->model->safe($row->anmeldedatum)."', "
-        ."1, '$this->source_table import', '', '', "
+        ."1, '$this->source_table import ".$this->model->safe($optional_data)."', '', '', "
         ."'".$this->model->safe($row->last_login)."', "
-        ."'".$this->model->safe($row->fs_password)."')");
+        ."'".$this->model->safe($row->fs_password)."', "
+        .$utype.") ON DUPLICATE KEY UPDATE fs_id=".$row->fs_id);
       }
     }
     $this->model->commit();
