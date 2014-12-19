d5d9f922d2fea58ca6dc16026ac389a6f257b811
Raphael Wintrich <raphi@waldorfweb.net>
2014-12-19 19:27:53 +0100

    quiz text start buttons2

--- app/settings/settings.view.php
+++ app/settings/settings.view.php
@@ -787,8 +787,8 @@ class SettingsView extends View
 		
 		if($quiz['id'] == 1)
 		{
-			$out .= '<p><a onclick="ajreq(\'startquiz\',{app:\'quiz\',qid:'.(int)$quiz['id'].'});" href="#" class="button button-big">Quiz jetzt mit Zeitlimit starten</a></p>';
-			$out .= '<p><a onclick="ajreq(\'startquiz\',{app:\'quiz\',easymode:1,qid:'.(int)$quiz['id'].'});" href="#" class="button button-big">Quiz ohne Zeitlimit starten</a></p>';
+			$out .= '<p><a onclick="ajreq(\'startquiz\',{app:\'quiz\',qid:'.(int)$quiz['id'].'});" href="#" class="button button-big">Quiz mit Zeitlimit & 10 Fragen starten</a></p>';
+			$out .= '<p><a onclick="ajreq(\'startquiz\',{app:\'quiz\',easymode:1,qid:'.(int)$quiz['id'].'});" href="#" class="button button-big">Quiz ohne Zeitlimit & 20 starten</a></p>';
 		}
 		else
 		{
