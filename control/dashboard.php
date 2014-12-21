6d32df364b69e5e5033c81f6165e9a2e46919847
Raphael Wintrich <raphi@waldorfweb.net>
2014-12-21 17:48:24 +0100

    fix infomessage for all roles

--- control/dashboard.php
+++ control/dashboard.php
@@ -1,6 +1,80 @@
 <?php
 
 //debug($_SESSION);
+
+if(S::may())
+{
+	$db =loadModel('content');
+	$check = false;
+	
+	$is_bieb = S::may('bieb');
+	$is_bot = S::may('bot');
+	$is_fs = S::may('fs');
+	
+	if(isset($_SESSION['client']['betriebe']) && is_array($_SESSION['client']['betriebe']) && count($_SESSION['client']['betriebe']) > 0)
+	{
+		$is_fs = true;
+	}
+	
+	if(isset($_SESSION['client']['verantwortlich']) && is_array($_SESSION['client']['verantwortlich']) && count($_SESSION['client']['verantwortlich']) > 0)
+	{
+		$is_bieb = true;
+	}
+	
+	if(isset($_SESSION['client']['botschafter']) && is_array($_SESSION['client']['botschafter']) && count($_SESSION['client']['botschafter']) > 0)
+	{
+		$is_bieb = true;
+	}
+	
+	if(
+			(
+					$is_fs
+					&&
+					(int)$db->qOne('SELECT COUNT(id) FROM fs_quiz_session WHERE quiz_id = 1 AND status = 1 AND foodsaver_id = '.(int)fsId()) == 0
+			)
+			||
+			(
+					$is_bieb
+					&&
+					(int)$db->qOne('SELECT COUNT(id) FROM fs_quiz_session WHERE quiz_id = 2 AND status = 1 AND foodsaver_id = '.(int)fsId()) == 0
+			)
+			||
+			(
+					$is_bot
+					&&
+					(int)$db->qOne('SELECT COUNT(id) FROM fs_quiz_session WHERE quiz_id = 3 AND status = 1 AND foodsaver_id = '.(int)fsId()) == 0
+			)
+	)
+	{
+		$check = true;
+	}
+	
+	if($check)
+	{
+		$cnt = $db->getContent(33);
+			
+		$cnt['body'] = str_replace(array(
+				'{NAME}',
+				'{ANREDE}'
+		),array(
+				S::user('name'),
+				s('anrede_'.S::user('gender'))
+		),$cnt['body']);
+			
+		if(S::option('quiz-infobox-seen'))
+		{
+			$cnt['body'] = '<div>' . substr(strip_tags($cnt['body']),0,120) . ' ...<a href="#" onclick="$(this).parent().hide().next().show();return false;">weiterlesen</a></div><div style="display:none;">'.$cnt['body'].'</div>';
+		}
+		else
+		{
+			$cnt['body'] = $cnt['body'].'<p><a href="#"onclick="$(this).parent().parent().hide();ajax.req(\'quiz\',\'hideinfo\');return false;"><i class="fa fa-check-square-o"></i> Hinweis gelesen und nicht mehr anzeigen</a></p>';
+		}
+			
+			
+		addContent(v_info($cnt['body'],$cnt['title']));
+	}
+}
+
 if(!may())
 {
 	getContent('login');
