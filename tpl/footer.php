88d391620eed6f8d35e7b783a4a67f2d8351c72a
Matthias Larisch <mail@matthias-larisch.de>
2015-02-28 22:38:50 +0100

    myfoodsharing.at logos

--- tpl/footer.php
+++ tpl/footer.php
@@ -1,7 +1,7 @@
 <footer>
 	<div class="wrapper">
 		<div class="pure-g">
-		    <div class="pure-u-1 pure-u-md-4-24">
+		    <div class="pure-u-1 pure-u-md-6-24"> 
 		    	<div class="inside">
 		    		<ul class="linklist">
 						<li><a class="corner-all" href="/team">Team</a></li>
@@ -11,7 +11,7 @@
 		    		</ul>
 		   		</div> 
 		    </div>
-		    <div class="pure-u-1 pure-u-md-4-24">
+		    <div class="pure-u-1 pure-u-md-6-24"> 
 		    	<div class="inside">
 		    		<ul class="linklist">
 			    		<li><a class="corner-all" href="/partner">Partner</a></li>
@@ -21,31 +21,19 @@
 					</ul>
 				</div>
 		    </div>
-		    <div class="pure-u-1 pure-u-md-16-24">
+		    <div class="pure-u-1 pure-u-md-12-24">
 		    	<div class="inside">
-					<div class="pure-g">
 <?php
 		if(strpos($_SERVER['REQUEST_URI'], 'myfoodsharing.at')) {
 ?>
-						<div class="pure-u-1 pure-u-md-1-3">
 							<a class="corner-all imglink" href="http://www.lebensministerium.at/" target="_blank"><img src="/img/wien-bmlfuw.png" alt="Lebensministerium" /></a>
-						</div>
-						<div class="pure-u-1 pure-u-md-1-3">
 							<a class="corner-all imglink" href="https://www.wienertafel.at/" target="_blank"><img src="/img/wien_tafel.png" alt="Wiener Tafel" /></a>
-						</div>
-						<div class="pure-u-1 pure-u-md-1-3">
-<?php
-			} else {
-?>
-						<div class="pure-u-1">
 <?php
 			}
 ?>
 
 							<h6>Vielen Dank an unseren Öko-Hoster</h6>
 							<a class="corner-all imglink" href="https://www.manitu.de/" target="_blank"><img src="/img/manitu_logo.png" alt="Manitu" /></a>
-						</div>
-					</div>
 		    	</div>
 		    </div>
 		</div>
