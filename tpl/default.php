b56b69240df099ac3fac970265dec87074202bfa
Matthias Larisch <mail@matthias-larisch.de>
2015-03-01 16:00:13 +0100

    invert foodsharing header colors

--- tpl/default.php
+++ tpl/default.php
@@ -83,7 +83,7 @@ if(!empty($content_left) && !empty($content_right))
 		<div class="inner">
 			<div class="pure-g">
 				<div class="pure-u-1">
-					<div id="layout_logo"><a href="<?php echo $logolink; ?>" title="foodsharing home"><span>food</span>sharing</a></div>
+					<div id="layout_logo"><a href="<?php echo $logolink; ?>" title="foodsharing home">food<span>sharing</span></a></div>
 					<?php echo $msgbar; ?>
 					<?php echo $menu['mobile']; ?>
 					<div class="menu">
