1f66527ae52165751ca080e66c48e431beaa934e
Matthias Larisch <mail@matthias-larisch.de>
2015-03-09 23:38:27 +0100

    Register routing and language

---
+++ lang/DE/register.lang.php
@@ -0,0 +1,29 @@
+<?
+global $g_lang;
+
+$g_lang['signup'] = 'Anmelden!';
+$g_lang['geb_datum'] = 'Geburtsdatum';
+$g_lang['email'] = 'Email-Adresse (für weitere Informationen vor dem Treffen)';
+$g_lang['phone'] = 'Telefonnummer (für kurzfristige Rückfragen)';
+$g_lang['address'] = 'Anschrift';
+$g_lang['ort'] = 'Wohnort';
+$g_lang['take_part'] = 'Ich nehme an folgenden Tagen verbindlich teil';
+$g_lang['sleep_at'] = 'Ich übernachte in folgenden Nächten im FEZ';
+$g_lang['sleep_slots'] = 'Ich kann in Berlin Schlafplätze zur Verfügung stellen';
+$g_lang['sleep_need'] = 'Ich möchte gerne vor Ort schlafen, sofern noch Plätze frei sind';
+$g_lang['languages'] = 'Ich spreche folgende Sprachen';
+$g_lang['other_languages'] = 'Andere Sprachen';
+$g_lang['nutrition'] = 'Ich esse';
+$g_lang['vegan'] = 'Vegan';
+$g_lang['vegetarian'] = 'Vegetarisch';
+$g_lang['everything'] = 'Alles';
+$g_lang['other_nutrition'] = 'Besonderheite Essgewohnheiten';
+$g_lang['translation_necessary'] = 'Ich benötige Übersetzungshilfe';
+$g_lang['already_foodsaver'] = 'Ich bin';
+$g_lang['interested'] = 'InteressentIn';
+$g_lang['foodsharer'] = 'FoodsharerIn';
+$g_lang['foodsaver'] = 'FoodsaverIn';
+$g_lang['bieb'] = 'BetriebsverantwortlicheR';
+$g_lang['bot'] = 'BotschafterIn';
+$g_lang['childcare'] = 'Ich benötige Kinderbetreung für meine Kinder (Name + Alter)';
+$g_lang['comments'] = 'Sonstige Anmerkungen, Wünsche oder Fragen';
