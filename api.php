a5e80aac8a3284b6c7c1a9532f5a5481ea8374f3
Matthias Larisch <mail@matthias-larisch.de>
2015-11-30 14:30:37 +0100

    Refactor calendar, generate event calendar
    Options s for store and e for events

--- api.php
+++ api.php
@@ -18,6 +18,21 @@ function check_api_token($fs, $key)
 	return ($res == 1);
 }
 
+function generate_calendar_event($utc_begin, $utc_end, $utc_change, $uid, $location, $description, $summary, $uri)
+{
+	$out = "BEGIN:VEVENT\r\nDTEND:";
+	$out .= dateToCal($dateend)."\r\nUID:";
+	$out .= $uid."\r\nDTSTAMP:";
+	$out .= dateToCal($utc_change)."\r\nLOCATION:";
+	$out .= escapeString($address)."\r\nDESCRIPTION:";
+	$out .= escapeString($description)."\r\nURL;VALUE=URI:";
+	$out .= escapeString($uri)."\r\nSUMMARY:";
+	$out .= escapeString($summary)."\r\nDTSTART:";
+	$out .= dateToCal($datestart)."\r\nEND:VEVENT\r\n";
+	return $out;
+}
+
+
 function api_generate_calendar($fs, $options)
 {
 	global $db;
@@ -33,33 +48,85 @@ function api_generate_calendar($fs, $options)
 	function escapeString($string) {
 		return preg_replace('/([\,;])/','\\\$1', $string);
 	}
-	$fetches = $db->q('SELECT b.id, b.name, b.str, b.hsnr, b.plz, b.stadt, a.confirmed, UNIX_TIMESTAMP(a.`date`) AS date_ts FROM '.PREFIX.'abholer a INNER JOIN '.PREFIX.'betrieb b ON a.betrieb_id = b.id WHERE a.foodsaver_id = '.(int)$fs.' AND a.`date` > NOW() - INTERVAL 1 DAY');
-
 	echo "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//Foodsharing.de//NONSGML v1.0//EN\r\nCALSCALE:GREGORIAN\r\n";
-	foreach($fetches as $f)
+	if(strpos($options, 's') !== FALSE)
 	{
-		$datestart = dateToCal($f['date_ts']);
-		$dateend = dateToCal($f['date_ts'] + 30 * 60);
-		$uid = $f['id'].$f['date_ts'].'@foodsharing.de';
-		$address = $f['str'].' '.$f['hsnr'].', '.$f['plz'].' '.$f['stadt'];
-		$summary = $f['name'].' Abholung';
-		if(!$f['confirmed'])
+		$fetches = $db->q('SELECT b.id, b.name, b.str, b.hsnr, b.plz, b.stadt, a.confirmed, UNIX_TIMESTAMP(a.`date`) AS date_ts FROM '.PREFIX.'abholer a INNER JOIN '.PREFIX.'betrieb b ON a.betrieb_id = b.id WHERE a.foodsaver_id = '.(int)$fs.' AND a.`date` > NOW() - INTERVAL 1 DAY');
+
+		foreach($fetches as $f)
 		{
-			$summary .= ' (unbestätigt)';
+			$datestart = $f['date_ts'];
+			$dateend = $f['date_ts'] + 30 * 60;
+			$uid = $f['id'].$f['date_ts'].'@fetch.foodsharing.de';
+			$address = $f['str'].' '.$f['hsnr'].', '.$f['plz'].' '.$f['stadt'];
+			$summary = $f['name'].' Abholung';
+			if(!$f['confirmed'])
+			{
+				$summary .= ' (unbestätigt)';
+			}
+			$description = 'Foodsharing Abholung bei '.$f['name'];
+			$uri = BASE_URL.'/?page=fsbetrieb&id='.$f['id'];
+		// 3. Echo out the ics file's contents
+			echo generate_calendar_event($datestart, $dateend, time(), $uid, $address, $description, $summary, $uri);
 		}
-		$description = 'Foodsharing Abholung bei '.$f['name'];
-		$uri = BASE_URL.'/?page=fsbetrieb&id='.$f['id'];
-	// 3. Echo out the ics file's contents
-		echo "BEGIN:VEVENT\r\nDTEND:";
-		echo $dateend."\r\nUID:";
-		echo $uid."\r\nDTSTAMP:";
-		echo dateToCal(time())."\r\nLOCATION:";
-		echo escapeString($address)."\r\nDESCRIPTION:";
-		echo escapeString($description)."\r\nURL;VALUE=URI:";
-		echo escapeString($uri)."\r\nSUMMARY:";
-		echo escapeString($summary)."\r\nDTSTART:";
-		echo $datestart."\r\nEND:VEVENT\r\n";
 	}
+
+	if(strpos($options, 'e') !== FALSE)
+	{
+		$calendar = $db->q('
+				SELECT
+					e.id,
+					e.name,
+					e.`description`,
+					UNIX_TIMESTAMP(e.`start`) AS start_ts,
+					UNIX_TIMESTAMP(e.`end`) AS end_ts,
+					e.online,
+					fe.`status`,
+					loc.name as loc_name,
+					loc.street,
+					loc.zip,
+					loc.city
+				FROM
+					`'.PREFIX.'event` e
+				INNER JOIN
+					`'.PREFIX.'foodsaver_has_event` fe
+				ON
+					e.id = fe.event_id AND fe.foodsaver_id = '.(int)fsId().'
+				LEFT JOIN
+					`'.PREFIX.'location` loc
+				ON
+					loc.id = e.location_id
+				WHERE
+					e.start  > NOW() - INTERVAL 1 DAY
+				AND
+					((e.public = 1 AND (fe.`status` IS NULL OR fe.`status` <> 3))
+					OR
+						fe.`status` IN(1,2)
+					)');
+		foreach($calendar as $c)
+		{
+			$datestart = $c['start_ts'];
+			$dateend = $c['end_ts'];
+			$uid = $c['id'].$c['start_ts'].'@event.foodsharing.de';
+			if($c['online'])
+			{
+				$address = "Online, mumble.lebensmittelretten.de";
+			} else
+			{
+				$address = $c['loc_name'].', '.$c['street'].' '.$c['zip'].', '.$c['city'];
+			}
+			$summary = $c['name'].' Event';
+			if(!$c['status'] == 1)
+			{
+				$summary = '('.$summary.')';
+			}
+			$description = 'Foodsharing Event: '.$c['description'];
+			$uri = BASE_URL.'/?page=event&id='.$c['id'];
+		// 3. Echo out the ics file's contents
+			echo generate_calendar_event($datestart, $dateend, time(), $uid, $address, $description, $summary, $uri);
+		}
+	}
+	
 	echo "END:VCALENDAR\r\n";
 }
 
