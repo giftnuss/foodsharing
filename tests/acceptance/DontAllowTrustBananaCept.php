5ed469cb6bf809a769d15b42c6189664dfa2e691
Michael Knoth <michael-knoth@gmx.de>
2016-08-01 21:08:32 +0200

    create testcase for issue 9 test-case throws currently a error local

---
+++ tests/acceptance/DontAllowTrustBananaCept.php
@@ -0,0 +1,23 @@
+<?php
+$I = new AcceptanceTester($scenario);
+
+// http://codeception.com/docs/modules/WebDriver
+
+
+$I->wantTo('Check if people can give self banana');
+$I->amOnPage('/');
+$I->fillField('email_adress', 'userB@example.com');
+$I->fillField('password', 'userb');
+$I->click('#loginbar input[type=submit]');
+$I->amOnPage('/?page=dashboard');
+$I->see('Hallo');
+$I->amOnPage('/profile/119684');
+$I->see('Status updates von User');
+$I->click('#bananas');
+$I->dontSee('Schenke User eine Banane');
+
+
+// disable popups, as they are not supported in PhantomJS
+// if they were could use seeInPopup/acceptPopup
+$I->executeJS("window.confirm = function(){return true;};");
+$I->executeJS("window.alert = function(){return true;};");
