2b28ab6b982231f67a89bf88779cd10bfa20fdb5
Michael Knoth <michael-knoth@gmx.de>
2016-10-09 17:12:15 +0200

    fix bezirk text
    fix bezirk text

--- lang/DE/bezirk.lang.php
+++ lang/DE/bezirk.lang.php
@@ -4,9 +4,9 @@ global $g_lang;
 $g_lang['title'] = 'Titel';
 $g_lang['body'] = 'Nachricht';
 $g_lang['forum'] = 'Forum';
-$g_lang['bot_forum'] = 'Botschafter Forum';
+$g_lang['bot_forum'] = 'Botschafterforum';
 $g_lang['compose_new_theme'] = 'Neues Thema verfassen';
-$g_lang['empty_forum'] = 'Noch keine Themen geposted';
+$g_lang['empty_forum'] = 'Noch keine Themen gepostet';
 $g_lang['delete_post'] = 'Beitrag löschen';
 $g_lang['delete_sure'] = 'Soll der Beitrag wirklich gelöscht werden?';
 $g_lang['delete_sure_title'] = 'Wirklich löschen?';
@@ -21,7 +21,7 @@ $g_lang['no_fairteiler_available'] = 'Noch keine Fair-Teiler eingetragen';
 
 $g_lang['older_themes'] = 'ältere Themen';
 $g_lang['no_events_posted'] = 'Noch keine Events vorhanden'; 
-$g_lang['new_event'] = 'Neuen Termin/Veranstaltung eintragen';
+$g_lang['new_event'] = 'Neue/n Termin/Veranstaltung eintragen';
 $g_lang['description'] = 'Beschreibung';
 $g_lang['desc_desc'] = 'Was ist das für ein Event?';
 $g_lang['location_name'] = 'Veranstaltungsort';
@@ -29,5 +29,5 @@ $g_lang['online_type'] = 'Findet das Event offline oder online auf unserem Mumbl
 $g_lang['offline'] = 'Ganz normal im echten Leben';
 $g_lang['online'] = 'Multilokal auf mumble.lebensmittelretten.de';
 $g_lang['mumble_room'] = 'In welchem mumble Konferenzraum treffen wir uns?';
-$g_lang['dateend'] = 'End-Datum';
+$g_lang['dateend'] = 'Enddatum';
 $g_lang['post_could_not_saved'] = 'Post konnte nicht gespeichert werden.';
