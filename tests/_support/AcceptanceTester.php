2e71c518ad3d840f16b1eb74eda207cfafe3c25c
Matthias Larisch <mail@matthias-larisch.de>
2016-10-11 16:10:55 +0200

    Add helper to wait for page body load

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -23,4 +23,8 @@ class AcceptanceTester extends \Codeception\Actor
    /**
     * Define custom actions here
     */
+	public function waitForPageBody()
+	{
+		return $this->waitForElement(['css' => 'body']);
+	}
 }
