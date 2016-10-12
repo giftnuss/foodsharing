51250b892ab3a8e11ac8479449f310954233afc4
Matthias Larisch <mail@matthias-larisch.de>
2016-10-12 22:05:12 +0200

    Remove unused ,useful information on landing page
    This should be looked in if someone is interested - makes nice content
    on the landing page: Foodbaskets, fairteiler, blog posts!

--- app/index/index.control.php
+++ app/index/index.control.php
@@ -46,47 +46,6 @@ class IndexControl extends Control
 
 		),CNT_OVERTOP);
 		
-		//$this->setContentWidth(9, 9);
-		
-		$articles = array();
-		
-		/*
-		if(!S::may())
-		{
-			$articles[] = $this->view->joinIndex();
-		}*/
-		/*
-		if($news = $this->view->newsSlider($this->model->latestNews()))
-		{
-			$articles = array_merge($articles,$news);
-		}
-		addContent($this->view->printSlider($articles),CNT_OVERTOP);
-		
-		$ftcount = 5;
-		if(!S::may())
-		{
-			addContent($this->view->login('/?page=dashboard'),CNT_LEFT);
-			$ftcount = 2;
-		}
-		
-		/*
-		 * display some some nice fairteiler posts with images if user locationis not defined
-		*/
-		/*
-		if($posts = $this->model->getNewestFairteilerPosts($ftcount))
-		{
-			addContent($this->view->fairteiler($posts),CNT_LEFT);
-		}
-		
-		/*
-		 * display some newest foodbaskets if user location is not defined
-		 */
-		/*
-		if($baskets = $this->model->getNewestFoodbaskets(5))
-		{
-			addContent($this->view->baskets($baskets));
-		}
-		*/
 		//addContent('Hallo Foodsharing-Welt');
 	}
 }
