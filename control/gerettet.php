c0661f33fc3d438f0f60c10d6aedbfa39349a487
Matthias Larisch <mail@matthias-larisch.de>
2016-10-13 19:00:00 +0200

    Remove second counter for amount of saved food

--- control/gerettet.php
+++
@@ -1,9 +0,0 @@
-<?php
-$gerettet = $db->getAllGerettet();
-addBread('Hochrechnung');
-
-addContent(v_field('<div class="ui-padding">
-		<p>Die Lebensmittelretter haben insgesammt ca. <strong>'.number_format($gerettet,2,",",".").' kg</strong> Lebensmittel gerettet</p>'.v_info('Die berechnung erfolgt nur bei Betrieben, die bereits Regelmäßig spenden, d.h. Unregelmäßige / monatliche Abholungen etc. sind nicht mir eingerechnet').'',
-		'Hochrechnung lebensmittelretten.de
-		
-		</div>'));
