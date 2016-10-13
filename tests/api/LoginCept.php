cf1a4372a6b6fd04d540bee8c22ff869cee5df7d
Nick Sellen <git@nicksellen.co.uk>
2016-10-13 13:12:43 +0200

    Create user for login api test

--- tests/api/LoginCept.php
+++ tests/api/LoginCept.php
@@ -3,11 +3,14 @@ $I = new ApiTester($scenario);
 
 $I->wantTo('login');
 
+$pass = sq('pass');
+$foodsaver = $I->createFoodsaver($pass);
+
 $I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 $I->sendPOST('/?page=login', [
-	'email_adress' => 'usera@example.com',
-	'password' => 'usera'
+	'email_adress' => $foodsaver['email'],
+	'password' => $pass
 ]);
 
 $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
