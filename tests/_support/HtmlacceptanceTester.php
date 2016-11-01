a78bfae8c8e61130100a794d149b42dc00e0daab
Matthias Larisch <mail@matthias-larisch.de>
2016-11-01 13:19:31 +0100

    Add HtmlacceptanceTester
    A new acceptance tester that runs in PHPBrowser. This combines fast API
    tests with HTML Parsing of PHP Browser to be able to use selectors etc.

---
+++ tests/_support/HtmlacceptanceTester.php
@@ -0,0 +1,43 @@
+<?php
+
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class HtmlacceptanceTester extends \Codeception\Actor
+{
+    use _generated\HtmlacceptanceTesterActions;
+
+   /**
+    * Define custom actions here
+    */
+	/**
+	 * Wait to see the body element
+	 */
+	public function waitForPageBody()
+	{
+		/* This is not needed and not even compatible with PHP Browser but should
+		be available so tests can be switched to another backend anytime */
+	}
+
+	public function login($email, $password)
+	{
+		$I = $this;
+		$I->amOnPage('/');
+		$I->fillField('email_adress', $email);
+		$I->fillField('password', $password);
+		$I->click('#loginbar input[type=submit]');
+	}
+}
