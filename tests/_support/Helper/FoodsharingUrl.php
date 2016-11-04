505510d23714b3e07b785038c8ea8a05d5655b05
Matthias Larisch <mail@matthias-larisch.de>
2016-11-04 21:35:33 +0100

    Add forumThemeUrl and forumUrl helpers

--- tests/_support/Helper/FoodsharingUrl.php
+++ tests/_support/Helper/FoodsharingUrl.php
@@ -4,11 +4,25 @@ namespace Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
-class FoodsharingUrl extends \Codeception\Module
+class FoodsharingUrl extends \Codeception\Module\Db
 {
 	public function StoreUrl($id)
 	{
 		return '/?page=fsbetrieb&id='.(int)$id;
 	}
 
+	public function forumThemeUrl($id, $bezirk_id = null)
+	{
+		if(!isset($bezirk_id))
+		{
+			$bezirk_id = $this->grabFromDatabase('fs_bezirk_has_theme', 'bezirk_id', ['theme_id' => $id]);
+		}
+		return '/?page=bezirk&bid='.(int)$bezirk_id.'&sub=forum&tid='.(int)$id;
+	}
+
+	public function forumUrl($id, $botforum = false)
+	{
+		$sub = $botforum ? 'botforum' : 'forum';
+		return '/?page=bezirk&bid='.(int)$id.'&sub='.$sub;
+	}
 }
