b38681f99190ac82e518ec954fd9184c047a749c
Matthias Larisch <mail@matthias-larisch.de>
2017-01-02 22:08:00 +0100

    Save failed test output also for HTMLAcceptance
    HTMLAcceptance tests use PHP Browser instead of Webdriver. Replicate the
    behaviour of saving failed tests HTML output (by lending the code from
    WebDriver)

--- tests/_support/Helper/HtmlAcceptance.php
+++ tests/_support/Helper/HtmlAcceptance.php
@@ -1,10 +1,20 @@
 <?php
 namespace Helper;
 
+use Codeception\TestInterface;
+use Codeception\Test\Descriptor;
+
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
 class HtmlAcceptance extends \Codeception\Module
 {
+    public function _failed(TestInterface $test, $fail)
+    {
+        $filename = preg_replace('~\W~', '.', Descriptor::getTestSignature($test));
+        $outputDir = codecept_output_dir();
+        $this->getModule('PhpBrowser')->_savePageSource($outputDir . mb_strcut($filename, 0, 244, 'utf-8') . '.fail.html');
+        $this->debug("page source was saved into '$outputDir' dir");
+    }
 
 }
