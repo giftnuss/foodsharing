9bd86fb66bf9a97f4cdac0f2c5240461660e89cb
Peter Tönnies <peter.toennies@gmail.com>
2017-01-08 02:36:37 +0100

    separate z.Z. to z. Z.

--- lang/DE/de.php
+++ lang/DE/de.php
@@ -184,7 +184,7 @@ $g_lang['bezirk_request_successfull'] = 'Die Anfrage wurde erfolgreich versendet
 $g_lang['position_search'] = 'Positions Suche';
 $g_lang['ort'] = 'Stadt/Gemeinde';
 
-$g_lang['team_status_0'] = 'Dieses Team ist voll, daher werden z.Z. keine weiteren Helfer gesucht.';
+$g_lang['team_status_0'] = 'Dieses Team ist voll. Daher werden z. Z. keine weiteren Helfer gesucht.';
 $g_lang['team_status_1'] = '<strong>Es werden noch Helfer gesucht</strong>';
 $g_lang['team_status_2'] = '<strong>Es werden dringend noch Helfer gesucht</strong>';
 $g_lang['attach_image'] = 'Bild anhängen';
