5cc1190661d059ef3515a92ac2174185d6485460
Peter Tönnies <peter.toennies@gmail.com>
2017-01-08 01:14:58 +0100

    fixed typos such as z.B. u.a. d.h.

--- lang/DE/email.lang.php
+++ lang/DE/email.lang.php
@@ -3,7 +3,7 @@ global $g_lang;
 $g_lang['send_as_pm'] = 'Als interne Nachricht versenden';
 $g_lang['send_as_email'] = 'Als E-Mail versenden';
 $g_lang['mailing_list'] = 'E-Mail Verteiler';
-$g_lang['personal_styling_desc'] = 'Um die Mail pers&ouml;nlicher zu gestalten kannst Du <strong>{NAME}</strong> anstelle des Namens einf&uuml;gen und <strong>{ANREDE}</strong> f&uuml;r die Anrede.<p>{ANREDE} {NAME} ergibt z.B. Lieber Peter';
+$g_lang['personal_styling_desc'] = 'Um die Mail pers&ouml;nlicher zu gestalten kannst Du <strong>{NAME}</strong> anstelle des Namens einf&uuml;gen und <strong>{ANREDE}</strong> f&uuml;r die Anrede.<p>{ANREDE} {NAME} ergibt z. B. Lieber Peter';
 $g_lang['personal_styling'] = 'Persönliche Anrede';
 
 $g_lang['mode'] = 'Wie soll die Nachricht versendet werden?';
