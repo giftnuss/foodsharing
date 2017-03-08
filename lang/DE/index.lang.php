787af5163f35f3c5eaf06331f29be60ece13f040
Matthias Larisch <mail@matthias-larisch.de>
2017-03-08 18:17:00 +0100

    Revert "fixed commata and spelling in "Öffentlichkeitsarbeit - Festivals""
    This reverts commit 3c3dd5269e4e7cd10269d0f678f5f4c3502569c0.

--- lang/DE/index.lang.php
+++ lang/DE/index.lang.php
@@ -2,7 +2,7 @@
 global $g_lang;
 
 $g_lang['new_foodbaskets'] = 'Neuste Essenskörbe';
-$g_lang['more_fairteiler'] = 'Alle Fair-Teiler auf der Karte';
+$g_lang['more_fairteiler'] = 'Alle Fairteiler auf der Karte';
 $g_lang['more_baskets'] = 'Alle Essenskörbe auf der Karte';
 $g_lang['new_fairteiler'] = 'Fair-Teiler Updates';
-$g_lang['fairteiler'] = 'Neuste Fair-Teiler Posts';
+$g_lang['fairteiler'] = 'Neuste Fairteiler Posts';
