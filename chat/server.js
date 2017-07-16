caada40ff2d19eef65384c71932e675e05c57407
Jannis R <mail@jannisr.de>
2017-07-16 02:15:01 +0200

    expose HTTP servers
    to allow programmatic stopping

--- chat/server.js
+++ chat/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var cookie = require('cookie');
 var input_port = 1338;
-var client_port = 1337;
+var chat_port = 1337;
 var connected_clients = {};
 var listenHost = process.argv[2] || '127.0.0.1';
 var num_registrations = 0;
@@ -18,7 +18,7 @@ sendtoclient = function(client,a,m,o){
 	}
 }
 
-var app = http.createServer(function  (req, res) {
+var inputServer = http.createServer(function  (req, res) {
 	if(req.url == "/stats") {
 		res.writeHead(200);
 		res.end(JSON.stringify({
@@ -35,21 +35,21 @@ var app = http.createServer(function  (req, res) {
 	app = 		query.a;
 	method = 	query.m;
 	options = 	query.o;
-	
+
 	if(client) {
 		sendtoclient(client,app,method,options);
 	}
 	res.writeHead(200);
 	res.end("\n");
 });
-app.listen(input_port, listenHost);
+inputServer.listen(input_port, listenHost);
 console.log("http server started on", listenHost + ':' + input_port);
 
-var app2 = http.createServer(function  (req, res) {
+var chatServer = http.createServer(function  (req, res) {
 	res.writeHead(200);
 	res.end("\n");
 });
-var io = require('socket.io')(app2);
+var io = require('socket.io')(chatServer);
 io.use(function(socket, next){
 	var cookieVal = socket.request.headers.cookie;
 	if(cookieVal) {
@@ -59,8 +59,8 @@ io.use(function(socket, next){
 	next(new Error('not authorized'));
 });
 
-app2.listen(client_port, listenHost);
-console.log("socket.io started on port", listenHost + ':' + client_port);
+chatServer.listen(chat_port, listenHost);
+console.log("socket.io started on port", listenHost + ':' + chat_port);
 
 io.on('connection', function (socket) {
 	var sid = socket.sid;
@@ -86,3 +86,8 @@ io.on('connection', function (socket) {
 		}
 	});
 });
+
+module.exports = {
+	inputServer: inputServer,
+	chatServer: chatServer
+}
