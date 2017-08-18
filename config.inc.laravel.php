6a0b7602e2e98ca5e07c26edcc967bcf28d052e4
Raphael Wintrich <raphael.wintrich@gmail.com>
2017-08-18 19:28:19 +0200

    use laravels db config env vars

--- config.inc.laravel.php
+++ config.inc.laravel.php
@@ -3,18 +3,22 @@
 $protocol = 'http';
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')
 {
-	$protocol = 'https';
+    $protocol = 'https';
+}
+$http_host = 'localhost';
+if(isset($_SERVER['HTTP_HOST'])) {
+    $http_host = $_SERVER['HTTP_HOST'];
 }
 
 define('PROTOCOL',$protocol);
-define('DB_HOST','db');
-define('DB_USER','root');
-define('DB_PASS','root');
-define('DB_DB','foodsharing');
+define('DB_HOST',  env('DB_HOST', '127.0.0.1'));
+define('DB_USER',env('DB_USERNAME', 'forge'));
+define('DB_PASS',env('DB_PASSWORD', ''));
+define('DB_DB',env('DB_DATABASE', 'forge'));
 define('PREFIX','fs_');
 define('ERROR_REPORT',E_ALL);
-define('BASE_URL', $protocol . '//lmr.local/');
-define('URL_INTERN',$protocol . '://lmr.local/freiwillige/');
+define('BASE_URL', $protocol . '//'.$http_host.'/');
+define('URL_INTERN',$protocol . '://'.$http_host.'/freiwillige/');
 define('DEFAULT_EMAIL','noreply@lebensmittelretten.de');
 define('DEFAULT_EMAIL_NAME','Foodsharing Freiwillige');
 define('VERSION','0.8.1');
@@ -28,7 +32,7 @@ define('SMTP_HOST','');
 define('SMTP_USER','');
 define('SMTP_PASS','');
 define('SMTP_PORT',25);
-define('MEM_ENABLED', true);
+define('MEM_ENABLED', false);
 
 define('SOCK_URL', 'http://chat:1338/');
 define('REDIS_HOST', 'redis');
@@ -36,5 +40,9 @@ define('REDIS_PORT', 6379);
 
 if(!defined('ROOT_DIR'))
 {
-	define('ROOT_DIR','./');
+    define('ROOT_DIR','./');
+}
+
+if(!defined('SOCK_URL')) {
+    define('SOCK_URL', 'http://127.0.0.1:1338/');
 }
