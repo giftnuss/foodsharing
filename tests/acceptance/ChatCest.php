ed095abcf2952cebe8755e9be03a2648d898197a
Nick Sellen <git@nicksellen.co.uk>
2017-08-24 11:44:00 +0100

    Make chat test send and receive

--- tests/acceptance/ChatCest.php
+++ tests/acceptance/ChatCest.php
@@ -19,17 +19,54 @@ class ChatCest
 		$this->foodsaver2 = $I->createFoodsaver('pw', ['bezirk_id' => $this->testBezirk]);
 	}
 
-	public function CanSendAChatMessage(AcceptanceTester $I)
+	public function CanSendAndReceiveChatMessages(AcceptanceTester $I)
 	{
 		$I->login($this->foodsaver1['email'], 'pw');
+
+		// view the other users profile and start a chat
 		$I->amOnPage('/profile/'.$this->foodsaver2['id']);
 		$I->click('Nachricht schreiben');
 		$I->waitForElementVisible('.chatboxtextarea', 4);
-		$I->fillField('.chatboxtextarea', 'heya');
+
+		// write a message to them
+		$I->fillField('.chatboxtextarea', 'is anyone there?');
 		$I->pressKey('.chatboxtextarea',WebDriverKeys::ENTER);
+		$I->wait(1);
+
+		$I->see('is anyone there?', '.chatboxcontent');
+
 		$I->seeInDatabase('fs_msg', [
 			'foodsaver_id' => $this->foodsaver1['id'],
-			'body' => 'heya'
+			'body' => 'is anyone there?'
+		]);
+
+		$matthias = $I->haveFriend('matthias');
+		$matthias->does(function(AcceptanceTester $I)
+		{
+			$I->login($this->foodsaver2['email'], 'pw');
+			$I->amOnPage('/');
+
+			// check they have the nice little notification badge
+			$I->see('1', '.msg .badge');
+
+			// open the conversation menu and open the new conversation
+			$I->click('.msg a');
+			$I->waitForElementVisible('.conversation-list', 4);
+			$I->click('.unread-1 a');
+			$I->waitForElementVisible('.chatboxtextarea', 4);
+
+			// write a nice reply
+			$I->fillField('.chatboxtextarea', 'yes! I am here!');
+			$I->pressKey('.chatboxtextarea',WebDriverKeys::ENTER);
+			$I->wait(1);
+		});
+
+
+		$I->see('yes! I am here!', '.chatboxcontent');
+
+		$I->seeInDatabase('fs_msg', [
+			'foodsaver_id' => $this->foodsaver2['id'],
+			'body' => 'yes! I am here!'
 		]);
 	}
 }
