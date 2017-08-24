d94925604e1991c7ab5fa0d9505641d1c7dae448
Nick Sellen <git@nicksellen.co.uk>
2017-08-24 15:31:50 +0100

    Remove ini_set in logout.php already handled
    Doh! This is loaded in such a way that the lib/Session.php stuff
    that also calls ini_set is already done. Was getting confused.

--- control/logout.php
+++ control/logout.php
@@ -1,6 +1,4 @@
 <?php
-ini_set('session.save_handler', 'redis');
-ini_set('session.save_path', 'tcp://'.REDIS_HOST.':'.REDIS_PORT);
 $db->logout();
 $_SESSION['login'] = false;
 $_SESSION = array();
