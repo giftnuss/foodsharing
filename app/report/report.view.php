c1f1704def36928670e59317d48ff7d5a535715b
Tönnies, Peter <peter.toennies@ge.com>
2017-09-08 14:15:04 +0200

    arrays to short syntax removed redundant unit in 0px explicit return for public function listReportedSavers($foodsaver)

--- app/report/report.view.php
+++ app/report/report.view.php
@@ -18,171 +18,63 @@ class ReportView extends View
 		return v_input_wrapper('Warum möchtest Du '.$this->foodsaver['name'].' melden?', '
 
 			'.v_form_select('reportreason',array('required'=>true,'nowrapper' => true,'value' => 1,'values'=>array(
-				array(
-					'id' => 1,
-					'name' => 'Ist zu spät zum Abholen gekommen'
-				),
-				array(
-					'id' => 2,
-					'name' => 'Ist gar nicht zum Abholen gekommen'
-				),
-				array(
-					'id' => 3,
-					'name' => 'Hat sich unhöflich oder respektlos verhalten'
-				),
-				array(
-					'id' => 4,
-					'name' => 'Hat den Abholort nicht sauber hinterlassen'
-				),
-				array(
-					'id' => 5,
-					'name' => 'Hat sich nicht gemeinschaftlich und sozial beim Abholen verhalten'
-				),
-				array(
-					'id' => 6,
-					'name' => 'Hat sich fordernd/übergriffig verhalten'
-				),
-				array(
-					'id' => 7,
-					'name' => 'Hat Vorwürfe gemacht'
-				),
-				array(
-					'id' => 8,
-					'name' => 'Hat Sachen mitgenommen die nicht für ihn/sie bestimmt waren'
-				),
-				array(
-					'id' => 9,
-					'name' => 'Hat Pfandflaschen/-kisten etc. nicht zurückgebracht'
-				),
-				array(
-					'id' => 10,
-					'name' => 'Häufiges kurzfristiges Absagen der Abholungen'
-				),
-				array(
-					'id' => 11,
-					'name' => 'Ignoriert Kontaktaufnahme'
-				),
-				array(
-					'id' => 12,
-					'name' => 'Schmeißt gerettete Lebensmittel weg'
-				),
-				array(
-					'id' => 13,
-					'name' => 'Nimmt nicht alle zur Abholung vorgesehenen Lebensmitteln mit'
-				),
-				array(
-					'id' => 14,
-					'name' => 'Hat sich außerhalb seiner/ihrer Abholzeit beim Betrieb zu rettende Lebensmittel genommen oder nachgefragt'
-				),
-				array(
-					'id' => 15,
-					'name' => 'Verkauft gerettete Lebensmittel'
-				),
-				array(
-					'id' => 16,
-					'name' => 'Hat gegen andere Verhaltensregeln verstoßen (alles andere)'
-				)
+                ['id' => 1, 'name' => 'Ist zu spät zum Abholen gekommen'],
+                ['id' => 2, 'name' => 'Ist gar nicht zum Abholen gekommen'],
+                ['id' => 3, 'name' => 'Hat sich unhöflich oder respektlos verhalten'],
+                ['id' => 4, 'name' => 'Hat den Abholort nicht sauber hinterlassen'],
+                ['id' => 5, 'name' => 'Hat sich nicht gemeinschaftlich und sozial beim Abholen verhalten'],
+                ['id' => 6, 'name' => 'Hat sich fordernd/übergriffig verhalten'],
+                ['id' => 7, 'name' => 'Hat Vorwürfe gemacht'],
+                ['id' => 8, 'name' => 'Hat Sachen mitgenommen die nicht für ihn/sie bestimmt waren'],
+                ['id' => 9, 'name' => 'Hat Pfandflaschen/-kisten etc. nicht zurückgebracht'],
+                ['id' => 10, 'name' => 'Häufiges kurzfristiges Absagen der Abholungen'],
+                ['id' => 11, 'name' => 'Ignoriert Kontaktaufnahme'],
+                ['id' => 12, 'name' => 'Schmeißt gerettete Lebensmittel weg'],
+                ['id' => 13, 'name' => 'Nimmt nicht alle zur Abholung vorgesehenen Lebensmitteln mit'],
+                ['id' => 14, 'name' => 'Hat sich außerhalb seiner/ihrer Abholzeit beim Betrieb zu rettende Lebensmittel genommen oder nachgefragt'],
+                ['id' => 15, 'name' => 'Verkauft gerettete Lebensmittel'],
+                ['id' => 16, 'name' => 'Hat gegen andere Verhaltensregeln verstoßen (alles andere)']
 			))).'<br />
-			<div id="reportreason_3" class="cb" style="margin:5px 0px;">
+			<div id="reportreason_3" class="cb" style="margin:5px 0;">
 			'.v_form_checkbox('reportreason_3',array('nowrapper' => true,'value' => 1,'values'=>array(
-				array(
-					'id' => 1,
-					'name' => 'gegenüber Foodsavern'
-				),
-				array(
-					'id' => 2,
-					'name' => 'gegenüber BetriebsmitarbeiterInnen'
-				)
+                ['id' => 1, 'name' => 'gegenüber Foodsavern'],
+                ['id' => 2, 'name' => 'gegenüber BetriebsmitarbeiterInnen']
 			))).'
 			</div>
 			'.v_form_select('reportreason_3_sub',array('nowrapper' => true,'value' => 1,'values'=>array(
-				array(
-					'id' => 1,
-					'name' => 'beleidigende Äußerungen'
-				),
-				array(
-					'id' => 2,
-					'name' => 'rassistische Äußerungen'
-				),
-				array(
-					'id' => 3,
-					'name' => 'sexistische Äußerungen'
-				),
-				array(
-					'id' => 4,
-					'name' => 'homophobe Äußerungen'
-				),
-				array(
-					'id' => 5,
-					'name' => 'Gewaltätigkeit und Drohung'
-				),
-				array(
-					'id' => 6,
-					'name' => 'Andere unangebrachte Äußerungen und Verhalten'
-				)
+                ['id' => 1, 'name' => 'beleidigende Äußerungen'],
+                ['id' => 2, 'name' => 'rassistische Äußerungen'],
+                ['id' => 3, 'name' => 'sexistische Äußerungen'],
+                ['id' => 4, 'name' => 'homophobe Äußerungen'],
+                ['id' => 5, 'name' => 'Gewaltätigkeit und Drohung'],
+                ['id' => 6, 'name' => 'Andere unangebrachte Äußerungen und Verhalten']
 			))).'
 			<div id="reportreason_6" class="cb">
 			'.v_form_checkbox('reportreason_6',array('nowrapper' => true,'value' => 1,'values'=>array(
-				array(
-					'id' => 1,
-					'name' => 'gegenüber BetriebsmitarbeiterInnen'
-				),
-				array(
-					'id' => 2,
-					'name' => 'gegenüber Foodsavern'
-				),
-				array(
-					'id' => 3,
-					'name' => 'gegenüber Kunden'
-				)
+                ['id' => 1, 'name' => 'gegenüber BetriebsmitarbeiterInnen'],
+                ['id' => 2, 'name' => 'gegenüber Foodsavern'],
+                ['id' => 3, 'name' => 'gegenüber Kunden']
 			))).'
 			</div>
 			<div id="reportreason_5" class="cb">
 			'.v_form_checkbox('reportreason_5',array('nowrapper' => true,'value' => 1,'values'=>array(
-				array(
-					'id' => 1,
-					'name' => 'vor BetriebsmitarbeiterInnen'
-				),
-				array(
-					'id' => 2,
-					'name' => 'vor Foodsavern'
-				),
-				array(
-					'id' => 3,
-					'name' => 'vor Kunden'
-				)
+                ['id' => 1, 'name' => 'vor BetriebsmitarbeiterInnen'],
+                ['id' => 2, 'name' => 'vor Foodsavern'],
+                ['id' => 3, 'name' => 'vor Kunden']
 			))).'
 			</div>
 			<div id="reportreason_7" class="cb">
 			'.v_form_checkbox('reportreason_7',array('nowrapper' => true,'value' => 1,'values'=>array(
-				array(
-					'id' => 1,
-					'name' => 'gegenüber BetriebsmitarbeiterInnen'
-				),
-				array(
-					'id' => 2,
-					'name' => 'gegenüber Foodsavern'
-				),
-				array(
-					'id' => 3,
-					'name' => 'gegenüber Kunden'
-				)
+                ['id' => 1, 'name' => 'gegenüber BetriebsmitarbeiterInnen'],
+                ['id' => 2, 'name' => 'gegenüber Foodsavern'],
+                ['id' => 3, 'name' => 'gegenüber Kunden']
 			))).'
 			</div>
 			<div id="reportreason_8" class="cb">
 			'.v_form_checkbox('reportreason_8',array('nowrapper' => true,'value' => 1,'values'=>array(
-				array(
-					'id' => 1,
-					'name' => 'von BetriebsmitarbeiterInnen'
-				),
-				array(
-					'id' => 2,
-					'name' => 'von Foodsavern'
-				),
-				array(
-					'id' => 3,
-					'name' => 'von Kunden'
-				)
+                ['id' => 1, 'name' => 'von BetriebsmitarbeiterInnen'],
+                ['id' => 2, 'name' => 'von Foodsavern'],
+                ['id' => 3, 'name' => 'von Kunden']
 			))).'
 			</div>');
 	}
@@ -237,11 +129,11 @@ class ReportView extends View
 		foreach ($reports as $r)
 		{
 			$rows[] = array(
-				array('cnt' => '<input type="hidden" class="rid" name="rid" value="'.$r['id'].'"><span class="photo"><a title="'.$r['fs_name'].' '.$r['fs_nachname'].'" href="#" onclick="profile('.(int)$r['fs_id'].');return false;"><img id="miniq-'.$r['fs_id'].'" src="'.img($r['fs_photo']).'" /></a></span>'),
-				array('cnt' => '<span class="photo"><a title="'.$r['rp_name'].' '.$r['rp_nachname'].'" href="#" onclick="profile('.(int)$r['rp_id'].');return false;"><img id="miniq-'.$r['rp_id'].'" src="'.img($r['rp_photo']).'" /></a></span>'),		
-				array('cnt' => tt($r['msg'],50)),
-				array('cnt' => '<span style="display:none;">a'.$r['time_ts'].' </span>'.niceDateShort($r['time_ts']).' Uhr'),
-				array('cnt' => $r['fs_stadt']),
+                ['cnt' => '<input type="hidden" class="rid" name="rid" value="' . $r['id'] . '"><span class="photo"><a title="' . $r['fs_name'] . ' ' . $r['fs_nachname'] . '" href="#" onclick="profile(' . (int)$r['fs_id'] . ');return false;"><img id="miniq-' . $r['fs_id'] . '" src="' . img($r['fs_photo']) . '" /></a></span>'],
+                ['cnt' => '<span class="photo"><a title="' . $r['rp_name'] . ' ' . $r['rp_nachname'] . '" href="#" onclick="profile(' . (int)$r['rp_id'] . ');return false;"><img id="miniq-' . $r['rp_id'] . '" src="' . img($r['rp_photo']) . '" /></a></span>'],
+                ['cnt' => tt($r['msg'], 50)],
+                ['cnt' => '<span style="display:none;">a' . $r['time_ts'] . ' </span>' . niceDateShort($r['time_ts']) . ' Uhr'],
+                ['cnt' => $r['fs_stadt']],
 			);
 		}
 		
@@ -255,12 +147,9 @@ class ReportView extends View
 		
 		return $table;
 	}
-	
-	public function listReportedSavers($foodsaver)
-	{
-		if(is_array($foodsaver))
-		{
-			return v_field($this->fsAvatarList($foodsaver,array('noshuffle' => true)),'Alle gemeldeten Foodsaver');
-		}
-	}
+
+    public function listReportedSavers($foodsaver)
+    {
+        return is_array($foodsaver) ? v_field($this->fsAvatarList($foodsaver, ['noshuffle' => true]), 'Alle gemeldeten Foodsaver') : null;
+    }
 }
