2990d992dcb7a216277178a7ed48eca9116e0fa2
Peter Tönnies <peter.toennies@gmail.com>
2017-09-20 15:51:11 +0200

    typo in word verified

--- app/bcard/bcard.control.php
+++ app/bcard/bcard.control.php
@@ -29,7 +29,7 @@ class BcardControl extends Control
 				error('Du musst eine gültige Telefonnummer angegeben haben um Deine Visitenkarte zu generieren');
 				go('/?page=settings');
             }
-            if ($data[`verfified`] == 0) {
+            if ($data[`verified`] == 0) {
                 // you have to be a verified user to generate your business card.
                 error('Du musst verifiziert sein, um Deine Visitenkarte generieren zu können.');
                 go('/?page=settings');
