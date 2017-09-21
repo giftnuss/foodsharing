6fb805382040fedcb8c8254d3df5eeb5acf44508
Matthias Larisch <mail@matthias-larisch.de>
2017-09-21 18:50:56 +0200

    Split revision generation into own file
    Can be used locally and/or in CI.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 git fetch gitlab && git checkout -f gitlab/master && composer install --no-dev
-REV=$((git rev-list HEAD --max-count=1))
-echo "<?php" > revision.inc.php
-echo "define('SRC_REVISION', '"$REV"');" >> revision.inc.php
+./generate-revision.sh
