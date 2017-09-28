c5a7d0c5c2926169634c39bd6ad1576cf1530191
Matthias Larisch <mail@matthias-larisch.de>
2017-09-28 19:45:41 +0200

    HtmlAcceptanceTester _failed fix for early failure
    Actually, PHPBrowser should already provide the functionality. Problem
    was, that for a test failure before a page load, the browser could not
    save the page source because there was none. InnerBrowser checks for
    that first.

--- tests/_support/Helper/HtmlAcceptance.php
+++ tests/_support/Helper/HtmlAcceptance.php
@@ -10,11 +10,4 @@ use Codeception\Test\Descriptor;
 
 class HtmlAcceptance extends \Codeception\Module
 {
-	public function _failed(TestInterface $test, $fail)
-	{
-		$filename = preg_replace('~\W~', '.', Descriptor::getTestSignature($test));
-		$outputDir = codecept_output_dir();
-		$this->getModule('PhpBrowser')->_savePageSource($outputDir . mb_strcut($filename, 0, 244, 'utf-8') . '.fail.html');
-		$this->debug("page source was saved into '$outputDir' dir");
-	}
 }
