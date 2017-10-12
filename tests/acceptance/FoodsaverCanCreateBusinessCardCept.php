3a77e81a530cbf9d3b971a01cd8c0c343fb066d4
Matthias Larisch <mail@matthias-larisch.de>
2017-10-12 17:54:59 +0200

    Add test for foodsaver business card generation

---
+++ tests/acceptance/FoodsaverCanCreateBusinessCardCept.php
@@ -0,0 +1,30 @@
+<?php
+
+function convertId($text) {
+	$text = strtolower($text);
+	str_replace(
+		array('ä', 'ö', 'ü', 'ß', ' '),
+		array('ae', 'oe', 'ue', 'ss', '_'),
+		$text
+	);
+	return preg_replace('/[^a-z0-9_]/', '', $text);
+}
+
+$testRegion = 241;
+$I = new AcceptanceTester($scenario);
+$I->wantTo('create a business card as a foodsaver');
+
+$regionName = $I->grabFromDatabase('fs_bezirk', 'name', ['id' => $testRegion]);
+$foodsaver = $I->createFoodsaver(null, ['name' => 'fs1', 'nachname' => 'saver1', 'photo' => 'does-not-exist.jpg']);
+$I->addBezirkMember($testRegion, $foodsaver['id']);
+
+$I->login($foodsaver['email']);
+
+$I->amOnPage('/?page=bcard');
+$I->selectOption('Optionen', 'Foodsaver für '. $regionName);
+$I->waitForElementVisible('#pulse-info');
+$I->click('Visitenkarten herunterladen');
+
+$I->wait(4);
+
+$I->seeFileExists('/downloads/bcard-fs-'. convertId($foodsaver['name']).'-'.convertId($foodsaver['nachname']).'.pdf');
