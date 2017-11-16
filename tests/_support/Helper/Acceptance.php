3c551c5a27a9c933f3248bfdd8a96e1f1e4e2932
Matthias Larisch <mail@matthias-larisch.de>
2017-11-16 21:33:08 +0100

    waitUrlEquals Helper to wait until current url is

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -5,6 +5,8 @@ namespace Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
+use Facebook\WebDriver\WebDriverExpectedCondition;
+
 class Acceptance extends \Codeception\Module
 {
 	/**
@@ -26,5 +28,17 @@ class Acceptance extends \Codeception\Module
 	public function seeFileExists($filename)
 	{
 		\PHPUnit_Framework_Assert::assertTrue(file_exists($filename));
+	}
+
+	/**
+	 * Wait until the open URL equals given one.
+	 *
+	 * @param $url
+	 * @param int $timeout
+	 */
+	public function waitUrlEquals($url, $timeout = 4)
+	{
+		$condition = WebDriverExpectedCondition::urlIs($url);
+		$this->getModule('WebDriver')->webDriver->wait($timeout)->until($condition);
 	}
 }
