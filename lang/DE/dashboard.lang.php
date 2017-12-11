9916782341726d36860ecdfdadde2b892408615c
Matthias Larisch <mail@matthias-larisch.de>
2017-12-11 20:26:28 +0100

    Fix wrong renaming of language files
    Also duplicates dashboard as there is still an 'old style' component
    needing the lowercase one.
    As dashboard is actually two distinct modules, duplication is somehow
    reasonable.

---
+++ lang/DE/dashboard.lang.php
@@ -0,0 +1,28 @@
+<?php 
+global $g_lang;
+$g_lang['already_in_bezirk'] = 'In diesem Bezirk bist Du schon aktiv';
+$g_lang['request_already_send'] = 'An diesen Bezirk hast Du schon eine Anfrage gesendet.';
+$g_lang['bezirk_request_successfull'] = 'Die Anfrage wurde erfolgreich versendet. Du wirst benachrichtigt sobald Du freigeschaltet wurdest.';
+$g_lang['position_search'] = 'Positionssuche';
+$g_lang['ort'] = 'Stadt/Gemeinde';
+$g_lang['team_status_0'] = 'Dieses Team ist voll. Daher werden z.<span style="white-space:nowrap">&thinsp;</span>Z. keine weiteren Helfer gesucht.';
+$g_lang['team_status_1'] = '<strong>Es werden noch Helfer gesucht</strong>';
+$g_lang['team_status_2'] = '<strong>Es werden dringend noch Helfer gesucht</strong>';
+$g_lang['attach_image'] = 'Bild anhängen';
+$g_lang['write_teaser'] = 'Schreibe etwas...';
+$g_lang['pubbtime_1'] = 'morgens';
+$g_lang['pubbtime_2'] = 'mittags/nachmittags';
+$g_lang['pubbtime_3'] = 'abends';
+$g_lang['pubbtime_4'] = 'nachts';
+$g_lang['next_dates'] = 'Nächste Abholtermine';
+$g_lang['updates'] = 'Letzte Aktivitäten';
+$g_lang['bot_partners'] = 'Deine Botschafterpatenkinder';
+$g_lang['fs_partners'] = 'Deine Patenkinder';
+$g_lang['welcome'] = 'Willkommen {name}! Schön, dass Du da bist!';
+$g_lang['no_saved_food'] = 'Viel Spaß beim Retten!';
+$g_lang['saved_food'] = 'Du hast schon {weight} Kilos gerettet!';
+$g_lang['new_foodbaskets'] = 'Neueste Essenskörbe';
+$g_lang['close_foodbaskets'] = 'Essenskörbe in Deiner Nähe';
+$g_lang['more_baskets'] = 'Alle Körbe auf der Karte';
+$g_lang['new_basket'] = 'Neuer Essenskorb';
+$g_lang['all_baskets'] ='Alle Körbe auf der Karte';
