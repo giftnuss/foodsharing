7a06ac0e62ee9dd9cba876dcb6c8fc2fd859b771
Matthias Larisch <mail@matthias-larisch.de>
2018-02-10 23:27:30 +0100

    Globally instantiate lib/View/Utils

--- includes/setup.php
+++ includes/setup.php
@@ -1,3 +1,6 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+
+/* Intermediate global instantiation. Should be done via DI at some point. */
+$g_view_utils = new \Foodsharing\Lib\View\Utils();
