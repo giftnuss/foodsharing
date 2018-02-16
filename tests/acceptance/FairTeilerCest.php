c0c4b25325b0e27f3d4e0b95dde1c72580d929f1
Matthias Larisch <mail@matthias-larisch.de>
2018-02-16 10:19:39 +0100

    Add simple fairteiler list/view test

---
+++ tests/acceptance/FairTeilerCest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: matthias
+ * Date: 16.02.18
+ * Time: 10:06.
+ */
+class FairTeilerCest
+{
+	private $testBezirk;
+	private $responsible;
+	private $user;
+	private $fairTeiler;
+
+	public function _before(AcceptanceTester $I)
+	{
+		$this->testBezirk = $I->createRegion('MyFunnyBezirk');
+		$this->user = $I->createFoodsharer(null, ['bezirk_id' => $this->testBezirk['id']]);
+		$this->responsible = $I->createFoodsaver();
+		$this->fairTeiler = $I->createFairteiler($this->responsible['id'], $this->testBezirk['id']);
+	}
+
+	/**
+	 * @param AcceptanceTester $I
+	 *
+	 * */
+	public function CanSeeFairTeilerInList(AcceptanceTester $I)
+	{
+		$I->amOnPage($I->fairTeilerRegionListUrl($this->testBezirk['id']));
+		$I->waitForText($this->fairTeiler['name']);
+		$I->click($this->fairTeiler['name']);
+		$I->waitForText(explode("\n", $this->fairTeiler['anschrift'])[0]);
+	}
+}
