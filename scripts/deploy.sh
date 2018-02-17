d53d4f815fc5593b94a66585666a7f74c6257465
Nick Sellen <git@nicksellen.co.uk>
2018-02-17 15:07:26 +0100

    Delete tmp files as www-data user

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git fetch gitlab && git checkout -f gitlab/master && composer install --no-dev
-rm -rf tmp/.views-cache tmp/di-cache.php
+sudo -u www-data rm -rf tmp/.views-cache tmp/di-cache.php
 ./scripts/generate-revision.sh
