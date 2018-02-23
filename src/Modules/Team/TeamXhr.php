49a8e2e776ae0b11ac2789ad3ddf10f03156ccf3
Peter Tönnies <peter.toennies@gmail.com>
2018-02-24 00:40:15 +0100

    removed dead code in team

--- src/Modules/Team/TeamXhr.php
+++ src/Modules/Team/TeamXhr.php
@@ -57,42 +57,4 @@ class TeamXhr extends Control
 		$xhr->addMessage($this->func->s('error'), 'error');
 		$xhr->send();
 	}
-
-	private function getIp()
-	{
-		if (!isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-			return $_SERVER['REMOTE_ADDR'];
-		} else {
-			return $_SERVER['HTTP_X_FORWARDED_FOR'];
-		}
-
-		return false;
-	}
-
-	/**
-	 * Function to check and block an ip address.
-	 *
-	 * @param int $duration
-	 * @param string $context
-	 *
-	 * @return bool
-	 */
-	private function ipIsBlocked($duration = 60, $context = 'default')
-	{
-		$ip = $this->getIp();
-
-		if ($block = $this->model->qRow('SELECT UNIX_TIMESTAMP(`start`) AS `start`,`duration` FROM ' . PREFIX . 'ipblock WHERE ip = ' . $this->model->strval($this->getIp()) . ' AND context = ' . $this->model->strval($context))) {
-			if (time() < ((int)$block['start'] + (int)$block['duration'])) {
-				return true;
-			}
-		}
-
-		$this->model->insert('
-	REPLACE INTO ' . PREFIX . 'ipblock
-	(`ip`,`context`,`start`,`duration`)
-	VALUES
-	(' . $this->model->strval($ip) . ',' . $this->model->strval($context) . ',NOW(),' . (int)$duration . ')');
-
-		return false;
-	}
 }
