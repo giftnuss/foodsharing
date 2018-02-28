0bf0310166c49fa50f8ef6610868d558cf637ef0
Matthias Larisch <mail@matthias-larisch.de>
2018-02-28 21:18:45 +0100

    Use ARGON2 Password hash

--- src/Lib/Db/Db.php
+++ src/Lib/Db/Db.php
@@ -597,7 +597,7 @@ abstract class Db
 	 */
 	public function password_hash($password)
 	{
-		return password_hash($password, PASSWORD_BCRYPT);
+		return password_hash($password, PASSWORD_ARGON2I);
 	}
 
 	/**
