1f6076cc06780ccc40c81bbe6cb4f06bb59d00ab
Matthias Larisch <mail@matthias-larisch.de>
2018-05-24 21:11:52 +0200

    Update static data

--- migrations/incremental-20180521-privacy-policy.sql
+++ migrations/incremental-20180521-privacy-policy.sql
@@ -1,2 +1,4 @@
 ALTER TABLE fs_foodsaver ADD `privacy_policy_accepted_date` DATETIME DEFAULT NULL after `anmeldedatum`;
 ALTER TABLE fs_foodsaver_archive ADD `privacy_policy_accepted_date` DATETIME DEFAULT NULL after `anmeldedatum`;
+ALTER TABLE fs_foodsaver ADD `privacy_notice_accepted_date` DATETIME DEFAULT NULL after `anmeldedatum`;
+ALTER TABLE fs_foodsaver_archive ADD `privacy_notice_accepted_date` DATETIME DEFAULT NULL after `anmeldedatum`;
