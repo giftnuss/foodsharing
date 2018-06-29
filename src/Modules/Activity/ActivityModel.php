c7505e2c8ec033cb1266bf65fcf878e0e488ae69
Kristijan <k.miklobusec@gmail.com>
2018-06-30 01:22:49 +0200

    some fixes

--- src/Modules/Activity/ActivityModel.php
+++ src/Modules/Activity/ActivityModel.php
@@ -23,13 +23,9 @@ class ActivityModel extends Model
 	public function loadEventWallUpdates($page = 0)
 	{
 		$updates = array();
-		if ($up = $this->activityGateway->fetchAllEventUpdates(S::id(), $page)) {
-			$updates = $up;
-		}
+		$updates = $this->activityGateway->fetchAllEventUpdates(S::id(), $page);
+
 		$out = array();
-		if (!empty($updates)) {
-			$out = array();
-		}
 
 		foreach ($updates as $u) {
 			if (isset($hb[$u['event_id']])) {
