dce16cf0556e8843abc6db1199e910ca68dd1c9b
Andreas Langecker <a.langecker@posteo.de>
2018-07-07 21:05:57 +0200

    vue: date formatting, changed vue i18n  & changed vue data handling

---
+++ client/src/urls.js
@@ -0,0 +1,6 @@
+// these are used for generating link-paths inside vue
+// e.g. $url('profile', 15)
+
+export default {
+  profile: (id) => `/profile/${id}`
+}
--- client/src/vue.js
+++ client/src/vue.js
@@ -1,11 +1,27 @@
 import Vue from 'vue'
 import $ from 'jquery'
 import i18n from '@/i18n'
+import urls from '@/urls'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import dateFormat from 'date-fns/format'
+import dateLocaleDE from 'date-fns/locale/de'
+
+
+Vue.filter('dateFormat', (date, format) => {
+  return dateFormat(date, format, { locale: dateLocaleDE })
+})
 
 Vue.filter('i18n', (key, variables = {}) => {
+  console.warn(`i18n as a vue filter is deprecated. use i18n() as a vue functions`)
   return i18n(key, variables)
 })
+Vue.prototype.$i18n = (key, variables = {}) => {
+  return i18n(key, variables)
+}
+
+Vue.prototype.$url = (key, ...params) => {
+  return urls[key](...params)
+}
 
 export function vueUse (components) {
   $('.vue-wrapper').each((index, el) => {
@@ -17,12 +33,19 @@ export function vueUse (components) {
     }
 
     // eslint-disable-next-line no-new
-    new Vue({
+    let vm = new Vue({
       el,
       components,
       render (h) {
-        return h(elementName, { props })
+        return h(elementName)
       }
     })
+
+    for (let key in props) {
+      if (typeof vm.$children[0][key] === 'undefined' || typeof vm.$children[0][key] === 'function') {
+        throw new Error(`vueUse() Error: prop '${key}' needs to be defined in data()`)
+      }
+      vm.$children[0][key] = props[key]
+    }
   })
 }
