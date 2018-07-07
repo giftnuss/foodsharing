4f65ee0e18aaa69bee72db63a1e4d572bd1090a2
Andreas Langecker <a.langecker@posteo.de>
2018-07-07 13:20:52 +0200

    avatar vue component

---
+++ client/src/components/Avatar.vue
@@ -0,0 +1,20 @@
+
+
+<template>
+  <span
+    :style="`background-image:url(${url});`"
+    :class="`avatar size-${size} sleepmode-${sleepStatus}`"
+  >
+    &nbsp;
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {},
+    size: {},
+    sleepStatus: {}
+  }
+}
+</script>
