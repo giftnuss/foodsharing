62aa24cc864a533b652bd527ccd47b25acca697d
Andreas Langecker <a.langecker@posteo.de>
2018-07-07 13:18:00 +0200

    basic emoji implementation

---
+++ client/src/components/Emoji.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="emoji">
+     {{ emojis[name] }}
+  </span>
+</template>
+
+<script>
+import emojiList from '@/emojiList.json'
+
+// TODO: 
+export default {
+  props: ['name'],
+  data() {
+      return {
+          emojis: emojiList
+      }
+  }
+}
+</script>
