5509bda9be582f28ae9b03b9a13532d8346787f4
Andreas Langecker <a.langecker@posteo.de>
2018-07-17 15:30:30 +0200

    vue-only avatar

--- client/src/components/Avatar.vue
+++ client/src/components/Avatar.vue
@@ -1,12 +1,10 @@
 
 
 <template>
-  <span
-    :style="`background-image:url(${url});`"
-    :class="`avatar size-${size} sleepmode-${sleepStatus}`"
-  >
-    &nbsp;
-  </span>
+  <div
+    :style="style"
+    :class="{avatarDiv: true, sleeping: sleepStatus}"
+   />
 </template>
 
 <script>
@@ -15,6 +13,36 @@ export default {
     url: {},
     size: {},
     sleepStatus: {}
+  },
+  computed: {
+    style() {
+      return {
+        width: this.size+'px',
+        height: this.size+'px',
+        backgroundImage: `url(${this.url && false ? this.url : '/img/130_q_avatar.png'})`
+      }
+    }
   }
 }
 </script>
+
+
+<style lang="scss" scoped>
+.avatarDiv {
+  position: relative;
+  margin: auto;
+  background-size: cover;
+}
+.sleeping::after {
+  content: '';
+  display: block;
+  height: 50%;
+  width: 50%;
+  background-image: url('/img/sleep130x130.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: absolute;
+  top: 0;
+  left: 10%;
+}
+</style>
