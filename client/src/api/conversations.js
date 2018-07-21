fcad8e9ad28c88a8c66be1db11430e0697e4d047
Andreas Langecker <a.langecker@posteo.de>
2018-07-21 20:23:49 +0200

    possible fix for #250.1

--- client/src/api/conversations.js
+++ client/src/api/conversations.js
@@ -2,15 +2,17 @@ import { get } from './base'
 
 // wrapper around the legacy SearchXHR method
 export async function getConversationList () {
-  return (await get('/../xhrapp.php?app=msg&m=loadconvlist')).data.convs.map(c => ({
+  const res = await get('/../xhrapp.php?app=msg&m=loadconvlist')
+  if(!res.data.convs) return []
+  return res.data.convs.map(c => ({
     id: parseInt(c.id),
     title: c.name,
     lastMessageTime: c.last,
-    members: c.member.map((m) => ({
+    members: c.member ? c.member.map((m) => ({
       id: parseInt(m.id),
       name: m.name,
       avatar: m.photo ? '/images/mini_q_' + m.photo : null
-    })),
+    })) : [],
     lastMessage: {
       bodyRaw: c.last_message,
       authorId: c.last_foodsaver_id
