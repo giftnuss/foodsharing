fc1ab72bd661534532e3e75c6db5ffc96ead986b
Andreas Langecker <a.langecker@posteo.de>
2018-07-23 14:36:17 +0200

    alphabetical sorted adminMenu #250.4

--- client/src/components/Topbar/MenuAdmin.vue
+++ client/src/components/Topbar/MenuAdmin.vue
@@ -4,19 +4,9 @@
             <i class="fa fa-gear"/>
             <span class="d-md-none">Administration</span>
         </template>
-        <!-- TODO: replace hard coded links with $url() -->
-        <a v-if="isOrgaTeam" class="dropdown-item" href="/?page=region"><i class="fa fa-map" /> {{ $i18n('menu_manage_regions') }}</a>
-        <a v-if="isOrgaTeam" class="dropdown-item" href="/?page=geoclean&sub=lostregion"><i class="fa fa-map" /> {{ $i18n('menu_regions_without_bots') }}</a>
-        <a v-if="isOrgaTeam" class="dropdown-item" href="/?page=newarea"><i class="fa fa-map-o" /> {{ $i18n('menu_newarea') }}</a>
-        <a v-if="isOrgaTeam" class="dropdown-item" href="/?page=email"><i class="fa fa-envelope" /> {{ $i18n('menu_email') }}</a>
-        <a v-if="isOrgaTeam" class="dropdown-item" href="/?page=message_tpl"><i class="fa fa-envelope" /> {{ $i18n('menu_email_tpl') }}</a>
-        <a v-if="isOrgaTeam" class="dropdown-item" href="/?page=mailbox&a=manage"><i class="fa fa-envelope-o" /> {{ $i18n('menu_mailbox_manage') }}</a>
-        <a v-if="isOrgaTeam" class="dropdown-item" href="/?page=geoclean"><i class="fa fa-user" /> {{ $i18n('menu_foodsaver_without_region') }}</a>
-        <a v-if="isOrgaTeam" class="dropdown-item" href="/?page=content"><i class="fa fa-file-text" /> {{ $i18n('menu_content') }}</a>
-        <a v-if="isOrgaTeam" class="dropdown-item" href="/?page=faq"><i class="fa fa-question" /> {{ $i18n('menu_faq') }}</a>
-        <a v-if="may.editBlog" class="dropdown-item" href="/?page=blog&sub=manage"><i class="fa fa-newspaper-o" /> {{ $i18n('menu_blog') }}</a>
-        <a v-if="may.editQuiz" class="dropdown-item" href="/?page=quiz"><i class="fa fa-question-circle" /> {{ $i18n('menu_quiz') }}</a>
-        <a v-if="may.handleReports" class="dropdown-item" href="/?page=report&sub=uncom"><i class="fa fa-exclamation" /> {{ $i18n('menu_reports') }}</a>
+        <a v-for="item in items" :key="item.url" class="dropdown-item" :href="item.url">
+            <i :class="item.icon" /> {{ item.label }}
+        </a>
     </nav-item-dropdown>
 </template>
 <script>
@@ -31,6 +21,84 @@ export default {
         },
         may: {
             type: Object
+        }
+    },
+    computed: {
+        items() {
+            // TODO: replace hard coded links with $url()
+            let items = []
+            if(this.isOrgaTeam) {
+                items.push(...[
+                    {
+                        url: '/?page=region',
+                        icon: 'fa fa-map',
+                        label: this.$i18n('menu_manage_regions')
+                    },
+                    {
+                        url: '/?page=geoclean&sub=lostregion',
+                        icon: 'fa fa-map',
+                        label: this.$i18n('menu_regions_without_bots')
+                    },
+                    {
+                        url: '/?page=newarea',
+                        icon: 'fa fa-map-o',
+                        label: this.$i18n('menu_newarea')
+                    },
+                    {
+                        url: '/?page=email',
+                        icon: 'fa fa-envelope',
+                        label: this.$i18n('menu_email')
+                    },
+                    {
+                        url: '/?page=message_tpl',
+                        icon: 'fa fa-envelope',
+                        label: this.$i18n('menu_email_tpl')
+                    },
+                    {
+                        url: '/?page=faq',
+                        icon: 'fa fa-question',
+                        label: this.$i18n('menu_faq')
+                    },
+                    {
+                        url: '/?page=geoclean',
+                        icon: 'fa fa-user',
+                        label: this.$i18n('menu_foodsaver_without_region')
+                    },
+                    {
+                        url: '/?page=mailbox&a=manage',
+                        icon: 'fa fa-envelope-o',
+                        label: this.$i18n('menu_mailbox_manage')
+                    },
+                    {
+                        url: '/?page=content',
+                        icon: 'fa fa-file-text',
+                        label: this.$i18n('menu_content')
+                    }
+                ])
+            }
+            if(this.may.editBlog) {
+                items.push({
+                    url: '/?page=blog&sub=manage',
+                    icon: 'fa fa-newspaper-o',
+                    label: this.$i18n('menu_blog')
+                })
+            }
+            if(this.may.editQuiz) {
+                items.push({
+                    url: '/?page=quiz',
+                    icon: 'fa fa-question-circle',
+                    label: this.$i18n('menu_quiz')
+                })
+            }
+            if(this.may.handleReports) {
+                items.push(    {
+                    url: '/?page=report&sub=uncom',
+                    icon: 'fa fa-exclamation',
+                    label: this.$i18n('menu_reports')
+                })
+            }
+
+            return items.sort((a,b) => a.label.localeCompare(b.label))
         }
     }
 }
