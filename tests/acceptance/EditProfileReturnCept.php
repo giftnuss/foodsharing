26b946eb62902b7b701d99c3c8a083d8996dddb0
sleising <sleising@localhost.localdomain>
2018-08-07 21:20:54 +0200

    Wrote a Test for Issue #285, Changed the string for back_to_overview

---
+++ tests/acceptance/EditProfileReturnCept.php
@@ -0,0 +1,21 @@
+<?php
+
+$I = new AcceptanceTester($scenario);
+
+$I->wantTo('Check if an someone edditing a profile sees return to profile button and if return to profile button points to the edited profile');
+
+$member = $I->createFoodsaver();
+$region = $I->createRegion(null, 0, 3);
+$I->addBezirkMember($region['id'], $member['id']);
+$ambassador = $I->createAmbassador(null, ['bezirk_id' => $region['id']]);
+$I->addBezirkAdmin($region['id'], $ambassador['id']);
+
+$I->login($ambassador['email']);
+
+$I->amOnPage('/?page=foodsaver&a=edit&id=' . $member['id']);
+
+$I->see('Zurück zum Profil');
+$I->see($member['name']);
+$I->click('Zurück zum Profil');
+
+$I->seeCurrentUrlEquals('/profile/' . $member['id']);
