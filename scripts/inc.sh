01fd89a6d84b9aa10fd6ed28c1c3206ffd4e7e6e
flukx <felix.h@hilsky.de>
2018-10-19 10:13:51 +0200

    answer to NickSellen: fix grammer

--- scripts/inc.sh
+++ scripts/inc.sh
@@ -18,7 +18,7 @@ MYSQL_PASSWORD=${MYSQL_PASSWORD:-root}
 dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 function log-header() {
-  # prints a log header, take one argument as the printed title
+  # print a log header, take one argument as the printed title
   local text=$1;
   echo
   echo "============================================"
