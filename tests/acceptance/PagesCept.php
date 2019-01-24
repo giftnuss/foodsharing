b48373872ecef779ffb4b2a3fe471e9e058680cd
Peter Tönnies <peter.toennies@gmail.com>
2019-01-24 15:48:36 +0100

    fixed tests for new landing page text.

--- tests/acceptance/PagesCept.php
+++ tests/acceptance/PagesCept.php
@@ -4,7 +4,7 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure various pages work');
 
 $I->amOnPage('/');
-$I->see('foodsharing ist und bleibt kostenlos');
+$I->see('Ab sofort neue E-Mail-Adressen');
 
 $I->amOnPage('/essenskoerbe');
 $I->seeCurrentUrlEquals('/essenskoerbe/find'); // it redirects
