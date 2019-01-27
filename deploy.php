8395b3d0c687b3b3475064002ae87e914920030f
Matthias Larisch <mail@matthias-larisch.de>
2019-01-27 11:02:23 +0100

    Try to fix deployer permissions for cache folder

--- deploy.php
+++ deploy.php
@@ -19,7 +19,7 @@ set('shared_files', ['config.inc.prod.php']);
 set('shared_dirs', ['images', 'data', 'tmp', 'cache/searchindex']);
 
 // Writable dirs by web server
-set('writable_dirs', ['tmp', 'cache', 'cache/searchindex', 'cache/htmlpurifier-cache']);
+set('writable_dirs', ['tmp', 'cache']);
 set('http_user', 'www-data');
 set('clear_paths', ['cache/.views-cache', 'cache/di-cache.php']);
 
