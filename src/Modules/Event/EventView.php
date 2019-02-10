fafaa31cd1219ff3e8cc02e3273332ec6cce6c5c
Timo <hartmanntimo@hotmail.de>
2019-02-10 19:08:32 +0100

    Cleaned up code

--- src/Modules/Event/EventView.php
+++ src/Modules/Event/EventView.php
@@ -236,10 +236,10 @@ class EventView extends View
 
 	public function eventTop($event)
 	{
-		$end = ' bis ' . $this->func->ts_time($event['end_ts']);
-
 		if (date('Y-m-d', $event['start_ts']) != date('Y-m-d', $event['end_ts'])) {
 			$end = ' bis ' . $this->func->niceDate($event['end_ts']);
+		} else {
+			$end = ' bis ' . $this->func->ts_time($event['end_ts']);
 		}
 
 		$out = '
