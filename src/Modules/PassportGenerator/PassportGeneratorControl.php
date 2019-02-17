05fdc8fbd2cf81c318b1c3f883049840abeed452
Johannes Franz <jofranz90@gmail.com>
2019-02-17 20:26:33 +0100

    Added check if user is AMB for a specific region #392

--- src/Modules/PassportGenerator/PassportGeneratorControl.php
+++ src/Modules/PassportGenerator/PassportGeneratorControl.php
@@ -45,6 +45,10 @@ class PassportGeneratorControl extends Control
 
 	public function index()
 	{
+        if (!$this->func->isBotForA((array)$this->bezirk_id, false, true)) {
+            return;
+        }
+
 		$this->func->addBread($this->bezirk['name'], '/?page=bezirk&bid=' . $this->bezirk_id . '&sub=forum');
 		$this->func->addBread('Pass-Generator', $this->func->getSelf());
 
