4ebb31d216af8c7e841abae571cf05781a05e0f8
Matthias Larisch <mail@matthias-larisch.de>
2019-02-19 12:51:34 +0100

    Use API for deleting groups/regions/workgroups

--- src/Modules/RegionAdmin/RegionAdmin.js
+++ src/Modules/RegionAdmin/RegionAdmin.js
@@ -7,11 +7,29 @@ import 'leaflet'
 import 'leaflet.awesome-markers'
 import 'leaflet.markercluster'
 
-import { img } from '@/script'
+import { goTo, img, pulseError, pulseSuccess } from '@/script'
 import { expose } from '@/utils'
 
 import './RegionAdmin.css'
+import i18n from '@/i18n'
+import { deleteGroup } from '@/api/groups'
 
 expose({
-  img
+  img,
+  deleteActiveGroup
 })
+
+async function deleteActiveGroup() {
+  const groupName = $("#tree-hidden-name").val()
+  const groupId = $("#tree-hidden").val()
+  if (window.confirm(i18n('group.delete_group_sure', { groupName }))) {
+    try {
+      await deleteGroup(groupId)
+      pulseSuccess(i18n('success'))
+      goTo('/?page=region')
+    } catch (err) {
+      pulseError(i18n('error_unexpected'))
+      throw err
+    }
+  }
+}
