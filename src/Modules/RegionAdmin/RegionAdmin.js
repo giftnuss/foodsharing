e317567057924766e3ce47490aed4afbbda39309
Matthias Larisch <mail@matthias-larisch.de>
2019-02-19 12:55:21 +0100

    Fix style

--- src/Modules/RegionAdmin/RegionAdmin.js
+++ src/Modules/RegionAdmin/RegionAdmin.js
@@ -1,17 +1,16 @@
 import '@/core'
 import '@/globals'
+import $ from 'jquery'
+import i18n from '@/i18n'
 import 'jquery-tagedit'
 import 'jquery-tagedit-auto-grow-input'
 import 'jquery-dynatree'
 import 'leaflet'
 import 'leaflet.awesome-markers'
 import 'leaflet.markercluster'
-
 import { goTo, img, pulseError, pulseSuccess } from '@/script'
 import { expose } from '@/utils'
-
 import './RegionAdmin.css'
-import i18n from '@/i18n'
 import { deleteGroup } from '@/api/groups'
 
 expose({
@@ -19,9 +18,9 @@ expose({
   deleteActiveGroup
 })
 
-async function deleteActiveGroup() {
-  const groupName = $("#tree-hidden-name").val()
-  const groupId = $("#tree-hidden").val()
+async function deleteActiveGroup () {
+  const groupName = $('#tree-hidden-name').val()
+  const groupId = $('#tree-hidden').val()
   if (window.confirm(i18n('group.delete_group_sure', { groupName }))) {
     try {
       await deleteGroup(groupId)
