a9ffce2540cefd71a4036492c6fda6443ac6a00e
Matthias Larisch <mail@matthias-larisch.de>
2019-02-22 00:41:34 +0100

    deploy ci built version

--- deploy.php
+++ deploy.php
@@ -16,18 +16,16 @@ set('git_tty', false);
 
 // Shared files/dirs between deploys
 set('shared_files', ['config.inc.prod.php']);
-set('shared_dirs', ['images', 'data', 'tmp', 'cache/searchindex']);
+set('shared_dirs', ['images', 'data', 'tmp']);
 
 // Writable dirs by web server
 set('writable_dirs', ['tmp', 'cache']);
 set('http_user', 'www-data');
-//set('clear_paths', ['cache/.views-cache', 'cache/di-cache.php', 'cache/prod', 'cache/log', 'cache/dev']);
 
 // default timeout of 300 was failing sometimes
 set('default_timeout', 600);
 
 // Hosts
-
 host('beta')
 	->hostname('banana.foodsharing.de')
 	->user('deploy')
@@ -39,12 +37,16 @@ host('production')
 	->set('deploy_path', '~/production-deploy');
 
 // Tasks
-
 desc('Create the revision information');
-task('deploy:create_revision', './scripts/generate-revision.sh');
+task('deploy:create_revision', function () {
+	$revision = input()->getOption('revision');
+	run('echo <?php > ' . get('release_path') . '/revision.inc.php');
+	run('echo define(\'SRC_REVISION\', \'' . $revision . '\'); >> ' . get('release_path') . '/revision.inc.php');
+});
 
-desc('Build the frontend');
-task('deploy:build_frontend', 'cd client && yarn && yarn build');
+task('deploy:update_code', function () {
+    upload(__DIR__ . '/', '{{release_path}}', ['--exclude deployer']);
+});
 
 desc('Deploy your project');
 task('deploy', [
@@ -55,10 +57,8 @@ task('deploy', [
 	'deploy:update_code',
 	'deploy:writable',
 	'deploy:shared',
-	'deploy:vendors',
 	'deploy:clear_paths',
 	'deploy:create_revision',
-	'deploy:build_frontend',
 	'deploy:symlink',
 	'deploy:unlock',
 	'cleanup',
