be1e3863c52fe2162b4628269d80cd4f07e43008
Tobias Kienzler <zommuter@users.noreply.github.com>
2019-02-22 22:15:31 +0100

    Add message informing user about the copy they'll receive

--- lang/DE/WorkGroup.lang.php
+++ lang/DE/WorkGroup.lang.php
@@ -8,4 +8,4 @@ $g_lang['week_num'] = 'Seit wie vielen Wochen sollte ein Bewerber schon dabei se
 $g_lang['report_num'] = 'Dürfen sich auch Foodsaver mit Meldungen von Regelverletzungen bewerben?';
 $g_lang['member'] = 'Mitglieder';
 $g_lang['leader'] = 'Gruppen-Admins';
-$g_lang['contact-disclaimer'] = 'Durch Senden einer Nachricht an diese Arbeitsgruppe erhalten alle Administratoren der Arbeitsgruppe deine Emailadresse, welche du auf deinem foodsharing.de Profil angegeben hast.';
+$g_lang['contact-disclaimer'] = 'Durch Senden einer Nachricht an diese Arbeitsgruppe erhalten alle Administratoren der Arbeitsgruppe deine Emailadresse, welche du auf deinem foodsharing.de Profil angegeben hast. Dir wird eine Kopie deiner Nachricht als Email geschickt.';
