1e5f120d71e89d91950de59ce33ff4569a0a6b2e
Nick Sellen <git@nicksellen.co.uk>
2019-02-24 16:33:23 +0100

    Add API Forum emoji test

--- tests/api/ForumApiCest.php
+++ tests/api/ForumApiCest.php
@@ -2,18 +2,26 @@
 
 class ForumApiCest
 {
+	private $tester;
+	private $user;
+	private $region;
+	private $thread;
+	private $faker;
+
 	public function _before(\ApiTester $I)
 	{
 		$this->tester = $I;
 		$this->user = $I->createFoodsaver();
 		$this->region = $I->createRegion();
+		$I->addBezirkMember($this->region['id'], $this->user['id']);
 		$this->thread = $I->addForumTheme($this->region['id'], $this->user['id']);
+		$this->faker = Faker\Factory::create('de_DE');
 	}
 
 	/**
 	 * @param ApiTester $I
 	 */
-	public function deleteNonExistingForumPostIs404(\ApiTester $I)
+	public function deleteNonExistingForumPostIs404(\ApiTester $I): void
 	{
 		$I->login($this->user['email']);
 		$I->sendDELETE('api/forum/post/9999999');
@@ -24,7 +32,7 @@ class ForumApiCest
 	/**
 	 * @param ApiTester $I
 	 */
-	public function deleteOwnPostSucceeds(\ApiTester $I)
+	public function deleteOwnPostSucceeds(\ApiTester $I): void
 	{
 		$I->login($this->user['email']);
 		$I->sendDELETE('api/forum/post/' . $this->thread['post']['id']);
@@ -35,12 +43,34 @@ class ForumApiCest
 	/**
 	 * @param ApiTester $I
 	 */
-	public function deleteForeignPostFails403(\ApiTester $I)
+	public function deleteForeignPostFails403(\ApiTester $I): void
 	{
 		$foreigner = $I->createFoodsaver();
 		$I->login($foreigner['email']);
 		$I->sendDELETE('api/forum/post/' . $this->thread['post']['id']);
 		$I->seeResponseCodeIs(\Codeception\Util\HttpCode::FORBIDDEN);
 		$I->seeResponseIsJson();
+	}
+
+	/**
+	 * @param ApiTester $I
+	 * @throws Exception
+	 */
+	public function canUseEmojis(\ApiTester $I): void
+	{
+		$I->login($this->user['email']);
+		$body = 'I am so 😂 for you! ' . $this->faker->text(50);
+		$threadPath = 'api/forum/thread/' . $this->thread['id'];
+		$I->sendPOST($threadPath . '/posts', [
+			'body' => $body
+		]);
+		$I->seeResponseCodeIs(\Codeception\Util\HttpCode::NO_CONTENT);
+		$I->seeInDatabase('fs_theme_post', ['body' => $body]);
+		$I->sendGET($threadPath);
+		$I->seeResponseIsJson();
+		$I->assertEquals(
+			'<p>' . $body . '</p>',
+			$I->grabDataFromResponseByJsonPath('$.data.posts[1].body')[0]
+		);
 	}
 }
