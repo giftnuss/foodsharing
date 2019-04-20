1b5f8920088f7b2fd354a86acf513d8935b983fa
Kristijan <kristijan80@web.de>
2019-04-20 08:52:23 +0200

    changing to tabs

--- src/Modules/Region/components/GenderList.vue
+++ src/Modules/Region/components/GenderList.vue
@@ -7,11 +7,31 @@
         {{ $i18n('genderlist.header_for_district', {bezirk: regionName}) }}
       </div>
       <div>
-        <b-table
-          striped
-          hover
-          :items="genderDataTab"
-        />
+        <b-tabs>
+          <b-tab
+            title="Gesamtbezirk"
+            active
+          >
+            <P />
+            <P>&nbsp;&nbsp;Aufteilung des Merkmales "Geschlecht" auf alle foodsaver, die dem Bezirk beigetreten sind.</P>
+            <b-table
+              striped
+              hover
+              :items="genderDataTab"
+            />
+          </b-tab>
+          <b-tab
+            title="Stammbezirk"
+          >
+            <P />
+            <p>&nbsp;&nbsp;Aufteilung des Merkmales "Geschlecht" auf die foodsaver, die als Stammbezirk eingetragen sind</p>
+            <b-table
+              striped
+              hover
+              :items="genderDataHomeDistrictTab"
+            />
+          </b-tab>
+        </b-tabs>
       </div>
     </div>
   </div>
@@ -20,15 +40,21 @@
 <script>
 
 import bTable from '@b/components/table/table'
+import bTabs from '@b/components/tabs/tabs'
+import bTab from '@b/components/tabs/tab'
 
 export default {
-  components: { bTable },
+  components: { bTable, bTabs, bTab },
   props: {
     regionName: {
       type: String,
       default: ''
     },
     genderDataTab: {
+      type: Array,
+      default: () => []
+    },
+    genderDataHomeDistrictTab: {
       type: Array,
       default: () => []
     },
