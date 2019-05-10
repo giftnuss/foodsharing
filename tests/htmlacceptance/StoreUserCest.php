1265b87374c3b9e957b0ff2b4e8cd51247afa8b5
Sven Oos <svenoos@mailbox.org>
2019-05-10 21:14:06 +0200

    Refactorings
    - Translated variable descriptors from German to English
    - Extracted method mentionPublicly($id) to improve functions’ level of abstraction
    - Added acceptance tests to make sure extract method did not break the behaviour.

--- tests/htmlacceptance/StoreUserCest.php
+++ tests/htmlacceptance/StoreUserCest.php
@@ -2,9 +2,13 @@
 
 class StoreUserCest
 {
-	private $min_amount_option = 1;
-	private $max_amount_option = 7;
-	private $amount_select_element_name = 'Abholmenge im Schnitt';
+	private $min_quantity_option = 1;
+	private $max_quantity_option = 7;
+	private $quantity_element_name = 'Abholmenge im Schnitt';
+
+	private $mentioning_public = 0;
+	private $mentioning_private = 1;
+	private $mentioning_element_name = 'Namensnennung';
 
 	public function _before(HtmlAcceptanceTester $I)
 	{
@@ -13,33 +17,55 @@ class StoreUserCest
 		$I->login($this->storeCoordinator['email']);
 	}
 
-	public function DontSeeTheAmountOfFood(HtmlAcceptanceTester $I)
+	public function DontSeeTheFetchedQuantity(HtmlAcceptanceTester $I)
 	{
-		$this->createStoreWithAmount($I, '');
+		$this->createStoreWithFetchedQuantity($I, '');
 
 		$I->amOnPage($I->storeUrl($this->store['id']));
-		$I->dontSee($this->amount_select_element_name);
+		$I->dontSee($this->quantity_element_name);
 	}
 
-	public function SeeTheMinimumAmountOfFood(HtmlAcceptanceTester $I)
+	public function SeeTheMinimumFetchedQuantity(HtmlAcceptanceTester $I)
 	{
-		$this->createStoreWithAmount($I, $this->min_amount_option);
+		$this->createStoreWithFetchedQuantity($I, $this->min_quantity_option);
 
 		$I->amOnPage($I->storeUrl($this->store['id']));
-		$I->see($this->amount_select_element_name);
+		$I->see($this->quantity_element_name);
 	}
 
-	public function SeeTheMaximumAmountOfFood(HtmlAcceptanceTester $I)
+	public function SeeTheMaximumFetchedQuantity(HtmlAcceptanceTester $I)
 	{
-		$this->createStoreWithAmount($I, $this->max_amount_option);
+		$this->createStoreWithFetchedQuantity($I, $this->max_quantity_option);
 
 		$I->amOnPage($I->storeUrl($this->store['id']));
-		$I->see($this->amount_select_element_name);
+		$I->see($this->quantity_element_name);
 	}
 
-	private function createStoreWithAmount($I, $store_amount_option)
+	private function createStoreWithFetchedQuantity($I, $fetched_quantity)
 	{
-		$this->store = $I->createStore($this->bezirk_id['id'], null, null, ['abholmenge' => $store_amount_option]);
+		$this->store = $I->createStore($this->bezirk_id['id'], null, null, ['abholmenge' => $fetched_quantity]);
+		$I->addStoreTeam($this->store['id'], $this->storeCoordinator['id'], true);
+	}
+
+	public function SeeStoreMayBeMentionedPublicly(HtmlAcceptanceTester $I)
+	{
+		$this->createStoreWithFetchedQuantity($I, $this->mentioning_public);
+
+		$I->amOnPage($I->storeUrl($this->store['id']));
+		$I->see($this->mentioning_element_name);
+	}
+
+	public function SeeStoreMustBeMentionedPrivately(HtmlAcceptanceTester $I)
+	{
+		$this->createStoreWithFetchedQuantity($I, $this->mentioning_private);
+
+		$I->amOnPage($I->storeUrl($this->store['id']));
+		$I->see($this->mentioning_element_name);
+	}
+
+	private function createStoreWithMentioning($I, $mentioning)
+	{
+		$this->store = $I->createStore($this->bezirk_id['id'], null, null, ['presse' => $mentioning]);
 		$I->addStoreTeam($this->store['id'], $this->storeCoordinator['id'], true);
 	}
 }
