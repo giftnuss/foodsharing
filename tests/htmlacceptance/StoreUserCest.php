066e3befd1f886ed6af77a129d8f8a528eae2c98
Sven Oos <svenoos@mailbox.org>
2019-05-12 15:49:01 +0200

    Parameterized tests
    Use parameter *example* for multiple options.

--- tests/htmlacceptance/StoreUserCest.php
+++ tests/htmlacceptance/StoreUserCest.php
@@ -2,14 +2,6 @@
 
 class StoreUserCest
 {
-	private $min_quantity_option = 1;
-	private $max_quantity_option = 7;
-	private $quantity_element_name = 'Abholmenge im Schnitt';
-
-	private $mentioning_public = 0;
-	private $mentioning_private = 1;
-	private $mentioning_element_name = 'Namensnennung';
-
 	public function _before(HtmlAcceptanceTester $I)
 	{
 		$this->bezirk_id = $I->createRegion('A region I test with');
@@ -17,55 +9,41 @@ class StoreUserCest
 		$I->login($this->storeCoordinator['email']);
 	}
 
-	public function DontSeeTheFetchedQuantity(HtmlAcceptanceTester $I)
+	/**
+	 * @param HtmlAcceptanceTester $I
+	 * @param \Codeception\Example $example
+	 * @example[1, "1-3 kg"]
+	 * @example[2, "3-5 kg"]
+	 * @example[3, "5-10 kg"]
+	 * @example[4, "10-20 kg"]
+	 * @example[5, "20-30 kg"]
+	 * @example[6, "40-50 kg"]
+	 * @example[7, "mehr als 50 kg"]
+	 */
+	public function SeeTheFetchedQuantity(HtmlAcceptanceTester $I, \Codeception\Example $example)
 	{
-		$this->createStoreWithFetchedQuantity($I, '');
-
-		$I->amOnPage($I->storeUrl($this->store['id']));
-		$I->dontSee($this->quantity_element_name);
-	}
-
-	public function SeeTheMinimumFetchedQuantity(HtmlAcceptanceTester $I)
-	{
-		$this->createStoreWithFetchedQuantity($I, $this->min_quantity_option);
-
-		$I->amOnPage($I->storeUrl($this->store['id']));
-		$I->see($this->quantity_element_name);
-	}
-
-	public function SeeTheMaximumFetchedQuantity(HtmlAcceptanceTester $I)
-	{
-		$this->createStoreWithFetchedQuantity($I, $this->max_quantity_option);
-
-		$I->amOnPage($I->storeUrl($this->store['id']));
-		$I->see($this->quantity_element_name);
-	}
-
-	private function createStoreWithFetchedQuantity($I, $fetched_quantity)
-	{
-		$this->store = $I->createStore($this->bezirk_id['id'], null, null, ['abholmenge' => $fetched_quantity]);
+		$this->store = $I->createStore($this->bezirk_id['id'], null, null, ['abholmenge' => $example[0]]);
 		$I->addStoreTeam($this->store['id'], $this->storeCoordinator['id'], true);
-	}
-
-	public function SeeStoreMayBeMentionedPublicly(HtmlAcceptanceTester $I)
-	{
-		$this->createStoreWithFetchedQuantity($I, $this->mentioning_public);
 
 		$I->amOnPage($I->storeUrl($this->store['id']));
-		$I->see($this->mentioning_element_name);
+
+		$I->see('Abholmenge im Schnitt');
+		$I->see($example[1]);
 	}
 
-	public function SeeStoreMustBeMentionedPrivately(HtmlAcceptanceTester $I)
+	/**
+	 * @param HtmlAcceptanceTester $I
+	 * @param \Codeception\Example $example
+	 * @example[0, "public"]
+	 * @example[1, "private"]
+	 */
+	public function SeeStoreMentioning(HtmlAcceptanceTester $I, \Codeception\Example $example)
 	{
-		$this->createStoreWithFetchedQuantity($I, $this->mentioning_private);
-
-		$I->amOnPage($I->storeUrl($this->store['id']));
-		$I->see($this->mentioning_element_name);
-	}
-
-	private function createStoreWithMentioning($I, $mentioning)
-	{
-		$this->store = $I->createStore($this->bezirk_id['id'], null, null, ['presse' => $mentioning]);
+		$this->store = $I->createStore($this->bezirk_id['id'], null, null, ['presse' => $example[0]]);
 		$I->addStoreTeam($this->store['id'], $this->storeCoordinator['id'], true);
+
+		$I->amOnPage($I->storeUrl($this->store['id']));
+
+		$I->see('Namensnennung');
 	}
 }
