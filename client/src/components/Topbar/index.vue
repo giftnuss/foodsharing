75ec7c03135478954e8968b29045020f7a0a7de5
chandi <git@chandi.it>
2019-06-05 18:25:01 +0200

    revert merge request 897:  Fixed big gap between brown menu bar and breadcrumb
    https://gitlab.com/foodsharing-dev/foodsharing/merge_requests/897

--- client/src/components/Topbar/index.vue
+++ client/src/components/Topbar/index.vue
@@ -446,12 +446,11 @@ export default {
 
 // move the main content below the topbar
 div#main {
-    margin-top: 45px;
-
-    @media (max-width: 630px) {
-        // two line topbar
-        margin-top: 74px;
-    }
+  margin-top: 45px;
+  @media (max-width: 630px) {
+      // two line topbar
+      margin-top: 85px;
+  }
 }
 
 // following is applied on the initial <div> before the vue component gets injected
