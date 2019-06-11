5e1e6aa2a7fb8657eb89b7399c5ac7d704a6bf8f
Peter Tönnies <peter.toennies@gmail.com>
2019-06-11 11:53:09 +0200

    removed bell id icremeenter

--- src/Modules/Maintenance/MaintenanceGateway.php
+++ src/Modules/Maintenance/MaintenanceGateway.php
@@ -12,9 +12,5 @@ class MaintenanceGateway extends BaseGateway
 		$this->db->delete('fs_foodsaver_has_bell', ['bell_id' => $bellIds]);
 
 		$this->db->delete('fs_bell', ['id' => $bellIds]);
-
-		$this->db->execute('LOCK TABLES `fs_bell` WRITE');
-		$this->db->execute('ALTER TABLE `fs_bell` AUTO_INCREMENT = (SELECT MAX(id) FROM `fs_bell`)');
-		$this->db->execute('UNLOCK TABLES');
 	}
 }
