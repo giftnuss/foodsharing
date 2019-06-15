7696f585a3cc09065a56ed51164a9312ab7faae0
Christian Walgenbach <chris.walg@posteo.de>
2019-06-16 00:34:10 +0200

    adjustments db sql-script

--- migrations/incremental-20190614-remove-tox-id.sql
+++ migrations/incremental-20190614-remove-tox-id.sql
@@ -1,7 +1,5 @@
-
-	ALTER TABLE fs_foodsaver
-	DROP COLUMN tox;
-	ALTER TABLE fs_foodsaver_archive4
-	DROP COLUMN tox;
-	ALTER TABLE fs_foodsaver_archive5
-	DROP COLUMN tox;
+ALTER TABLE fs_foodsaver DROP IF EXISTS tox;
+ALTER TABLE fs_foodsaver_archive DROP IF EXISTS tox;
+ALTER TABLE fs_foodsaver_archive2 DROP IF EXISTS tox;
+ALTER TABLE fs_foodsaver_archive4 DROP IF EXISTS tox;
+ALTER TABLE fs_foodsaver_archive5 DROP IF EXISTS tox;
