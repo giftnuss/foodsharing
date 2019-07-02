8368ecd7e4c4f01470a833a3ac29779345c68a1d
Johannes Franz <jofranz90@gmail.com>
2019-07-02 03:00:02 +0200

    Added new region id constants

--- src/Modules/Core/DBConstants/Region/RegionIDs.php
+++ src/Modules/Core/DBConstants/Region/RegionIDs.php
@@ -5,7 +5,9 @@ namespace Foodsharing\Modules\Core\DBConstants\Region;
 class RegionIDs
 {
 	/* second level from top. First selectable level */
-	public const EUROPE = 741;
-
+	public const QUIZ_AND_REGISTRATION_WORK_GROUP = 341;
+	public const GLOBAL_WORKING_GROUPS = 392;
 	public const EUROPE_REPORT_TEAM = 432;
+	public const EUROPE = 741;
+	public const EUROPE_BOT_GROUP = 881;
 }
