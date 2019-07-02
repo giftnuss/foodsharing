beb64bdd95ae1d5caac225bb1898f58afde47be3
Kniggerich <koelnerdomi@gmail.com>
2019-07-02 22:34:41 +0200

    case corrections

--- client/src/conv.js
+++ client/src/conv.js
@@ -306,10 +306,10 @@ const conv = {
   /**
   * addedclass to indicate own messages
   */
-    var ownmessageclass = ''
-    if (message.fs_id == serverData.user.id) { ownmessageclass = ' mymessage' }
+    var ownMessageClass = ''
+    if (message.fs_id == serverData.user.id) { ownMessageClass = ' my-message' }
     conv.chatboxes[key].last_mid = parseInt(message.id)
-    conv.chatboxes[key].el.children(`.slimScrollDiv`).children(`.chatboxcontent`).append(`<div title="${message.time}" class="chatboxmessage${ownmessageclass}"><span class="chatboxmessagefrom"><a class="photo" href="/profile/${message.fs_id}"><img src="${conv.img(message.fs_photo, 'mini')}"></a></span><span class="chatboxmessagecontent">${nl2br(autoLink(message.body))}<span class="time">${timeformat.nice(message.time)}</span></span><div style="clear:both;"></div></div>`)
+    conv.chatboxes[key].el.children(`.slimScrollDiv`).children(`.chatboxcontent`).append(`<div title="${message.time}" class="chatboxmessage${ownMessageClass}"><span class="chatboxmessagefrom"><a class="photo" href="/profile/${message.fs_id}"><img src="${conv.img(message.fs_photo, 'mini')}"></a></span><span class="chatboxmessagecontent">${nl2br(autoLink(message.body))}<span class="time">${timeformat.nice(message.time)}</span></span><div style="clear:both;"></div></div>`)
   },
 
   /**
