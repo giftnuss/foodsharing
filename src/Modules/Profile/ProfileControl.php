ba0e5ef84dd5323a2ab4c519fcd7f953ac4f997a
Johannes Franz <jofranz90@gmail.com>
2019-07-05 22:40:27 +0200

    Enable pickups in profile view

--- src/Modules/Profile/ProfileControl.php
+++ src/Modules/Profile/ProfileControl.php
@@ -81,7 +81,14 @@ final class ProfileControl extends Control
 			);
 		} else {
 			$this->view->profile(
-				$this->wallposts('foodsaver', $this->foodsaver['id'])
+				$this->wallposts('foodsaver', $this->foodsaver['id']),
+				false,
+				false,
+				false,
+				false,
+				null,
+				null,
+				$this->profileGateway->getNextDates($this->foodsaver['id'], 50)
 			);
 		}
 	}
