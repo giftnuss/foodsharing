9f204ccaa8a0a41c06316b332a4564c0a25299fc
Johannes Franz <jofranz90@gmail.com>
2019-07-06 15:16:09 +0200

    Made constants public

--- src/Modules/Store/StoreStatus.php
+++ src/Modules/Store/StoreStatus.php
@@ -6,10 +6,10 @@ class StoreStatus
 {
 	// see db/sql: SELECT * FROM fs_betrieb_status
 
-	const NO_CONTACT = 1; // "Es besteht noch kein Kontakt"
-	const IN_NEGOTIATION = 2; // "Verhandlungen laufen"
-	const COOPERATION_STARTING = 3; // "Betrieb ist bereit zu spenden :)"
-	const DOES_NOT_WANT_TO_WORK_WITH_US = 4; // "Will nicht kooperieren"
-	const COOPERATION_ESTABLISHED = 5; // "Betrieb kooperiert bereits" (mit uns)
-	const GIVES_TO_OTHER_CHARITY = 6; // "Spendet an Tafel etc. und wirft nichts weg"
+	public const NO_CONTACT = 1; // "Es besteht noch kein Kontakt"
+    public const IN_NEGOTIATION = 2; // "Verhandlungen laufen"
+    public const COOPERATION_STARTING = 3; // "Betrieb ist bereit zu spenden :)"
+    public const DOES_NOT_WANT_TO_WORK_WITH_US = 4; // "Will nicht kooperieren"
+    public const COOPERATION_ESTABLISHED = 5; // "Betrieb kooperiert bereits" (mit uns)
+    public const GIVES_TO_OTHER_CHARITY = 6; // "Spendet an Tafel etc. und wirft nichts weg"
 }
