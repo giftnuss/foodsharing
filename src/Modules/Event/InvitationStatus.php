8f4fd8b634ab5dd4050b92bb2f1d482c8dcb1616
Johannes Franz <jofranz90@gmail.com>
2019-07-07 15:01:42 +0200

    Made constants public

--- src/Modules/Event/InvitationStatus.php
+++ src/Modules/Event/InvitationStatus.php
@@ -4,8 +4,8 @@ namespace Foodsharing\Modules\Event;
 
 class InvitationStatus
 {
-	const INVITED = 0; // invited
-	const ACCEPTED = 1; // will join
-	const MAYBE = 2; // might join
-	const WONT_JOIN = 3; // will not join (but has been invited)
+	public const INVITED = 0; // invited
+	public const ACCEPTED = 1; // will join
+	public const MAYBE = 2; // might join
+	public const WONT_JOIN = 3; // will not join (but has been invited)
 }
