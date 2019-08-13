9df24114e08c24c4ffa29d6e5991f844a35216ab
Jano <jano.paetzold@gmail.com>
2019-08-13 02:30:19 +0200

    Typo

--- src/Modules/PushNotification/Notification/PushNotification.php
+++ src/Modules/PushNotification/Notification/PushNotification.php
@@ -8,7 +8,7 @@ abstract class PushNotification
 {
 	/**
 	 * If a PushNotificationHandler does not know a push notification type, it can create a simple text notification
-	 * without any extra features that features this fallback string. This is could useful for outdated clients, that
+	 * without any extra features that features this fallback string. This could be useful for outdated clients, that
 	 * could still display some sort of notification even if it does not know any features specific to the notification
 	 * type.
 	 */
