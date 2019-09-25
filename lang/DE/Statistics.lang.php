a3cb7c9909112deb50485f84f90bc87a24324dd2
Peter Tönnies <peter.toennies@gmail.com>
2019-09-25 16:17:36 +0200

    more accurate wording in statistics.

--- lang/DE/Statistics.lang.php
+++ lang/DE/Statistics.lang.php
@@ -9,7 +9,7 @@ $g_lang['stat_whole'] = 'Gesamtstatistik';
 // getStatFoodsaver
 $g_lang['most_active_foodsavers'] = 'Aktivste Foodsaver';
 // getStatAverageDailyFetches
-$g_lang['average_daily_fetches'] = 'Rettungseinsätze gibt es durchschnittlich pro Tag.';
+$g_lang['average_daily_fetches'] = 'Rettungseinsätze gibt es derzeit durchschnittlich pro Tag.';
 // getStatTotalBaskets
 $g_lang['total_baskets'] = 'Essenskörbe wurden insgesamt online gestellt.';
 // getStatAverageDailyBaskets
