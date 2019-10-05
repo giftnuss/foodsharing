73bce6b5860139a7371f7089965a887826a52811
Timo H <hartmanntimo@hotmail.de>
2019-10-05 19:30:54 +0200

    Fixing bug - end date required when creating single day event

--- src/Modules/Event/EventView.php
+++ src/Modules/Event/EventView.php
@@ -76,6 +76,8 @@ class EventView extends View
                 dateend_wrapper.style.display = "block";
 			}
 			
+			dateend_wrapper.classList.remove("required");
+			
 			$("#addend").on("change", function(){
 				if($("#addend:checked").length > 0)
 				{
