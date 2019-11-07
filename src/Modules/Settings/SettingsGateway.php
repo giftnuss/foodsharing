2ce8f97a8fbb0d872f19432715958b0a9223a225
Sven Oos <svenoos@mailbox.org>
2019-11-08 00:36:03 +0100

    Move method getQuizSession from SettingsModel to SettingsGateway

--- src/Modules/Settings/SettingsGateway.php
+++ src/Modules/Settings/SettingsGateway.php
@@ -57,4 +57,120 @@ class SettingsGateway extends BaseGateway
 				id = :fsId
 		', [':fsId' => $fsId]);
 	}
+
+	final public function getQuizSession(int $sessionId): array
+	{
+		$fsId = $this->session->id();
+		if ($session = $this->getQuizSession($sessionId, $fsId)) {
+			$tmp = array();
+			$session['try_count'] = getQuizSessionTryCount($fsId, $session['quiz_id']);
+
+			/*
+			 * First of all sort the question array and get all questions_ids etc to calculate the result
+			 */
+			if (!empty($session['quiz_questions'])) {
+				$session['quiz_questions'] = unserialize($session['quiz_questions']);
+
+				foreach ($session['quiz_questions'] as $quizQuestion) {
+					$tmp[$quizQuestion['id']] = $quizQuestion;
+					$ttmp = array();
+					if (isset($quizQuestion['answers'])) {
+						foreach ($quizQuestion['answers'] as $answer) {
+							$ttmp[$answer] = $answer;
+						}
+					}
+					if (!empty($ttmp)) {
+						$tmp[$quizQuestion['id']]['answers'] = $ttmp;
+					}
+				}
+			}
+
+			if (!empty($session['quiz_result'])) {
+				$session['quiz_result'] = unserialize($session['quiz_result']);
+
+				foreach ($session['quiz_result'] as $k => $quizResult) {
+					$session['quiz_result'][$k]['user'] = $tmp[$quizResult['id']];
+
+					foreach ($quizResult['answers'] as $k2 => $v2) {
+						$session['quiz_result'][$k]['answers'][$k2]['right'] = 0;
+						if ($v2['right'] == 1) {
+							$session['quiz_result'][$k]['answers'][$k2]['right'] = 1;
+						}
+						if ($v2['right'] == 2) {
+							$session['quiz_result'][$k]['answers'][$k2]['right'] = 2;
+						}
+						$session['quiz_result'][$k]['answers'][$k2]['user_say'] = false;
+						if (isset($session['quiz_result'][$k]['user']['answers'][$v2['id']])) {
+							$session['quiz_result'][$k]['answers'][$k2]['user_say'] = true;
+						}
+					}
+					if (!isset($session['quiz_result'][$k]['user']['userduration'])) {
+						$session['quiz_result'][$k]['userduration'] = $session['quiz_result'][$k]['user']['duration'];
+					} else {
+						$session['quiz_result'][$k]['userduration'] = $session['quiz_result'][$k]['user']['userduration'];
+					}
+					if (!isset($session['quiz_result'][$k]['user']['noco'])) {
+						$session['quiz_result'][$k]['noco'] = false;
+					} else {
+						$session['quiz_result'][$k]['noco'] = $session['quiz_result'][$k]['user']['noco'];
+					}
+					unset($session['quiz_result'][$k]['user']);
+				}
+
+				if ($quiz = $this->getValues(array('name', 'desc'), 'quiz', $session['quiz_id'])) {
+					$session = array_merge($quiz, $session);
+					unset($session['quiz_questions']);
+
+					/*
+					 * Add questions they're complete right answered
+					 */
+					$session['quiz_result'] = $this->addRightAnswers($tmp, $session['quiz_result']);
+
+					return $session;
+				}
+			}
+		}
+
+		return [];
+	}
+
+	private function getQuizSession(int $sessionId, int $fsId): array
+	{
+		return $this->db->fetch('
+			SELECT
+				`quiz_id`,
+				`status`,
+				`quiz_index`,
+				`quiz_questions`,
+				`quiz_result`,
+				`fp`,
+				`maxfp`
+
+			FROM
+				fs_quiz_session
+
+			WHERE
+				`id` = :sessionId
+
+			AND
+				`foodsaver_id` = :fsId
+		', [':sessionId' => $sessionId, ':fsId' => $fsId]);
+	}
+
+	private function getQuizSessionTryCount(int $fsId, int $quizId): string
+	{
+		return $this->db->fetchValue('
+			SELECT
+				COUNT(quiz_id)
+
+			FROM
+				fs_quiz_session
+
+			WHERE
+				foodsaver_id = :fsId
+
+			AND
+				`quiz_id` = :quizId
+		', [':fsId' => $fsId, ':quizId' => $quizId]);
+	}
 }
