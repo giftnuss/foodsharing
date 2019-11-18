996b35cf1b84f8e178d68288d8281c8f6053b9fa
Sven Oos <svenoos@mailbox.org>
2019-11-18 16:31:46 +0100

    Added unit test for updateGroupMembers

--- tests/unit/FoodsaverGatewayTest.php
+++ tests/unit/FoodsaverGatewayTest.php
@@ -11,11 +11,21 @@ class FoodsaverGatewayTest extends \Codeception\Test\Unit
 	 * @var \Foodsharing\Modules\Foodsaver\FoodsaverGateway
 	 */
 	private $gateway;
+	private $region;
+	private $regionMember;
+	private $foodsaver;
+	private $admin;
 
 	protected function _before()
 	{
 		$this->gateway = $this->tester->get(\Foodsharing\Modules\Foodsaver\FoodsaverGateway::class);
+		
 		$this->foodsaver = $this->tester->createFoodsaver();
+		$this->region = $this->tester->createRegion('TestRegion');
+		$this->regionMember = $this->tester->createFoodsaver();
+		$this->ambassador = $this->tester->createAmbassador();
+		$this->tester->addBezirkAdmin($this->region['id'], $this->ambassador['id']);
+		$this->tester->addBezirkMember($this->region['id'], $this->regionMember['id']);
 	}
 
 	public function testUpdateProfile()
@@ -41,5 +51,31 @@ class FoodsaverGatewayTest extends \Codeception\Test\Unit
 			$this->gateway->getPhoto($this->foodsaver['id']),
 			'mypicture.png'
 		);
+	}
+	
+	public function testUpdateGroupMembersByAddition() {
+		$newFoodsaver = $this->tester->createFoodsaver();
+		
+		$fsIds = [$newFoodsaver['id'], $this->regionMember['id']];
+		$updates = $this->gateway->updateGroupMembers($this->region['id'], $fsIds, true);
+		
+		$this->tester->seeInDatabase(
+			'fs_foodsaver_has_bezirk',
+			['foodsaver_id' => $newFoodsaver['id'], 'bezirk_id' => $this->region['id']]
+		);
+		$this->tester->assertEquals(count($fsIds), $updates[0], 'Wrong number of inserts!');
+		$this->tester->assertEquals(0, $updates[1], 'Wrong number of deletions!');
+	}
+	
+	public function testUpdateGroupMembersByDeletion() {
+		$fsIds = [];
+		$updates = $this->gateway->updateGroupMembers($this->region['id'], $fsIds, false);
+		
+		$this->tester->dontSeeInDatabase(
+			'fs_foodsaver_has_bezirk',
+			['foodsaver_id' => $this->regionMember['id'], 'bezirk_id' => $this->region['id']]
+		);
+		$this->tester->assertEquals(count($fsIds), $updates[0], 'Wrong number of inserts!');
+		$this->tester->assertEquals(1, $updates[1], 'Wrong number of deletions!');
 	}
 }
