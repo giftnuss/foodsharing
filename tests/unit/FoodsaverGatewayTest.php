16a9cd0c0ca66ff14228adecafe17d1b5f058599
Sven Oos <svenoos@mailbox.org>
2019-11-18 17:17:31 +0100

    Extend updateGroupMemberTest

--- tests/unit/FoodsaverGatewayTest.php
+++ tests/unit/FoodsaverGatewayTest.php
@@ -11,21 +11,23 @@ class FoodsaverGatewayTest extends \Codeception\Test\Unit
 	 * @var \Foodsharing\Modules\Foodsaver\FoodsaverGateway
 	 */
 	private $gateway;
+	private $foodsaver;
 	private $region;
 	private $regionMember;
-	private $foodsaver;
-	private $admin;
+	private $regionAdmin;
 
 	protected function _before()
 	{
 		$this->gateway = $this->tester->get(\Foodsharing\Modules\Foodsaver\FoodsaverGateway::class);
 		
 		$this->foodsaver = $this->tester->createFoodsaver();
+
 		$this->region = $this->tester->createRegion('TestRegion');
 		$this->regionMember = $this->tester->createFoodsaver();
-		$this->ambassador = $this->tester->createAmbassador();
-		$this->tester->addBezirkAdmin($this->region['id'], $this->ambassador['id']);
 		$this->tester->addBezirkMember($this->region['id'], $this->regionMember['id']);
+		$this->regionAdmin = $this->tester->createAmbassador();
+		$this->tester->addBezirkMember($this->region['id'], $this->regionAdmin['id']);
+		$this->tester->addBezirkAdmin($this->region['id'], $this->regionAdmin['id']);
 	}
 
 	public function testUpdateProfile()
@@ -53,7 +55,7 @@ class FoodsaverGatewayTest extends \Codeception\Test\Unit
 		);
 	}
 	
-	public function testUpdateGroupMembersByAddition() {
+	public function testUpdateGroupMembersByAdditionWhileLeavingAdmin() {
 		$newFoodsaver = $this->tester->createFoodsaver();
 		
 		$fsIds = [$newFoodsaver['id'], $this->regionMember['id']];
@@ -63,11 +65,37 @@ class FoodsaverGatewayTest extends \Codeception\Test\Unit
 			'fs_foodsaver_has_bezirk',
 			['foodsaver_id' => $newFoodsaver['id'], 'bezirk_id' => $this->region['id']]
 		);
-		$this->tester->assertEquals(count($fsIds), $updates[0], 'Wrong number of inserts!');
+		$this->tester->assertEquals(2, $updates[0], 'Wrong number of inserts!');
 		$this->tester->assertEquals(0, $updates[1], 'Wrong number of deletions!');
 	}
 	
-	public function testUpdateGroupMembersByDeletion() {
+	public function testUpdateGroupMembersByAdditionWhileNotLeavingAdmin() {
+		$newFoodsaver = $this->tester->createFoodsaver();
+		
+		$fsIds = [$newFoodsaver['id'], $this->regionMember['id']];
+		$updates = $this->gateway->updateGroupMembers($this->region['id'], $fsIds, false);
+		
+		$this->tester->seeInDatabase(
+			'fs_foodsaver_has_bezirk',
+			['foodsaver_id' => $newFoodsaver['id'], 'bezirk_id' => $this->region['id']]
+		);
+		$this->tester->assertEquals(1, $updates[0], 'Wrong number of inserts!');
+		$this->tester->assertEquals(1, $updates[1], 'Wrong number of deletions!');
+	}
+	
+	public function testUpdateGroupMembersByDeletionWhileLeavingAdmin() {
+		$fsIds = [];
+		$updates = $this->gateway->updateGroupMembers($this->region['id'], $fsIds, true);
+		
+		$this->tester->dontSeeInDatabase(
+			'fs_foodsaver_has_bezirk',
+			['foodsaver_id' => $this->regionMember['id'], 'bezirk_id' => $this->region['id']]
+		);
+		$this->tester->assertEquals(0, $updates[0], 'Wrong number of inserts!');
+		$this->tester->assertEquals(1, $updates[1], 'Wrong number of deletions!');
+	}
+	
+	public function testUpdateGroupMembersByDeletionWhileNotLeavingAdmin() {
 		$fsIds = [];
 		$updates = $this->gateway->updateGroupMembers($this->region['id'], $fsIds, false);
 		
@@ -75,7 +103,7 @@ class FoodsaverGatewayTest extends \Codeception\Test\Unit
 			'fs_foodsaver_has_bezirk',
 			['foodsaver_id' => $this->regionMember['id'], 'bezirk_id' => $this->region['id']]
 		);
-		$this->tester->assertEquals(count($fsIds), $updates[0], 'Wrong number of inserts!');
-		$this->tester->assertEquals(1, $updates[1], 'Wrong number of deletions!');
+		$this->tester->assertEquals(0, $updates[0], 'Wrong number of inserts!');
+		$this->tester->assertEquals(2, $updates[1], 'Wrong number of deletions!');
 	}
 }
