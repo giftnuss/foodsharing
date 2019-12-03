d1ee265a7f56d2d3776aae7ad9201fc493ae3d9e
Johannes Franz <jofranz90@gmail.com>
2019-12-03 12:16:00 +0100

    Replace session check with $this->session->may()

--- src/Modules/Map/MapControl.php
+++ src/Modules/Map/MapControl.php
@@ -21,7 +21,7 @@ class MapControl extends Control
 		$this->pageHelper->addTitle($this->translationHelper->s('map'));
 		$this->setTemplate('map');
 
-		if ($this->session->id()) {
+		if ($this->session->may()) {
 			$center = $this->mapGateway->getFoodsaverLocation($this->session->id());
 		}
 		$this->pageHelper->addContent($this->view->mapControl(), CNT_TOP);
