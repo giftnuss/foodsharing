7b4d0ef140feca59f497cd4cf98e31de6c19683b
Johannes Franz <jofranz90@gmail.com>
2019-12-11 00:24:03 +0100

    alter sql column default values for migration strategy

--- migrations/incremental-20191107-add-email-forum-settings.sql
+++ migrations/incremental-20191107-add-email-forum-settings.sql
@@ -1,4 +1,4 @@
---ALTER TABLE `fs_theme_follower` CHANGE `infotype` `email_notification` BIT NOT NULL DEFAULT 0 AFTER bell_notification;
+--ALTER TABLE `fs_theme_follower` CHANGE `infotype` `email_notification` BIT NOT NULL DEFAULT '0' AFTER theme_id;
 
-
-ALTER TABLE `fs_theme_follower` ADD `bell_notification` BIT NOT NULL DEFAULT 0 AFTER `infotype`;
+ALTER TABLE `fs_theme_follower` ADD `bell_notification` BIT NOT NULL DEFAULT 1 AFTER `infotype`;
+ALTER TABLE `fs_theme_follower` CHANGE `infotype` `infotype` TINYINT(1) NOT NULL DEFAULT 0;
