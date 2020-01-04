910802d85d70ccfe2ef230d8718559719ab0d2a0
Johannes Franz <jofranz90@gmail.com>
2020-01-04 01:41:39 +0100

    Make sure not to allow this tool for workgroups

--- src/Modules/Foodsaver/FoodsaverControl.php
+++ src/Modules/Foodsaver/FoodsaverControl.php
@@ -60,8 +60,8 @@ class FoodsaverControl extends Control
 	 */
 	public function index()
 	{
-		if (isset($_GET['bid'])) {
-			$regionId = $_GET['bid'];
+		$regionId = $_GET['bid'];
+		if (isset($regionId) && $this->session->isAmbassadorForRegion($regionId, false, false)) {
 			if ($this->regionPermissions->mayHandleFoodsaverRegionMenu($regionId) && $region = $this->regionGateway->getRegion($regionId)) {
 				if ($foodsavers = $this->foodsaverGateway->getFoodsaversByRegion($regionId)) {
 					$this->pageHelper->addBread('Foodsaver', '/?page=foodsaver&bid=' . $regionId);
