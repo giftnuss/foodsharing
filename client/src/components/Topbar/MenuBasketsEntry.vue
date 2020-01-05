2d06562f41ae66d3141b503836fb2b7ad501bc07
Rafael Muselmann <rafael.muselmann@gmx.de>
2020-01-05 14:03:46 +0100

    Simplify menu basket entry (HTML, CSS).

--- client/src/components/Topbar/MenuBasketsEntry.vue
+++ client/src/components/Topbar/MenuBasketsEntry.vue
@@ -3,68 +3,56 @@
     :href="$url('basket', basket.id)"
     class="list-group-item list-group-item-action"
   >
-    <div class="row">
-      <div class="col-2 pl-2">
-        <img src="/img/basket.png">
-      </div>
-      <div class="col-10">
-        <div class="text-truncate">
-          <strong>{{ basket.description }}</strong>
-        </div>
+    <div class="row basket-entry">
+      <img
+        style="height:100%"
+        src="/img/basket.png"
+      >
+      <div class="basket-info-box">
+        <h5 class="basket-title text-truncate">{{ basket.description }}</h5>
         <small
           v-if="!basket.requests.length"
           class="text-muted"
         >
           {{ $i18n('basket.no_requests') }}
         </small>
-        <h5
-          v-if="basket.requests.length"
-          class="text-muted mb-1 pl-2"
-        >
-          {{ $i18n('basket.requested_by') }}
-        </h5>
         <div
           v-if="basket.requests.length"
-          class="requests list-group"
+          class="text-muted"
         >
-          <b-list-group>
-            <b-list-group-item
-              v-for="req in basket.requests"
-              :key="req.id"
-              @click.prevent="openChat(req.user.id, $event)"
-              href="#"
-              class="flex-column align-items-start"
-              style="padding: 5px;"
-            >
-              <div
-                class="d-flex w-100 align-items-center"
-                style="height:30px;"
-              >
-                <avatar
-                  :url="req.user.avatar"
-                  :size="35"
-                  :sleep-status="req.user.sleepStatus"
-                />
-                <div
-                  class="d-flex"
-                  style="flex-flow:column; margin: 0 auto 0 5px;"
-                >
-                  <span>{{ req.user.name }}</span>
-                  <small>{{ req.time | dateDistanceInWords }}</small>
-                </div>
-                <a
-                  v-b-tooltip
-                  @click.prevent.stop="openRemoveDialog(req.user.id, $event)"
-                  :title="$i18n('basket.request_close')"
-                  href="#"
-                  class="m-1 btn btn-sm btn-secondary"
-                >
-                  <i class="fas fa-times" />
-                </a>
-              </div>
-            </b-list-group-item>
-          </b-list-group>
+          {{ $i18n('basket.requested_by') }}
         </div>
+        <b-list-group
+          v-if="basket.requests.length"
+          class="requests"
+        >
+          <b-list-group-item
+            v-for="req in basket.requests"
+            :key="req.id"
+            @click.prevent="openChat(req.user.id, $event)"
+            href="#"
+            class="d-flex w-100 align-items-center"
+          >
+            <avatar
+              :url="req.user.avatar"
+              :size="35"
+              :sleep-status="req.user.sleepStatus"
+            />
+            <div class="d-flex flex-column basket-entry-request">
+              <span>{{ req.user.name }}</span>
+              <small>{{ req.time | dateDistanceInWords }}</small>
+            </div>
+            <b-button
+              v-b-tooltip
+              :title="$i18n('basket.request_close')"
+              @click.prevent.stop="openRemoveDialog(req.user.id, $event)"
+              size="sm"
+              variant="secondary"
+            >
+              <i class="fas fa-times" />
+            </b-button>
+          </b-list-group-item>
+        </b-list-group>
       </div>
     </div>
   </a>
@@ -96,37 +84,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-h5 {
-    font-size: 0.8em;
-}
-.requests {
-    h6 {
-        font-size: 1em;
-        font-weight: bold;
-
+.basket-entry {
+  flex-flow: row;
+  .basket-title {
+    margin-bottom: 0;
+  }
+  .basket-info-box {
+    margin: 0 10px;
+    width: 100%;
+    overflow: hidden;
+    .list-group-item {
+      padding: 5px;
     }
-    div {
-        font-size: 0.9em;
-    }
-
+  }
+  .basket-entry-request {
+    margin: 0 auto 0 5px;
+  }
 }
-.request .btn {
-    padding: 0 0.2rem;
-    position: absolute;
-    right: 1.2em;
-    top: -0.3em;
-}
-.request .hover {
-    display: none;
-}
-
-.request:hover .nhover {
-    display: none;
-}
-.request:hover .hover {
-    display: block;
-}
-.nowrap {
-    white-space: nowrap;
+.requests div {
+  font-size: 0.9em;
 }
 </style>
