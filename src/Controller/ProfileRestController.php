756f083faa19fb3d25011c86955fe7bf67d2270c
Paul-Gabriel Müller <panschk@googlemail.com>
2020-01-12 17:33:51 +0100

    add use statement for use FOS\RestBundle\Controller\Annotations as Rest;

--- src/Controller/ProfileRestController.php
+++ src/Controller/ProfileRestController.php
@@ -5,6 +5,7 @@ namespace Foodsharing\Controller;
 use Foodsharing\Lib\Session;
 use Foodsharing\Modules\Profile\ProfileGateway;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Foodsharing\Permissions\ReportPermissions;
