ef56cd46f0c344bbf232650082818b208c841103
Dominik Hayon <dominikhayon@gmail.com>
2020-02-08 22:09:38 +0100

    Removed trailing spaces in BlogGateway

--- src/Modules/Blog/BlogGateway.php
+++ src/Modules/Blog/BlogGateway.php
@@ -75,14 +75,14 @@ final class BlogGateway extends BaseGateway
 				b.`body`,
 				b.`time`,
 				b.`picture`,
-				CONCAT(fs.name," ",fs.nachname) AS fs_name	
+				CONCAT(fs.name," ",fs.nachname) AS fs_name
 			FROM
 				`fs_blog_entry` b,
-				`fs_foodsaver` fs	
+				`fs_foodsaver` fs
 			WHERE
-				b.foodsaver_id = fs.id	
+				b.foodsaver_id = fs.id
 			AND
-				b.`active` = 1	
+				b.`active` = 1
 			AND
 				b.id = :fs_id',
 		[':fs_id' => $id]);
@@ -94,7 +94,7 @@ final class BlogGateway extends BaseGateway
 
 		return $this->db->fetchAll(
 			'
-			SELECT 	 	
+			SELECT
 				b.`id`,
 				b.`name`,
 				b.`time`,
@@ -103,16 +103,16 @@ final class BlogGateway extends BaseGateway
 				b.`teaser`,
 				b.`time`,
 				b.`picture`,
-				CONCAT(fs.name," ",fs.nachname) AS fs_name		
-			FROM 
+				CONCAT(fs.name," ",fs.nachname) AS fs_name
+			FROM
 				`fs_blog_entry` b,
-				`fs_foodsaver` fs		
-			WHERE 
-				b.foodsaver_id = fs.id				
+				`fs_foodsaver` fs
+			WHERE
+				b.foodsaver_id = fs.id
 			AND
-				b.`active` = 1		
-			ORDER BY 
-				b.`id` DESC				
+				b.`active` = 1
+			ORDER BY
+				b.`id` DESC
 			LIMIT :page,10',
 			[':page' => $page]
 		);
@@ -131,9 +131,9 @@ final class BlogGateway extends BaseGateway
 						`time`,
 						UNIX_TIMESTAMP(`time`) AS time_ts,
 						`active`,
-						`bezirk_id`		
-			FROM 		`fs_blog_entry`	
-			' . $not . '	
+						`bezirk_id`
+			FROM 		`fs_blog_entry`
+			' . $not . '
 			ORDER BY `id` DESC');
 	}
 
@@ -156,8 +156,8 @@ final class BlogGateway extends BaseGateway
 			`body`,
 			`time`,
 			UNIX_TIMESTAMP(`time`) AS time_ts,
-			`picture`			
-			FROM 		`fs_blog_entry`			
+			`picture`
+			FROM 		`fs_blog_entry`
 			WHERE 		`id` = :fs_id',
 			[':fs_id' => $id]
 		);
