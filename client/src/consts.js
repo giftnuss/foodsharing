2b2853579392b4d4209afc812d74fae3e6fb7541
David Thulke <davidthulke@gmail.com>
2020-03-03 22:49:44 +0100

    update maptiler key

--- client/src/consts.js
+++ client/src/consts.js
@@ -1,6 +1,6 @@
 export const AVATAR_DEFAULT = '/img/130_q_avatar.png'
 export const GROUP_PICTURE_DEFAULT = '/img/groups.png'
 
-export const MAP_TILES_URL = 'https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=VySMwJDBkeFxJfh6LqhQ'
+export const MAP_TILES_URL = 'https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=4XAweTbMsSogokoKnOvn'
 export const MAP_ATTRIBUTION = 'Tiles <a href="https://www.maptiler.com/copyright/">© MapTiler</a>, Map data <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>'
 export const MAP_GEOCODING_ATTRIBUTION = 'Geocoding by <a href="https://photon.komoot.de">Komoot Photon</a>, ' + MAP_ATTRIBUTION
