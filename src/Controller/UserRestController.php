ec0ed0a12e392bfc9c4ef5ae7717c7c21914f2c6
Peter Tönnies <peter.toennies@gmail.com>
2020-03-19 19:21:40 +0100

    fix run after merge

--- src/Controller/UserRestController.php
+++ src/Controller/UserRestController.php
@@ -5,13 +5,13 @@ namespace Foodsharing\Controller;
 use Foodsharing\Lib\Session;
 use Foodsharing\Modules\Foodsaver\FoodsaverGateway;
 use Foodsharing\Modules\Login\LoginGateway;
+use Foodsharing\Permissions\ProfilePermissions;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcher;
 use Mobile_Detect;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Foodsharing\Permissions\ProfilePermissions;
 
 class UserRestController extends AbstractFOSRestController
 {
