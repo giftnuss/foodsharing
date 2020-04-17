5cc6110886299c66ab2c387fcae1fb897e68e27b
Caluera <caluera@protonmail.com>
2020-04-17 06:03:09 +0200

    removed unnecessary database migration

--- migrations/incremental-20200403-about-me-intern.sql
+++ migrations/incremental-20200403-about-me-intern.sql
@@ -1,4 +1,3 @@
 ALTER TABLE fs_foodsaver ADD `about_me_intern` TEXT DEFAULT NULL;
 ALTER TABLE fs_foodsaver_archive ADD `about_me_intern` TEXT DEFAULT NULL;
-ALTER TABLE fs_foodsaver_archive2 ADD `about_me_intern` TEXT DEFAULT NULL;
 
