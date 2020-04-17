05423efedf3728a400cad763638beb02bf985db6
Caluera <caluera@protonmail.com>
2020-04-17 13:46:03 +0200

    redo bugfix in seed command

--- src/Dev/SeedCommand.php
+++ src/Dev/SeedCommand.php
@@ -111,7 +111,7 @@ class SeedCommand extends Command implements CustomCommandInterface
 		$userbot = $I->createAmbassador($password, [
 			'email' => 'userbot@example.com',
 			'name' => 'Bot',
-			'bezirk_id' => $bezirk1,
+			'bezirk_id' => $region1,
 			'about_me_intern' => 'hello!'
 		]);
 		$this->writeUser($userbot, $password, 'ambassador');
