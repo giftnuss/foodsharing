giftnuss/foodsharing

commit f10ed9b91af3c45387cc0f59bac0d6df7a9e4c50

Thread.vue: matching api and component keys

src/Modules/Region/components/Thread.vue
@@ -4,7 +4,7 @@
       v-if="isLoading"
       class="disabledLoading">
       <div class="card-header text-white bg-primary">
-        {{ title || '...' }}
+        {{ name || '...' }}
       </div>
       <div class="card-body p-5"/>
     </div>
@@ -15,7 +15,7 @@
       <div class="card-header text-white bg-primary">
         <div class="row">
           <div class="col text-truncate ml-2 pt-1 font-weight-bold">
-            {{ title }}
+            {{ name }}
           </div>
           <div class="col text-right">
             <a
@@ -24,9 +24,10 @@
               {{ $i18n(isFollowing ? 'forum.unfollow' : 'forum.follow') }}
             </a>
             <a
+              v-if="mayModerate"
               class="btn btn-sm btn-secondary"
               @click="toggleStickyness">
-              {{ $i18n(isSticked ? 'forum.unstick' : 'forum.stick') }}
+              {{ $i18n(isSticky ? 'forum.unstick' : 'forum.stick') }}
             </a>
           </div>
         </div>
@@ -88,23 +89,31 @@ export default {
   },
   data () {
     return {
-      regionId: null,
-      title: '',
+      name: '',
       posts: [],
-      loadingPosts: [],
+      
+      isSticky: true,
+      isActive: true,
+      mayModerate: false,
       isFollowing: true,
-      isSticked: true,
-      mayChangeStickyness: true,
 
       isLoading: false,
+      loadingPosts: [],
       errorMessage: null
     }
   },
   async created () {
     try {
       this.isLoading = true
-      let thread = await api.getThread(this.id)
-      Object.assign(this, thread)
+      let res = (await api.getThread(this.id)).data
+      Object.assign(this, {
+        name: res.name,
+        posts: res.posts,
+        isSticky: res.isSticky,
+        isActive: res.isActive,
+        mayModerate: res.mayModerate,
+        isFollowing: res.isFollowing
+      })
       this.isLoading = false
     } catch (err) {
       this.isLoading = false
@@ -189,8 +198,8 @@ export default {
       }
     },
     async toggleStickyness () {
-      let targetState = !this.isSticked
-      this.isSticked = targetState
+      let targetState = !this.isSticky
+      this.isSticky = targetState
       try {
         if (targetState) {
           await api.stickThread(this.id)
@@ -199,7 +208,7 @@ export default {
         }
       } catch (err) {
         // failed? undo it
-        this.isSticked = !targetState
+        this.isSticky = !targetState
         pulseError(i18n('error_unexpected'))
       }
     },

src/Modules/Region/components/ThreadPost.vue
@@ -8,9 +8,9 @@
         <div class="col-3 avatarSide text-center">
           <a :href="$url('profile', author.id)">
             <Avatar
-              :url="author.avatar.url"
-              :size="author.avatar.size"
+              :url="author.avatar"
               :sleep-status="author.sleepStatus"
+              size="130"
             />
           </a>
           <a
@@ -24,7 +24,7 @@
       <div class="card-footer">
         <div class="row">
           <div class="col-4 text-muted">
-            <small>{{ time | dateFormat('dddd, Do MMM YYYY, HH:mm [Uhr]') }}</small>
+            <small>{{ createdAt | dateFormat('dddd, Do MMM YYYY, HH:mm [Uhr]') }}</small>
           </div>
           <div class="col text-right">
             <ThreadPostActions
@@ -51,9 +51,10 @@ import conv from '@/conv'
 export default {
   components: { Avatar, ThreadPostActions },
   props: {
+    id: { type: Number, default: null },
     body: { type: String, default: '' },
     author: { type: Object, default: () => ({ avatar: {} }) },
-    time: { type: Date, default: null },
+    createdAt: { type: Date, default: null },
     reactions: { type: Array, default: () => [] },
     mayEdit: { type: Boolean, default: false },
     mayDelete: { type: Boolean, default: false },

src/Modules/Region/components/ThreadPostActions.vue
@@ -4,15 +4,15 @@
     <!-- emoji buttons & selector -->
     <span class="emojis">
       <span
-        v-for="emoji in reactions"
-        v-if="emoji.count"
-        :key="emoji.key">
+        v-for="reaction in reactions"
+        v-if="reaction.count"
+        :key="reaction.key">
         <a
-          :class="['btn', 'btn-sm', (emoji.mine ? 'btn-primary' : 'btn-secondary')]"
-          :title="emoji.key"
-          @click="toggleReaction(emoji.key)"
+          :class="['btn', 'btn-sm', (reaction.mine ? 'btn-primary' : 'btn-secondary')]"
+          :title="reaction.key"
+          @click="toggleReaction(reaction.key)"
         >
-          {{ emoji.count }}x <Emoji :name="emoji.key" />
+          {{ reaction.users.length }}x <Emoji :name="reaction.key" />
         </a>
       </span>
       <b-dropdown

views/pages/Region/forum/showThread.twig
@@ -19,10 +19,6 @@
     elementName: "thread", 
     props: {
         id: thread.id,
-    },
-    initialData: {
-        title: thread.name,
-        posts: posts
     }
 } %}
 

commit 0dfd24c6ffb3d6d85b8bf11cfe529be5eded0a8e

Fix vue error when replying in forum
the dummy post had the wrong property defined, so an invalid date was
being computed from createdAt===null instead of the "now" value.

this was broken since f10ed9b91af3c45387cc0f59bac0d6df7a9e4c50

src/Modules/Region/components/Thread.vue
@@ -278,7 +278,7 @@ export default {
       this.errorMessage = null
       const dummyPost = {
         id: -1,
-        time: new Date(),
+        createdAt: new Date(),
         body: body,
         reactions: {},
         author: {