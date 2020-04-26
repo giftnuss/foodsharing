f5e1e87ad4a40eba3f02f7a5e27becf381df6c14
Alexander Simm <alexandersimm2@googlemail.com>
2020-04-26 11:16:33 +0200

    client side function for registration email test

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -1,4 +1,4 @@
-import { post, remove } from './base'
+import { get, post, remove } from './base'
 
 export function login (email, password) {
   return post('/user/login', { email, password })
@@ -6,4 +6,8 @@ export function login (email, password) {
 
 export function deleteUser (id) {
   return remove(`/user/${id}`)
+}
+
+export async function testRegisterEmail(email) {
+  return get('user/validemail', { email: email })
 }
