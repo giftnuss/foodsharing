7dfe9b735fd1ff2189741926e95db86eaa68a2fb
flukx <felix.h@hilsky.de>
2020-04-28 14:25:15 +0200

    add checking database entries in region tests

--- tests/api/RegionApiCest.php
+++ tests/api/RegionApiCest.php
@@ -15,9 +15,17 @@ class RegionApiCest
 	public function canJoinRegion(\ApiTester $I)
 	{
 		$I->login($this->user['email']);
+		$I->dontSeeInDatabase('fs_foodsaver_has_bezirk', [
+			'foodsaver_id' => $this->user['id'],
+			'bezirk_id' => $this->region['id']
+			]);
 		$I->sendPOST('api/region/' . $this->region['id'] . '/join');
 		$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
 		$I->seeResponseIsJson();
+		$I->seeInDatabase('fs_foodsaver_has_bezirk', [
+			'foodsaver_id' => $this->user['id'],
+			'bezirk_id' => $this->region['id']
+			]);
 	}
 
 	public function joinNotExistingRegionIs404(\ApiTester $I)
@@ -26,6 +34,10 @@ class RegionApiCest
 		$I->sendPOST('api/region/999999999/join');
 		$I->seeResponseCodeIs(\Codeception\Util\HttpCode::NOT_FOUND);
 		$I->seeResponseIsJson();
+		$I->dontSeeInDatabase('fs_foodsaver_has_bezirk', [
+			'foodsaver_id' => $this->user['id'],
+			'bezirk_id' => 999999999
+			]);
 	}
 
 	public function canNotJoinRegionAsFoodsharer(\ApiTester $I)
@@ -35,6 +47,10 @@ class RegionApiCest
 		$I->sendPOST('api/region/' . $this->region['id'] . '/join');
 		$I->seeResponseCodeIs(\Codeception\Util\HttpCode::FORBIDDEN);
 		$I->seeResponseIsJson();
+		$I->dontSeeInDatabase('fs_foodsaver_has_bezirk', [
+			'foodsaver_id' => $this->user['id'],
+			'bezirk_id' => $this->region['id']
+			]);
 	}
 
 	public function canJoinRegionTwice(\ApiTester $I)
@@ -44,6 +60,7 @@ class RegionApiCest
 		$I->sendPOST('api/region/' . $this->region['id'] . '/join');
 		$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
 		$I->seeResponseIsJson();
+		// database entry is not interesting, already tested that in other test
 	}
 
 	public function canNotLeaveRegionWithoutLogin(\ApiTester $I)
@@ -51,6 +68,8 @@ class RegionApiCest
 		$I->sendPOST('api/region/' . $this->region['id'] . '/leave');
 		$I->seeResponseCodeIs(\Codeception\Util\HttpCode::FORBIDDEN);
 		$I->seeResponseIsJson();
+		// cannot test whether leaving did not change database since
+		// there is no user to look at
 	}
 
 	public function canLeaveRegionWithoutJoiningFirst(\ApiTester $I)
@@ -59,6 +78,10 @@ class RegionApiCest
 		$I->sendPOST('api/region/' . $this->region['id'] . '/leave');
 		$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
 		$I->seeResponseIsJson();
+		$I->dontSeeInDatabase('fs_foodsaver_has_bezirk', [
+			'foodsaver_id' => $this->user['id'],
+			'bezirk_id' => $this->region['id']
+			]);
 	}
 
 	public function canLeaveRegion(\ApiTester $I)
@@ -66,12 +89,18 @@ class RegionApiCest
 		$I->login($this->user['email']);
 		$I->sendPOST('api/region/' . $this->region['id'] . '/join');
 		$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
-		$I->seeResponseIsJson();
 
 		$I->login($this->user['email']);
+		// second login necessary since the list of regions of the current
+		// user are saved in the session and not updated there by the
+		// join request. So without relogin the leave would fail.
 		$I->sendPOST('api/region/' . $this->region['id'] . '/leave');
 		$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
 		$I->seeResponseIsJson();
+		$I->dontSeeInDatabase('fs_foodsaver_has_bezirk', [
+			'foodsaver_id' => $this->user['id'],
+			'bezirk_id' => $this->region['id']
+			]);
 	}
 
 	public function canNotLeaveDifferentRegionThanJoined(\ApiTester $I)
@@ -81,5 +110,9 @@ class RegionApiCest
 		$I->sendPOST('api/region/999999/leave');
 		$I->seeResponseCodeIs(\Codeception\Util\HttpCode::FORBIDDEN);
 		$I->seeResponseIsJson();
+		$I->seeInDatabase('fs_foodsaver_has_bezirk', [
+			'foodsaver_id' => $this->user['id'],
+			'bezirk_id' => $this->region['id']
+			]);
 	}
 }
