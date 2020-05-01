57249c5b1e3c12276fae7c1099396ccdef7b9bfb
Jano <jano.paetzold@posteo.de>
2020-05-01 21:41:43 +0200

    Re-enable @typescript-eslint/no-dynamic-delete check
    I changed the implementation to use a Map instead of object keys exactly
    for being able to safely delete entries. So this check doesn't conflict
    with our code any more.

--- chat/.eslintrc.js
+++ chat/.eslintrc.js
@@ -15,6 +15,5 @@ module.exports = {
     "@typescript-eslint/semi": ["error", "always"], // implicit semicolons can cause confusion, as not every new line is a semicolon, but some are
     "@typescript-eslint/indent": ["error", 4], // larger indentations make the code easier to oversee, and in object-oriented languages, you shouldn't indent so deeply. That's why we also use indentations of 4 for our PHP code
     "@typescript-eslint/strict-boolean-expressions": "off", // explicit boolean comparisons are good, but JavaScript is not made for this. If you always have to list all possibilities of something being null/undefined/NaN/whatever JS offers, you're more likely to produce errors and you code becomes less readable
-    "@typescript-eslint/no-dynamic-delete": "off" // Unfortunately, we need this, as we use a Map-like object to organize our
   }
 };
