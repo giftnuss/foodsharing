12d624894c9a70d9a82d86f93dd10f10e9e8ac30
Jano <jano.paetzold@posteo.de>
2020-05-02 23:56:06 +0200

    Adjust chat tests to the new REST api
    The tests pass now. See how green they are?

--- chat/test.ts
+++ chat/test.ts
@@ -7,6 +7,7 @@ import * as io from 'socket.io-client';
 import { serialize } from 'cookie';
 import * as superagent from 'superagent';
 import Socket = SocketIOClient.Socket;
+import {Response} from "superagent";
 
 const HTTP_URL = 'http://127.0.0.1:1338';
 const WS_URL = 'http://127.0.0.1:1337';
@@ -150,98 +151,24 @@ test('unregistering', t => {
 test('can send a message', t => {
     t.timeoutAfter(10000);
     t.plan(1);
-    sendMessage({
-        c: 'ignored',
-        a: 'ignored',
-        m: 'ignored',
-        o: 'ignored'
-    }, (err: any) => {
+    sendMessage([1, 2, 3], 'foo', 'bar', {}, (err: any) => {
         t.error(err, 'does not error');
-    });
-});
-
-test('can send and receive a message', t => {
-    t.timeoutAfter(10000);
-    t.plan(3);
-    const sessionId = 'somesessionid';
-    const socket = connect(t, sessionId);
-    socket.on('connect', () => {
-        socket.emit('register');
-        socket.on('someapp', (data: any) => {
-            t.equal(data.m, 'foo', 'passed m param');
-            t.equal(data.o, 'bar', 'passed o param');
-        });
-        sendMessage({
-            c: sessionId,
-            // used as channel to recv on
-            a: 'someapp',
-            // m and o passed as payload
-            m: 'foo',
-            o: 'bar'
-        }, (err: any) => {
-            t.error(err, 'does not error');
-        });
-    });
-});
-
-test('can send and receive a message for multiple clients', t => {
-    t.timeoutAfter(10000);
-    t.plan(7);
-    const sessionId = 'somesessionid';
-    const socket1 = connect(t, sessionId);
-    const socket2 = connect(t, sessionId);
-    const socket3 = connect(t, sessionId);
-    register(socket1, () => {
-        register(socket2, () => {
-            register(socket3, () => {
-                socket1.on('someapp', (data: any) => {
-                    t.equal(data.m, 'foo', 'passed m param');
-                    t.equal(data.o, 'bar', 'passed o param');
-                });
-                socket2.on('someapp', (data: any) => {
-                    t.equal(data.m, 'foo', 'passed m param');
-                    t.equal(data.o, 'bar', 'passed o param');
-                });
-                socket3.on('someapp', (data: any) => {
-                    t.equal(data.m, 'foo', 'passed m param');
-                    t.equal(data.o, 'bar', 'passed o param');
-                });
-                sendMessage({
-                    c: sessionId,
-                    // used as channel to recv on
-                    a: 'someapp',
-                    // m and o passed as payload
-                    m: 'foo',
-                    o: 'bar'
-                }, (err: any) => {
-                    t.error(err, 'does not error');
-                });
-            });
-        });
     });
 });
 
 test('can send to php users', t => {
     t.timeoutAfter(10000);
-    t.plan(4);
+    t.plan(3);
     const sessionId = randomString.generate();
     const userId = 1;
-    addPHPSessionToRedis(userId, sessionId, (err: any) => {
-        t.error(err);
+    addPHPSessionToRedis(userId, sessionId, () => {
         const socket = connect(t, sessionId);
-        socket.on('someapp', (data: any) => {
-            t.equal(data.m, 'foo', 'passed m param');
-            t.equal(data.o, 'bar', 'passed o param');
+        socket.on('some-app', (data: any) => {
+            t.equal(data.m, 'some-method', 'passed m param');
+            t.deepEqual(data.o, { someKey: 'some-payload' }, 'passed o param');
         });
         register(socket, () => {
-            sendMessage({
-                u: userId,
-                // used as channel to recv on
-                a: 'someapp',
-                // m and o passed as payload
-                m: 'foo',
-                o: 'bar'
-            }, (err: any) => {
+            sendMessage([userId], 'some-app', 'some-method', { someKey: 'some-payload' }, (err: any) => {
                 t.error(err, 'does not error');
             });
         });
@@ -250,25 +177,17 @@ test('can send to php users', t => {
 
 test('can send to api users', t => {
     t.timeoutAfter(10000);
-    t.plan(4);
+    t.plan(3);
     const sessionId = randomString.generate();
     const userId = 2;
     addAPISessionToRedis(userId, sessionId, (err: any) => {
-        t.error(err);
         const socket = connect(t, sessionId, 'sessionid'); // django session cookie name
-        socket.on('someapp', (data: any) => {
-            t.equal(data.m, 'foo', 'passed m param');
-            t.equal(data.o, 'bar', 'passed o param');
+        socket.on('some-app', (data: any) => {
+            t.equal(data.m, 'some-method', 'passed m param');
+            t.deepEqual(data.o, { someKey: 'some-payload' }, 'passed o param');
         });
         register(socket, () => {
-            sendMessage({
-                u: userId,
-                // used as channel to recv on
-                a: 'someapp',
-                // m and o passed as payload
-                m: 'foo',
-                o: 'bar'
-            }, (err: any) => {
+            sendMessage([userId], 'some-app', 'some-method', { someKey: 'some-payload' }, (err: any) => {
                 t.error(err, 'does not error');
             });
         });
@@ -276,33 +195,29 @@ test('can send to api users', t => {
 });
 
 test('works with two connections per user', t => {
-    t.timeoutAfter(10000);
+    t.timeoutAfter(20000);
 
     const client1 = connect(t, 'test-1-user-1');
     const client2 = connect(t, 'test-1-user-1');
+    addPHPSessionToRedis(1, 'test-1-user-1', () => {});
 
-    t.plan(2 * 2 + 1);
+    t.plan(5); // checkEvent contains 2 checks and will be invoked for 2 sessions + the status code check
     const checkEvent = (ev: any): void => {
         t.deepEqual(ev.m, 'some-method');
-        t.deepEqual(ev.o, 'some-payload');
+        t.deepEqual(ev.o, { someKey: 'some-payload' });
     };
-    client1.on('some-event', checkEvent);
-    client2.on('some-event', checkEvent);
+    client1.on('some-app', checkEvent);
+    client2.on('some-app', checkEvent);
 
     register(client1, () => {
         register(client2, () => {
-            const query = {
-                c: 'test-1-user-1', // client
-                a: 'some-event', // app a.k.a channel/event
-                m: 'some-method', // method
-                o: 'some-payload' // options a.k.a payload
-            };
-            superagent.get(HTTP_URL).query(query).end((error, res) => {
-                if (error) {
-                    t.error(error);
-                }
-                t.equal(res.status, 200);
-            });
+            sendMessage([1], 'some-app', 'some-method', { someKey: 'some-payload' },
+                (error, res) => {
+                    if (error) {
+                        t.error(error);
+                    }
+                    t.equal(res.status, 200);
+                });
         });
     });
 });
@@ -314,28 +229,28 @@ test('does not send to other users', t => {
     const user1 = connect(t, 'test-2-user-1');
     const user2 = connect(t, 'test-2-user-2');
 
+    addPHPSessionToRedis(1, 'test-2-user-1', () => {});
+    addPHPSessionToRedis(2, 'test-2-user-2', () => {});
+
     t.plan(1 + 1);
     user1.on('some-event', () => t.pass('user 1 has received `some-event`'));
     user2.on('some-event', () => t.fail('user 2 has received `some-event`'));
 
     register(user1, () => {
         register(user2, () => {
-            const query = {
-                c: 'test-2-user-1', // client
-                a: 'some-event', // app a.k.a channel/event
-                m: 'some-method', // method
-                o: 'some-payload' // options a.k.a payload
-            };
-            superagent.get(HTTP_URL).query(query).end((error, res) => {
-                if (error) {
-                    t.error(error);
+            sendMessage([1], 'some-event', 'some-method', { foo: 'bar' },
+                (error, res) => {
+                    if (error) {
+                        t.error(error);
+                    }
+                    t.equal(res.status, 200);
+                    setTimeout(() => t.end(), 100); // 100ms window to see if user2 receives event...
                 }
-                t.equal(res.status, 200);
-                setTimeout(() => t.end(), 100); // 100ms window to see if user2 receives event...
-            });
+            );
         });
     });
 });
+
 function connect (t: Test, sessionId: string, cookieName = 'PHPSESSID'): Socket {
     // const manager = new Manager(WS_URL, { extraHeaders: { cookie: serialize(cookieName, sessionId) } });
     const socket = io.connect(WS_URL, {
@@ -362,15 +277,12 @@ function register (socket: Socket, callback: () => any): void {
     }
 }
 
-function sendMessage (params: object, callback: (error?: any) => any): void {
-    superagent.get(HTTP_URL).query(params).end((err) => {
-        if (err) return callback(err);
-        callback();
-    });
+function sendMessage (userIds: number[], channel: string, method: string, options: object, callback: (error: any, res: Response) => any): void {
+    superagent.post(HTTP_URL + `/user/${userIds.join('-')}/${channel}/${method}`).send(options).end(callback);
 }
 
 function fetchStats (callback: (error: any, stats?: {connections: number, registrations: number, sessions: number}) => any): void {
-    superagent.get(`${HTTP_URL}/stats`).end((error, response) => {
+    superagent.get(HTTP_URL + '/stats').end((error, response) => {
         if (error) {
             callback(error);
         }
@@ -383,20 +295,18 @@ function fetchStats (callback: (error: any, stats?: {connections: number, regist
 }
 
 function addPHPSessionToRedis (userId: number, sessionId: string, callback: (error: any) => any): void {
-    redisClient
-        .set(`PHPREDIS_SESSION:${sessionId}`, 'foo')
-        .catch(error => callback(error));
-    redisClient
-        .sadd(`php:user:${userId}:sessions`, sessionId)
+    redisClient.set(`PHPREDIS_SESSION:${sessionId}`, 'foo')
+        .then(async () =>
+            await redisClient.sadd(`php:user:${userId}:sessions`, sessionId)
+        ).then(callback)
         .catch(error => callback(error));
 }
 
 function addAPISessionToRedis (userId: number, sessionId: string, callback: (error: any) => any): void {
-    redisClient
-        .set(`:1:django.contrib.sessions.cache${sessionId}`, 'foo')
-        .catch(error => callback(error));
-    redisClient
-        .sadd(`api:user:${userId}:sessions`, sessionId)
+    redisClient.set(`:1:django.contrib.sessions.cache${sessionId}`, 'foo')
+        .then(async ()  =>
+            await redisClient.sadd(`api:user:${userId}:sessions`, sessionId)
+        ).then(callback)
         .catch(error => callback(error));
 }
 
