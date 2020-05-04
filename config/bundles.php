e8a4d77eee861513e15428ba8e8644c25ddef5a9
Matthias Larisch <mail@matthias-larisch.de>
2020-05-04 12:38:40 +0200

    Use twig bundle instead of manually adding twig
    Previously, we added the twig components we needed manually and wired
    them in services.yml. TwigBundle provides a better integration of twig
    into symfony as it uses a "well-known" configuration and loading
    mechanism etc., other bundles might rely on it.
    This Commit converts our configuration into the same for twig bundle.
    Downside might be a slightly increased twig stack as we now have all
    parts of TwigBundle available.

--- config/bundles.php
+++ config/bundles.php
@@ -2,6 +2,7 @@
 
 return [
 	Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+	Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 	FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 	JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 	Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true]
