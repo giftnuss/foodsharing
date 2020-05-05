195c7efd76835250ad780757ba04d5ef2cd7861b
Matthias Larisch <mail@matthias-larisch.de>
2020-05-05 10:24:26 +0200

    fix symfony configuration

--- config/bundles.php
+++ config/bundles.php
@@ -3,8 +3,8 @@
 return [
 	Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
 	Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-	FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 	JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 	Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true],
-	Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true]
+	Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+	FOS\RestBundle\FOSRestBundle::class => ['all' => true]
 ];
