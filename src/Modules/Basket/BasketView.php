abefa434d8ab16ba72814ba37ece0790ba1ffb7c
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-05-08 22:13:46 +0200

    Food basket: Replace pure-grid with flexbox

--- src/Modules/Basket/BasketView.php
+++ src/Modules/Basket/BasketView.php
@@ -77,11 +77,11 @@ class BasketView extends View
 		$page = new vPage(
 			$this->translationHelper->s('basket') . ' #' . $basket['id'], '
 		
-		<div class="pure-g">
-		    <div class="pure-u-1 pure-u-md-1-3">
+		<div class="fbasket-wrap">
+		    <div class="fbasket-pic">
 				' . $this->pageImg($basket['picture'] ?? '') . '	
 			</div>
-		    <div class="pure-u-1 pure-u-md-2-3">
+		    <div class="fbasket-desc">
 				<p>' . nl2br($basket['description']) . '</p>
 			</div>
 		</div>
@@ -125,10 +125,8 @@ class BasketView extends View
 		$page = new vPage(
 			$this->translationHelper->s('basket') . ' #' . $basket['id'], '
 		
-		<div class="pure-g">
-		    <div class="pure-u-1 pure-u-md-2-3">
-				<p>' . $this->translationHelper->s('basket_picked_up') . '</p>
-			</div>
+		<div>
+			<p>' . $this->translationHelper->s('basket_picked_up') . '</p>
 		</div>
 		'
 		);
