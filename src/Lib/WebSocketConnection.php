de1cf5a486e25141d12cf6a31f60b65c45e24356
Jano <jano.paetzold@posteo.de>
2020-05-09 14:34:19 +0200

    Adjust WebSocketConnection to the once again altered API

--- src/Lib/WebSocketConnection.php
+++ src/Lib/WebSocketConnection.php
@@ -23,19 +23,19 @@ class WebSocketConnection
 
 	public function sendSock(int $fsid, string $app, string $method, array $options): void
 	{
-		$url = SOCK_URL . 'user/' . $fsid . '/' . $app . '/' . $method;
+		$url = SOCK_URL . 'users/' . $fsid . '/' . $app . '/' . $method;
 		$this->guzzle->post($url, [RequestOptions::JSON => $options]);
 	}
 
 	public function sendSockMulti(array $fsids, string $app, string $method, array $options): void
 	{
-		$url = SOCK_URL . 'user/' . join('-', $fsids) . '/' . $app . '/' . $method;
+		$url = SOCK_URL . 'users/' . join(',', $fsids) . '/' . $app . '/' . $method;
 		$this->guzzle->post($url, [RequestOptions::JSON => $options]);
 	}
 
 	public function isUserOnline(int $fsid): bool
 	{
-		$userIsOnline = $this->guzzle->get(SOCK_URL . 'user/' . $fsid . '/is-online')->getBody()->getContents();
+		$userIsOnline = $this->guzzle->get(SOCK_URL . 'users/' . $fsid . '/is-online')->getBody()->getContents();
 
 		if ($userIsOnline === 'true') {
 			return true;
