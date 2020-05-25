05f00bfe931eb130ffe123ef151210f6c612e9ac
Matthias Larisch <mail@matthias-larisch.de>
2020-05-25 08:08:14 +0200

    Update test privacy* dates

--- tests/_support/Helper/Foodsharing.php
+++ tests/_support/Helper/Foodsharing.php
@@ -89,8 +89,8 @@ class Foodsharing extends \Codeception\Module\Db
 			'anschrift' => $this->faker->streetName,
 			'handy' => $this->faker->phoneNumber,
 			'active' => 1,
-			'privacy_policy_accepted_date' => '2035-05-24 10:24:53',
-			'privacy_notice_accepted_date' => '2035-05-24 18:25:28',
+			'privacy_policy_accepted_date' => '2020-05-16 00:09:33',
+			'privacy_notice_accepted_date' => '2018-05-24 18:25:28',
 			'token' => uniqid('', true)
 		], $extra_params);
 		$params['password'] = password_hash($pass, PASSWORD_ARGON2I, [
