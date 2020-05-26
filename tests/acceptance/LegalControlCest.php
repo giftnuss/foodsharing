d7841fc34a2e4ec7a253aacd9be7aa575bcd29d7
Matthias Larisch <mail@matthias-larisch.de>
2020-05-26 20:19:24 +0200

    Fix and extend LegalControl tests

--- tests/acceptance/LegalControlCest.php
+++ tests/acceptance/LegalControlCest.php
@@ -2,6 +2,7 @@
 
 class LegalControlCest
 {
+	/* ToDo: The legal tests fiddle with data. If one of them fails, likely all other tests afterwards fail as well as the database is not reset in between. */
 	public function testGivenIAmNotLoggedInThenTheLegalPageShowsThePrivacyPolicyWithoutAskingForConsent(AcceptanceTester $I)
 	{
 		$I->amOnPage('/?page=legal');
@@ -14,11 +15,11 @@ class LegalControlCest
 		$foodsharer = $I->createFoodsharer();
 		$I->login($foodsharer['email']);
 		$lastModified = $I->updateThePrivacyPolicyDate();
-		$I->amOnPage('/?page=dashbord');
+		$I->amOnPage('/?page=dashboard');
 		$I->see('Datenschutzerklärung');
 		$I->checkOption('#legal_form_privacyPolicyAcknowledged');
 		$I->click('Einstellungen übernehmen');
-		$I->amOnPage('/?page=dashbord');
+		$I->waitForText('schön, dass Du dabei bist und Dich gegen die Lebensmittelverschwendung');
 		$I->dontSee('Datenschutzerklärung');
 		$I->logout();
 		$I->resetThePrivacyPolicyDate($lastModified);
@@ -29,7 +30,7 @@ class LegalControlCest
 		$foodsharer = $I->createFoodsharer();
 		$I->login($foodsharer['email']);
 		$lastModified = $I->updateThePrivacyPolicyDate();
-		$I->amOnPage('/?page=dashbord');
+		$I->amOnPage('/?page=dashboard');
 		$I->see('Datenschutzerklärung');
 		$I->uncheckOption('#legal_form_privacyPolicyAcknowledged');
 		$I->click('Einstellungen übernehmen');
@@ -43,7 +44,7 @@ class LegalControlCest
 		$foodsharer = $I->createFoodsharer();
 		$I->login($foodsharer['email']);
 		$lastModified = $I->updateThePrivacyPolicyDate();
-		$I->amOnPage('/?page=dashbord');
+		$I->amOnPage('/?page=dashboard');
 		$I->see('Datenschutzerklärung');
 		$I->click('Ich möchte meinen Account löschen.');
 		$I->amOnPage('/?page=settings&sub=deleteaccount');
@@ -53,32 +54,46 @@ class LegalControlCest
 
 	public function testGivenIAmLoggedInAndHaveARoleHigherThanOneThenICanAcceptThePrivacyPolicyAndNotice(AcceptanceTester $I)
 	{
-		$orga = $I->createOrga();
-		$I->login($orga['email']);
-		$lastModified = $I->updateThePrivacyPolicyDate();
-		$I->amOnPage('/?page=dashbord');
+		$user = $I->createAmbassador();
+		$I->login($user['email']);
+		$lastModifiedpp = $I->updateThePrivacyPolicyDate();
+		$I->amOnPage('/?page=dashboard');
 		$I->see('Datenschutzerklärung');
+		$I->dontSeeCheckboxIsChecked('#legal_form_privacyPolicyAcknowledged');
+		$I->seeOptionIsSelected('#legal_form_privacyNoticeAcknowledged', 'Ich stimme zu');
 		$I->checkOption('#legal_form_privacyPolicyAcknowledged');
-		$I->checkOption('#legal_form_privacyNoticeAcknowledged', 'Ich stimme zu.');
 		$I->click('Einstellungen übernehmen');
-		$I->amOnPage('/?page=dashbord');
+		$I->waitForText('Dein Stammbezirk ist');
 		$I->dontSee('Datenschutzerklärung');
+
+		$I->seeInDatabase('fs_foodsaver', ['id' => $user['id'], 'rolle' => 3]);
+		$lastModifiedpn = $I->updateThePrivacyNoticeDate();
+		$I->amOnPage('/?page=dashboard');
+		$I->seeCheckboxIsChecked('#legal_form_privacyPolicyAcknowledged');
+		$I->seeOptionIsSelected('#legal_form_privacyNoticeAcknowledged', 'Ich stimme nicht zu');
+		$I->selectOption('#legal_form_privacyNoticeAcknowledged', 'Ich stimme zu');
+		$I->click('Einstellungen übernehmen');
+		$I->waitForText('Dein Stammbezirk ist');
+		$I->dontSee('Datenschutzerklärung');
+		$I->seeInDatabase('fs_foodsaver', ['id' => $user['id'], 'rolle' => 3]);
 		$I->logout();
-		$I->resetThePrivacyPolicyDate($lastModified);
+		$I->resetThePrivacyPolicyDate($lastModifiedpp);
+		$I->resetThePrivacyNoticeDate($lastModifiedpn);
 	}
 
 	public function testGivenIAmLoggedInAndAHaveRoleHigherThanOneThenICanDegradeToFoodsaver(AcceptanceTester $I)
 	{
-		$orga = $I->createOrga();
-		$I->login($orga['email']);
+		$user = $I->createAmbassador();
+		$I->login($user['email']);
 		$lastModified = $I->updateThePrivacyPolicyDate();
-		$I->amOnPage('/?page=dashbord');
+		$I->amOnPage('/?page=dashboard');
 		$I->see('Datenschutzerklärung');
 		$I->checkOption('#legal_form_privacyPolicyAcknowledged');
-		$I->checkOption('#legal_form_privacyNoticeAcknowledged', 'Ich stimme nicht zu und möchte zum Foodsaver herabgestuft werden');
+		$I->selectOption('#legal_form_privacyNoticeAcknowledged', 'Ich stimme nicht zu und möchte zum Foodsaver herabgestuft werden');
 		$I->click('Einstellungen übernehmen');
-		$I->amOnPage('/?page=dashbord');
+		$I->amOnPage('/?page=dashboard');
 		$I->dontSee('Datenschutzerklärung');
+		$I->seeInDatabase('fs_foodsaver', ['id' => $user['id'], 'rolle' => 1]);
 		$I->logout();
 		$I->resetThePrivacyPolicyDate($lastModified);
 	}
