56e3ad2ee02bcd5fe42889d347cc3a1197f87310
Matthias Larisch <mail@matthias-larisch.de>
2020-06-16 08:50:36 +0200

    Add phinx as separate project
    This allows phinx to be run in production without being installed, as
    that might have performance implications.
    Phinx has been reconfigured to use the configuration directly as
    importing the symfony container out of another project triggered
    recompilation that I wanted to avoid.

--- phinx.php
+++ phinx.php
@@ -1,13 +1,6 @@
 <?php
 
-use Foodsharing\Kernel;
-
-require __DIR__ . '/vendor/autoload.php';
 include __DIR__ . '/config.inc.php';
-
-$kernel = new Kernel('dev', false);
-$kernel->boot();
-$pdo = $kernel->getContainer()->get('PDO');
 
 return
 [
@@ -19,11 +12,11 @@ return
 		'default_migration_table' => 'phinxlog',
 		'default_environment' => 'symfony',
 		'symfony' => [
-			/* silence some PHP notices that are generated otherwise */
-			'user' => 'unused',
-			'pass' => 'unused',
-			'name' => 'foodsharing',
-			'connection' => $pdo,
+			'adapter' => 'mysql',
+			'host' => DB_HOST,
+			'user' => DB_USER,
+			'pass' => DB_PASS,
+			'name' => DB_DB,
 			'charset' => 'utf8mb4',
 			'collation' => 'utf8mb4_unicode_ci'
 		]
