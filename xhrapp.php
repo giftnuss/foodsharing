8ad4d7b99e2ce87e368dab2a20f9248cfbc7c5c4
fridtjof <fridtjof@das-labor.org>
2020-07-02 01:29:28 +0200

    new xhrapp.php derived from restApi.php, it is now a standard symfony entry point! (with a small hack to make routing work)

---
+++ xhrapp.php
@@ -0,0 +1,37 @@
+<?php
+
+use Foodsharing\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require __DIR__ . '/vendor/autoload.php';
+require_once 'config.inc.php';
+
+// The check is to ensure we don't use .env in production
+$env = $_SERVER['FS_ENV'] ?? getenv('FS_ENV') ?? 'dev';
+$debug = (bool)($_SERVER['APP_DEBUG'] ?? ('prod' !== $env));
+
+if ($debug) {
+	umask(0000);
+
+	Debug::enable();
+}
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+	Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
+	Request::setTrustedHosts(explode(',', $trustedHosts));
+}
+
+// hack to make Symfony routing work for this
+// because routing looks at a URL's path info (see Request::getPathInfo),
+// we prepend the file name again, so the path info is '/xhrapp.php' instead of '/'
+$_SERVER['REQUEST_URI'] = '/xhrapp.php' . $_SERVER['REQUEST_URI'];
+
+$kernel = new Kernel($env, $debug);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
