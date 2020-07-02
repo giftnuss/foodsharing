97e6281d8095041b51b15bdb140c7aa81317e707
fridtjof <fridtjof@das-labor.org>
2020-07-02 02:20:58 +0200

    XhrController.php: clean up

--- src/Entrypoint/XhrController.php
+++ src/Entrypoint/XhrController.php
@@ -63,13 +63,6 @@ class XhrController extends AbstractController
 		InfluxMetrics $influxdb,
 		XhrMethods $xhr
 	): Response {
-		require 'includes/setup.php';
-		require_once 'config.inc.php';
-
-		/* @var Container $container */
-		global $container;
-		$container = initializeContainer();
-
 		$session->initIfCookieExists();
 
 		if (isset($g_page_cache)) {
@@ -77,12 +70,13 @@ class XhrController extends AbstractController
 			$cache->lookup();
 		}
 
+		global $g_lang;
 		require_once 'lang/DE/de.php';
 
 		$action = $request->query->get('f');
 
 		if ($action === null) {
-			exit();
+			return new Response(Response::HTTP_BAD_REQUEST);
 		}
 
 		if (!in_array($action, XhrController::csrf_whitelist) && !$session->isValidCsrfHeader()) {
@@ -95,41 +89,40 @@ class XhrController extends AbstractController
 		}
 
 		$func = 'xhr_' . $action;
-		if (method_exists($xhr, $func)) {
-			$response = new Response();
-
-			$influxdb->addPageStatData(['controller' => $func]);
-
-			ob_start();
-			echo $xhr->$func($_GET);
-			$page = ob_get_contents();
-			ob_end_clean();
-
-			if ($page === XhrResponses::PERMISSION_DENIED) {
-				$response->setProtocolVersion('1.1');
-				$response->setStatusCode(Response::HTTP_FORBIDDEN);
-				$response->setContent('Permission denied');
-
-				return $response;
-			}
-
-			if (is_string($page) && (!trim($page) || $page[0] == '{' || $page[0] == '[')) {
-				// just assume it's JSON, to prevent the browser from interpreting it as
-				// HTML, which could result in XSS possibilities
-				$response->headers->set('Content-Type', 'application/json');
-			}
-			/*
-			 * check for page caching
-			*/
-			if (isset($cache) && $cache->shouldCache()) {
-				$cache->cache($page);
-			}
-
-			$response->setContent($page);
-
-			return $response;
-		} else {
+		if (!method_exists($xhr, $func)) {
 			return new Response(Response::HTTP_BAD_REQUEST);
 		}
+
+		$response = new Response();
+
+		$influxdb->addPageStatData(['controller' => $func]);
+
+		ob_start();
+		echo $xhr->$func($_GET);
+		$page = ob_get_contents();
+		ob_end_clean();
+
+		if ($page === XhrResponses::PERMISSION_DENIED) {
+			$response->setProtocolVersion('1.1');
+			$response->setStatusCode(Response::HTTP_FORBIDDEN);
+			$response->setContent('Permission denied');
+
+			return $response;
+		}
+
+		if (is_string($page) && (!trim($page) || $page[0] == '{' || $page[0] == '[')) {
+			// just assume it's JSON, to prevent the browser from interpreting it as
+			// HTML, which could result in XSS possibilities
+			$response->headers->set('Content-Type', 'application/json');
+		}
+
+		// check for page caching
+		if (isset($cache) && $cache->shouldCache()) {
+			$cache->cache($page);
+		}
+
+		$response->setContent($page);
+
+		return $response;
 	}
 }
