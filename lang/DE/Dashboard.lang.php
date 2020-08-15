92bb24d2d2134950ec0062b77fdc19162e7c3181
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-08-15 12:34:01 +0200

    &7 Dashboard: Translation refactoring

--- lang/DE/Dashboard.lang.php
+++ lang/DE/Dashboard.lang.php
@@ -1,17 +1,9 @@
 <?php
 global $g_lang;
-$g_lang['next_dates'] = 'Nächste Abholtermine';
 $g_lang['updates'] = 'Letzte Aktivitäten';
 $g_lang['welcome'] = 'Willkommen {name}! Schön, dass Du da bist!';
 $g_lang['no_saved_food'] = 'Viel Spaß beim Retten!';
 $g_lang['saved_food'] = 'Du hast schon {weight} Kilos gerettet!';
-$g_lang['new_foodbaskets'] = 'Neueste Essenskörbe';
-$g_lang['close_foodbaskets'] = 'Essenskörbe in Deiner Nähe';
-$g_lang['new_basket'] = 'Neuer Essenskorb';
-$g_lang['all_baskets'] = 'Alle Körbe auf der Karte';
-$g_lang['events_headline'] = 'Nächste Events';
-$g_lang['event_headline'] = 'Nächstes Event';
-$g_lang['you_were_invited'] = 'Du wurdest eingeladen';
 $g_lang['please_check_address'] = 'Bitte überprüfe Deine Adresse! Die Koordinaten konnten nicht ermittelt werden.';
 $g_lang['your_home_district_is'] = 'Dein Stammbezirk ist ';
 $g_lang['please_choose_your_home_district'] = 'Bitte wähle einen Stammbezirk aus.';
