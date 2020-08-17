1b69a96992d312a5276fbd223159cfce6374fe40
Alexander Simm <alexandersimm2@googlemail.com>
2020-08-17 22:11:35 +0200

    client side voting API

---
+++ client/src/api/voting.js
@@ -0,0 +1,34 @@
+import { get, post, put, remove } from './base'
+import { formatISO } from 'date-fns'
+
+export async function getPoll (pollId) {
+  return get(`/polls/${pollId}`)
+}
+
+export async function listPolls (groupId) {
+  return get(`/groups/${groupId}/polls`)
+}
+
+export async function createPoll (regionId, name, description, startDate, endDate, scope, type, options, notifyVoters) {
+  return post('/polls', {
+    regionId: regionId,
+    name: name,
+    description: description,
+    startDate: formatISO(startDate),
+    endDate: formatISO(endDate),
+    scope: scope,
+    type: type,
+    options: options,
+    notifyVoters: notifyVoters
+  })
+}
+
+export async function deletePoll (pollId) {
+  return remove(`/polls/${pollId}`)
+}
+
+export async function vote (pollId, options) {
+  return put(`/polls/${pollId}/vote`, {
+    options: options
+  })
+}
