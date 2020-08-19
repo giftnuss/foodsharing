207dacd923aac89c74a6090303ce86908bd7a70b
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-08-19 11:13:20 +0200

    &7 MailboxXhr translations

--- lang/DE/Mailbox.lang.php
+++
@@ -1,5 +0,0 @@
-<?php
-global $g_lang;
-$g_lang['email_error'] = 'Die E-Mail-Adresse des Empfängers ist nicht korrekt.';
-$g_lang['send_success'] = 'E-Mail wurde erfolgreich versendet.';
-$g_lang['wrong_file'] = 'Dieser Dateityp ist nicht erlaubt';
