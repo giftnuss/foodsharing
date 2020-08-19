253b27ab784d98987c68446086f24da1300ff84d
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-08-19 11:13:20 +0200

    &7 BusinessCard translations

--- lang/DE/BusinessCard.lang.php
+++ lang/DE/BusinessCard.lang.php
@@ -1,15 +1,3 @@
 <?php
 global $g_lang;
-$g_lang['bcard_generator'] = 'Visitenkartengenerator';
-$g_lang['bcard_generator_desc'] = 'Hier kannst Du Dir Deine persönlichen Visitenkarten erstellen.';
-$g_lang['bot_for'] = 'Botschafter für {var}';
-$g_lang['sm_for'] = 'Betriebsverantwortlicher für {var}';
-$g_lang['fs_for'] = 'Foodsaver für {var}';
-$g_lang['bot_for_w'] = 'Botschafterin für {var}';
-$g_lang['sm_for_w'] = 'Betriebsverantwortliche für {var}';
-$g_lang['fs_for_w'] = 'Foodsaverin für {var}';
 $g_lang['opt'] = 'Optionen';
-$g_lang['should_choose_option'] = 'Du musst eine Option wählen';
-$g_lang['download_card'] = 'Visitenkarten herunterladen';
-$g_lang['generation_success'] = 'Visitenkarten erfolgreich generiert';
-$g_lang['opt_desc'] = 'Wähle hier, für welchen Bezirk bzw. für welche Rolle Du Deine Visitenkarte erstellen möchtest!';
--- src/Modules/BusinessCard/BusinessCardView.php
+++ src/Modules/BusinessCard/BusinessCardView.php
@@ -8,7 +8,10 @@ class BusinessCardView extends View
 {
 	public function top()
 	{
-		return $this->topbar('Deine foodsharing Visitenkarte', 'Hier easy generieren, ausdrucken und ausschneiden...', '<img src="/img/bcard.png" />');
+		return $this->topbar($this->translator->trans('bcard.card'),
+			$this->translator->trans('bcard.claim'),
+			'<img src="/img/bcard.png" />'
+		);
 	}
 
 	public function optionForm($selectedData)
@@ -16,26 +19,22 @@ class BusinessCardView extends View
 		$this->pageHelper->addJs('
 			$("#optionen-form .input-wrapper:last").hide();
 			
-			$("#opt").on("change", function(){
+			$("#opt").on("change", function () {
 				$("#optionen-form").trigger("submit");
 			});
 				
-			$("#optionen-form").on("submit", function(ev){
+			$("#optionen-form").on("submit", function (ev) {
 				ev.preventDefault();
-				if($("#opt").val() == "")
-				{
-					pulseError(\'' . $this->sanitizerService->jsSafe($this->translationHelper->s('should_choose_option')) . '\');
-				}
-				else
-				{
+				if ($("#opt").val() == "") {
+					pulseError(\'' . $this->translator->trans('bcard.choose') . '\');
+				} else {
 					goTo("/?page=bcard&sub=makeCard&opt=" + $("#opt").val());
 				}
 				
-			});		
-		');
+			});');
 
-		return $this->v_utils->v_quickform($this->translationHelper->s('options'), [
-				$this->v_utils->v_form_select('opt', ['desc' => $this->translationHelper->s('opt_desc'), 'values' => $selectedData])
-			], ['submit' => 'Visitenkarten erstellen']);
+		return $this->v_utils->v_quickform($this->translator->trans('bcard.actions'), [
+			$this->v_utils->v_form_select('opt', ['desc' => $this->translator->trans('bcard.desc'), 'values' => $selectedData]),
+		], ['submit' => $this->translator->trans('bcard.generate')]);
 	}
 }
