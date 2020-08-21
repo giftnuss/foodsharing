135968e19c598a24c6d28a14547f2739874c9ca9
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-08-21 13:51:01 +0200

    &7 WorkGroup translations (and some flattening)

--- lang/DE/WorkGroup.lang.php
+++ lang/DE/WorkGroup.lang.php
@@ -1,11 +1,10 @@
 <?php
 global $g_lang;
+$g_lang['member'] = 'Mitglieder';
+$g_lang['leader'] = 'Gruppen-Admins';
 
 $g_lang['apply_type'] = 'Wer kann sich für diese Gruppe eintragen?';
 $g_lang['banana_count'] = 'Wie viele Vertrauensbananen braucht ein Mitglied?';
 $g_lang['fetch_count'] = 'Wie viele Abholungen sollte ein Bewerber gemacht haben?';
 $g_lang['week_num'] = 'Seit wie vielen Wochen sollte ein Bewerber schon dabei sein?';
 $g_lang['report_num'] = 'Dürfen sich auch Foodsaver mit Meldungen von Regelverletzungen bewerben?';
-$g_lang['member'] = 'Mitglieder';
-$g_lang['leader'] = 'Gruppen-Admins';
-$g_lang['contact-disclaimer'] = 'Durch Senden einer Nachricht an diese Arbeitsgruppe sehen alle Administratoren der Arbeitsgruppe Deine E-Mail-Adresse, welche Du auf Deinem foodsharing-Profil angegeben hast. Dir wird eine Kopie dieser Nachricht als E-Mail zugeschickt.';
