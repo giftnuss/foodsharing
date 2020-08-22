b44aad5485bed167e0d529c432cbdff53593ee19
Alexander Simm <alexandersimm2@googlemail.com>
2020-08-22 16:04:27 +0200

    more fine grained permissions for creating polls

--- src/Permissions/VotingPermissions.php
+++ src/Permissions/VotingPermissions.php
@@ -5,18 +5,32 @@ namespace Foodsharing\Permissions;
 use DateTime;
 use Exception;
 use Foodsharing\Lib\Session;
+use Foodsharing\Modules\Core\DBConstants\Region\Type;
+use Foodsharing\Modules\Region\RegionGateway;
 use Foodsharing\Modules\Voting\DTO\Poll;
 use Foodsharing\Modules\Voting\VotingGateway;
 
 final class VotingPermissions
 {
-	private $session;
-	private $votingGateway;
+	private Session $session;
+	private VotingGateway $votingGateway;
+	private RegionGateway $regionGateway;
 
-	public function __construct(Session $session, VotingGateway $votingGateway)
+	/**
+	 * In regions with one of these types all verified foodsavers are allowed to create polls. In other regions only
+	 * ambassadors are allowed to do so.
+	 */
+	private const LOWER_REGIONS = [Type::CITY, Type::DISTRICT, Type::REGION, Type::PART_OF_TOWN, Type::BIG_CITY,
+		Type::WORKING_GROUP];
+
+	public function __construct(
+		Session $session,
+		VotingGateway $votingGateway,
+		RegionGateway $regionGateway)
 	{
 		$this->session = $session;
 		$this->votingGateway = $votingGateway;
+		$this->regionGateway = $regionGateway;
 	}
 
 	public function maySeeResults(Poll $poll): bool
@@ -40,7 +54,16 @@ final class VotingPermissions
 
 	public function mayCreatePoll(int $regionId): bool
 	{
-		return $this->session->mayBezirk($regionId);
+		if (!$this->session->mayBezirk($regionId) || !$this->session->isVerified()) {
+			return false;
+		}
+
+		$type = $this->regionGateway->getType($regionId);
+		if (in_array($type, self::LOWER_REGIONS)) {
+			return true;
+		} else {
+			return $this->session->isAdminFor($regionId);
+		}
 	}
 
 	public function mayDeletePoll(int $pollId): bool
