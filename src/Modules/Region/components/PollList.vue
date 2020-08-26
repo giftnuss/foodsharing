bbb8607b6848d8250533209d48281e4fd2bca5eb
Alexander Simm <alexandersimm2@googlemail.com>
2020-08-26 23:14:38 +0200

    updated poll list design

--- src/Modules/Region/components/PollList.vue
+++ src/Modules/Region/components/PollList.vue
@@ -1,68 +1,80 @@
 <template>
   <div class="container bootstrap">
     <div class="card mb-3 rounded">
-      <div
-        class="card-header text-white bg-primary"
-      >
-        {{ $i18n('terminology.polls') }}
-        <span>
-          ({{ polls.length }})
-        </span>
+      <div class="card-header text-white bg-primary">
+        {{ $i18n('polls.ongoing') }}
       </div>
 
-      <b-table
-        :fields="fields"
-        :items="polls"
-        :current-page="currentPage"
-        :per-page="perPage"
-        :sort-compare="compare"
-        small
-        hover
-        responsive
-        class="foto-table"
+      <div
+        v-if="mayCreatePoll"
+        class="p-1"
       >
-        <template v-slot:cell(title)="row">
-          <a
-            :href="$url('poll', row.item.id)"
+        <b-link
+          :href="$url('pollNew', regionId)"
+          class="btn btn-sm btn-secondary btn-block"
+        >
+          {{ $i18n('polls.new_poll') }}
+        </b-link>
+      </div>
+
+      <div
+        v-for="poll in ongoingPolls"
+        :key="poll.id"
+      >
+        <b-link :href="$url('poll', poll.id)">
+          <span
+            class="calendar m-1"
           >
-            {{ row.item.name }}
-          </a>
-        </template>
-        <template v-slot:cell(startDate)="row">
-          <a
-            :href="$url('poll', row.item.id)"
+            <span class="month">{{ formatDate(convertDate(poll.endDate.date), 'MMMM') }}</span>
+            <span class="day">{{ formatDate(convertDate(poll.endDate.date), 'd') }}</span>
+          </span>
+          <div class="title mt-2">
+            <b>{{ poll.name }}</b>
+          </div>
+          <div class="mt-2">
+            {{ convertDate(poll.startDate.date) | dateFormat }} - {{ convertDate(poll.endDate.date) | dateFormat }}
+          </div>
+          <span class="clear" />
+        </b-link>
+      </div>
+    </div>
+
+    <div
+      v-if="endedPolls.length > 0"
+      class="card mb-3 rounded"
+    >
+      <div class="card-header text-white bg-primary">
+        {{ $i18n('polls.ended') }}
+      </div>
+      <div class="card-body">
+        <ul>
+          <li
+            v-for="poll in endedPolls"
+            :key="poll.id"
+            class="mb-2"
           >
-            {{ new Date(row.item.startDate.date) | dateFormat('full-short') }}
-          </a>
-        </template>
-        <template v-slot:cell(endDate)="row">
-          <a
-            :href="$url('poll', row.item.id)"
-          >
-            {{ new Date(row.item.endDate.date) | dateFormat('full-short') }}
-          </a>
-        </template>
-      </b-table>
-      <div class="float-right p-1 pr-3">
-        <b-pagination
-          v-model="currentPage"
-          :total-rows="polls.length"
-          :per-page="perPage"
-          class="my-0"
-        />
+            <b-link
+              :href="$url('poll', poll.id)"
+            >
+              <b>{{ poll.name }}</b>
+              <div>{{ $i18n('poll.ended_at') }} {{ convertDate(poll.endDate.date) | dateFormat }}</div>
+            </b-link>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { BTable, BPagination, VBTooltip } from 'bootstrap-vue'
+import { BLink } from 'bootstrap-vue'
 import { dateFormat, optimizedCompare } from '@/utils'
-import isPast from 'date-fns/isPast'
+import { isBefore, isAfter, format } from 'date-fns'
+import dateFnsParseISO from 'date-fns/parseISO'
 
 export default {
-  components: { BTable, BPagination },
-  directives: { VBTooltip, dateFormat },
+  components: { BLink },
+  directives: { dateFormat, format },
   props: {
     regionId: {
       type: Number,
@@ -71,40 +83,44 @@ export default {
     polls: {
       type: Array,
       default: () => []
+    },
+    mayCreatePoll: {
+      type: Boolean,
+      default: true
     }
   },
   data () {
     return {
       currentPage: 1,
-      perPage: 20,
-      fields: [
-        {
-          key: 'title',
-          label: this.$i18n('polls.title'),
-          sortable: true,
-          class: 'align-middle'
-        }, {
-          key: 'startDate',
-          sortable: true,
-          label: this.$i18n('polls.start_date'),
-          class: 'align-middle'
-        }, {
-          key: 'endDate',
-          sortable: true,
-          label: this.$i18n('polls.end_date'),
-          class: 'align-middle'
-        }
-      ]
+      perPage: 20
+    }
+  },
+  computed: {
+    ongoingPolls: function () {
+      return this.polls.filter(p => this.isOngoingPoll(p))
+    },
+    endedPolls: function () {
+      return this.polls.filter(p => !this.isOngoingPoll(p))
     }
   },
   methods: {
     compare: optimizedCompare,
-    isActivePoll (poll) {
-      return isPast(poll.startDate) && !isPast(poll.endDate)
+    isOngoingPoll (poll) {
+      return isAfter(this.convertDate(poll.endDate.date), new Date()) &&
+        isBefore(this.convertDate(poll.startDate.date), new Date())
+    },
+    convertDate (date) {
+      return dateFnsParseISO(date)
+    },
+    formatDate (date, formatStr) {
+      return format(date, formatStr)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.btn {
+  width: 200px;
+}
 </style>
