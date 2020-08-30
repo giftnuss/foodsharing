4a619792ab98c1f7f44fe8e662df0b40d01fde3e
Kristijan <kristijan80@web.de>
2020-08-30 16:01:48 +0200

    replaced numbers with constant for content ids

--- src/Modules/Core/DBConstants/Content/ContentId.php
+++ src/Modules/Core/DBConstants/Content/ContentId.php
@@ -13,8 +13,35 @@ namespace Foodsharing\Modules\Core\DBConstants\Content;
 class ContentId
 {
 	public const PARTNER_PAGE_10 = 10;
+	public const QUIZ_DESCRIPTION_PAGE_12 = 12;
+	public const QUIZ_FAILED_PAGE_13 = 13;
+	public const QUIZ_CONFIRM_FS_PAGE_14 = 14;
+	public const QUIZ_CONFIRM_SM_PAGE_15 = 15;
+	public const QUIZ_CONFIRM_AMB_PAGE_16 = 16;
+	public const QUIZ_START_PAGE_17 = 17;
+	public const QUIZ_POPUP_PAGE_18 = 18;
+	public const QUIZ_FAILED_FS_TRY_1_PAGE_19 = 19;
+	public const QUIZ_FAILED_FS_TRY_2_PAGE_20 = 20;
+	public const QUIZ_FAILED_FS_TRY_3_PAGE_21 = 21;
+	public const QUIZ_FAILED_SM_TRY_1_PAGE_22 = 22;
+	public const QUIZ_FAILED_SM_TRY_2_PAGE_23 = 23;
+	public const QUIZ_FAILED_SM_TRY_3_PAGE_24 = 24;
+	public const QUIZ_FAILED_AMB_TRY_1_PAGE_25 = 25;
+	public const QUIZ_FAILED_AMB_TRY_2_PAGE_26 = 26;
+	public const QUIZ_FAILED_AMB_TRY_3_PAGE_27 = 27;
+	public const QUIZ_REMARK_PAGE_33 = 33;
+	public const QUIZ_POPUP_SM_PAGE_34 = 34;
+	public const QUIZ_POPUP_AMB_PAGE_35 = 35;
+	public const QUIZ_POPUP_AMB_LAST_PAGE_36 = 36;
+
 	public const SUPPORT_FOODSHARING_PAGE_42 = 42;
 	public const PRIVACY_POLICY_CONTENT = 28;
+	public const START_MAIN_AU_PAGE_37 = 37;
+	public const START_MAIN_DE_PAGE_38 = 38;
+	public const TEAM_HEADER_PAGE_39 = 39;
+	public const START_BETA_PAGE_48 = 48;
+	public const TEAM_ACTIVE_PAGE_53 = 53;
+	public const TEAM_FORMER_ACTIVE_PAGE_54 = 54;
 	public const PRIVACY_NOTICE_CONTENT = 64;
 	public const BROADCAST_MESSAGE = 51;
 }
--- src/Permissions/ContentPermissions.php
+++ src/Permissions/ContentPermissions.php
@@ -3,15 +3,43 @@
 namespace Foodsharing\Permissions;
 
 use Foodsharing\Lib\Session;
+use Foodsharing\Modules\Core\DBConstants\Content\ContentId;
 use Foodsharing\Modules\Core\DBConstants\Region\RegionIDs;
 
 final class ContentPermissions
 {
 	private $session;
 
-	private $PR_PARTNER_AND_TEAM_CONTENT_IDS = ['id' => [10, 39, 54, 53]];
-	private $QUIZ_PAGES = ['id' => [12, 13, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 33, 35, 36]];
-	private $START_PAGE = ['id' => [38, 48]];
+	private $PR_PARTNER_AND_TEAM_CONTENT_IDS = ['id' => [ContentId::PARTNER_PAGE_10,
+														 ContentId::TEAM_HEADER_PAGE_39,
+														 ContentId::TEAM_ACTIVE_PAGE_53,
+														 ContentId::TEAM_FORMER_ACTIVE_PAGE_54
+														]];
+
+	private $QUIZ_CONTENT_IDS = ['id' => [ContentId::QUIZ_DESCRIPTION_PAGE_12,
+											ContentId::QUIZ_FAILED_PAGE_13,
+											ContentId::QUIZ_CONFIRM_FS_PAGE_14,
+											ContentId::QUIZ_CONFIRM_SM_PAGE_15,
+											ContentId::QUIZ_CONFIRM_AMB_PAGE_16,
+											ContentId::QUIZ_START_PAGE_17,
+											ContentId::QUIZ_POPUP_PAGE_18,
+											ContentId::QUIZ_FAILED_FS_TRY_1_PAGE_19,
+											ContentId::QUIZ_FAILED_FS_TRY_2_PAGE_20,
+											ContentId::QUIZ_FAILED_FS_TRY_3_PAGE_21,
+											ContentId::QUIZ_FAILED_SM_TRY_1_PAGE_22,
+											ContentId::QUIZ_FAILED_SM_TRY_2_PAGE_23,
+											ContentId::QUIZ_FAILED_SM_TRY_3_PAGE_24,
+											ContentId::QUIZ_FAILED_AMB_TRY_1_PAGE_25,
+											ContentId::QUIZ_FAILED_AMB_TRY_2_PAGE_26,
+											ContentId::QUIZ_FAILED_AMB_TRY_3_PAGE_27,
+											ContentId::QUIZ_REMARK_PAGE_33,
+											ContentId::QUIZ_POPUP_SM_PAGE_34,
+											ContentId::QUIZ_POPUP_AMB_PAGE_35,
+											ContentId::QUIZ_POPUP_AMB_LAST_PAGE_36]];
+
+	private $START_CONTENT_IDS = ['id' => [ContentId::START_MAIN_AU_PAGE_37,
+										   ContentId::START_MAIN_DE_PAGE_38,
+										   ContentId::START_BETA_PAGE_48]];
 
 	public function __construct(Session $session)
 	{
@@ -33,7 +61,7 @@ final class ContentPermissions
 		}
 
 		if ($this->session->isAdminFor(RegionIDs::QUIZ_AND_REGISTRATION_WORK_GROUP)) {
-			return $this->QUIZ_PAGES;
+			return $this->QUIZ_CONTENT_IDS;
 		}
 		if ($this->session->isAdminFor(RegionIDs::PR_PARTNER_AND_TEAM_WORK_GROUP)) {
 			return $this->PR_PARTNER_AND_TEAM_CONTENT_IDS;
@@ -49,7 +77,13 @@ final class ContentPermissions
 			return true;
 		}
 		if ($this->session->isAdminFor(RegionIDs::QUIZ_AND_REGISTRATION_WORK_GROUP)) {
-			return in_array($id, $this->QUIZ_PAGES['id']);
+			return in_array($id, $this->QUIZ_CONTENT_IDS['id']);
+		}
+		if ($this->session->isAdminFor(RegionIDs::PR_PARTNER_AND_TEAM_WORK_GROUP)) {
+			return in_array($id, $this->PR_PARTNER_AND_TEAM_CONTENT_IDS['id']);
+		}
+		if ($this->session->isAdminFor(RegionIDs::PR_START_PAGE)) {
+			return in_array($id, $this->START_CONTENT_IDS['id']);
 		}
 	}
 
