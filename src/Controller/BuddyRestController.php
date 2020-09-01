f7930aff81056e7f6186d012623f16f76673f2c7
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-09-01 16:41:02 +0200

    &6 Annotate BuddyRestController

--- src/Controller/BuddyRestController.php
+++ src/Controller/BuddyRestController.php
@@ -8,6 +8,7 @@ use Foodsharing\Modules\Buddy\BuddyTransactions;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class BuddyRestController extends AbstractFOSRestController
@@ -39,7 +40,7 @@ class BuddyRestController extends AbstractFOSRestController
 	 *
 	 * @Rest\Put("buddy/{userId}", requirements={"userId" = "\d+"})
 	 */
-	public function sendRequestAction(int $userId)
+	public function sendRequestAction(int $userId): Response
 	{
 		if (!$this->session->id()) {
 			throw new HttpException(403);
