giftnuss/foodsharing

commit cad8747f6c2e27496aefdbea4229dd3d78229c15

&7 Build translator into Control

src/Modules/Event/EventXhr.php
@@ -5,7 +5,6 @@ namespace Foodsharing\Modules\Event;
 use Foodsharing\Lib\Xhr\XhrResponses;
 use Foodsharing\Modules\Core\Control;
 use Foodsharing\Permissions\EventPermissions;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class EventXhr extends Control
 {
@@ -13,18 +12,15 @@ class EventXhr extends Control
 	private $gateway;
 	private $responses;
 	private $eventPermissions;
-	private $translator;
 	private $responseOptions;
 
 	public function __construct(
 		EventGateway $gateway,
-		EventPermissions $eventPermissions,
-		TranslatorInterface $translator
+		EventPermissions $eventPermissions
 	) {
 		$this->gateway = $gateway;
 		$this->responses = new XhrResponses();
 		$this->eventPermissions = $eventPermissions;
-		$this->translator = $translator;
 		$this->responseOptions = [
 			InvitationStatus::ACCEPTED => 'pulseSuccess("' . $this->translator->trans('events.rsvp.yes') . '");',
 			InvitationStatus::MAYBE => 'pulseSuccess("' . $this->translator->trans('events.rsvp.maybe') . '");',

src/Modules/Blog/BlogControl.php
@@ -8,7 +8,6 @@ use Foodsharing\Permissions\BlogPermissions;
 use Foodsharing\Utility\DataHelper;
 use Foodsharing\Utility\IdentificationHelper;
 use Foodsharing\Utility\TimeHelper;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class BlogControl extends Control
 {
@@ -17,7 +16,6 @@ class BlogControl extends Control
 	private $dataHelper;
 	private $identificationHelper;
 	private $timeHelper;
-	private $translator;
 
 	public function __construct(
 		BlogView $view,
@@ -25,8 +23,7 @@ class BlogControl extends Control
 		BlogPermissions $blogPermissions,
 		DataHelper $dataHelper,
 		IdentificationHelper $identificationHelper,
-		TimeHelper $timeHelper,
-		TranslatorInterface $translator
+		TimeHelper $timeHelper
 	) {
 		$this->view = $view;
 		$this->blogGateway = $blogGateway;
@@ -34,7 +31,6 @@ class BlogControl extends Control
 		$this->dataHelper = $dataHelper;
 		$this->identificationHelper = $identificationHelper;
 		$this->timeHelper = $timeHelper;
-		$this->translator = $translator;
 
 		parent::__construct();
 		if ($id = $this->identificationHelper->getActionId('delete')) {

src/Modules/Content/ContentControl.php
@@ -8,7 +8,6 @@ use Foodsharing\Permissions\ContentPermissions;
 use Foodsharing\Utility\DataHelper;
 use Foodsharing\Utility\IdentificationHelper;
 use Parsedown;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ContentControl extends Control
 {
@@ -16,22 +15,19 @@ class ContentControl extends Control
 	private IdentificationHelper $identificationHelper;
 	private DataHelper $dataHelper;
 	private ContentPermissions $contentPermissions;
-	private TranslatorInterface $translator;
 
 	public function __construct(
 		ContentView $view,
 		ContentGateway $contentGateway,
 		IdentificationHelper $identificationHelper,
 		DataHelper $dataHelper,
-		ContentPermissions $contentPermissions,
-		TranslatorInterface $translator
+		ContentPermissions $contentPermissions
 	) {
 		$this->view = $view;
 		$this->contentGateway = $contentGateway;
 		$this->identificationHelper = $identificationHelper;
 		$this->dataHelper = $dataHelper;
 		$this->contentPermissions = $contentPermissions;
-		$this->translator = $translator;
 
 		parent::__construct();
 	}

src/Modules/Core/Control.php
@@ -12,6 +12,7 @@ use Foodsharing\Utility\PageHelper;
 use Foodsharing\Utility\RouteHelper;
 use Foodsharing\Utility\TranslationHelper;
 use ReflectionClass;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 abstract class Control
 {
@@ -21,60 +22,18 @@ abstract class Control
 	private $sub;
 	private $sub_func;
 
-	/**
-	 * @var PageHelper
-	 */
-	protected $pageHelper;
-
-	/**
-	 * @var Mem
-	 */
-	protected $mem;
-
-	/**
-	 * @var \Foodsharing\Lib\Session
-	 */
-	protected $session;
-
-	/**
-	 * @var Utils
-	 */
-	protected $v_utils;
-
-	/**
-	 * @var \Twig\Environment
-	 */
-	private $twig;
-
-	/**
-	 * @var FoodsaverGateway
-	 */
-	private $foodsaverGateway;
-
-	/**
-	 * @var InfluxMetrics
-	 */
-	private $metrics;
-
-	/**
-	 * @var EmailHelper
-	 */
-	protected $emailHelper;
-
-	/**
-	 * @var RouteHelper
-	 */
-	protected $routeHelper;
-
-	/**
-	 * @var TranslationHelper
-	 */
-	protected $translationHelper;
-
-	/**
-	 * @var FlashMessageHelper
-	 */
-	protected $flashMessageHelper;
+	protected PageHelper $pageHelper;
+	protected Mem $mem;
+	protected \Foodsharing\Lib\Session $session;
+	protected Utils $v_utils;
+	private \Twig\Environment $twig;
+	private FoodsaverGateway $foodsaverGateway;
+	private InfluxMetrics $metrics;
+	protected EmailHelper $emailHelper;
+	protected FlashMessageHelper $flashMessageHelper;
+	protected RouteHelper $routeHelper;
+	protected TranslationHelper $translationHelper;
+	protected TranslatorInterface $translator;
 
 	public function __construct()
 	{
@@ -87,8 +46,9 @@ abstract class Control
 		$this->pageHelper = $container->get(PageHelper::class);
 		$this->emailHelper = $container->get(EmailHelper::class);
 		$this->routeHelper = $container->get(RouteHelper::class);
-		$this->translationHelper = $container->get(TranslationHelper::class);
 		$this->flashMessageHelper = $container->get(FlashMessageHelper::class);
+		$this->translationHelper = $container->get(TranslationHelper::class);
+		$this->translator = $container->get('translator'); // TODO TranslatorInterface is an alias
 
 		$reflection = new ReflectionClass($this);
 		$dir = dirname($reflection->getFileName()) . DIRECTORY_SEPARATOR;

src/Modules/Dashboard/DashboardControl.php
@@ -15,7 +15,6 @@ use Foodsharing\Modules\Quiz\QuizSessionGateway;
 use Foodsharing\Modules\Store\StoreGateway;
 use Foodsharing\Utility\ImageHelper;
 use Foodsharing\Utility\Sanitizer;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DashboardControl extends Control
 {
@@ -31,7 +30,6 @@ class DashboardControl extends Control
 	private $sanitizerService;
 	private $imageService;
 	private $quizSessionGateway;
-	private $translator;
 
 	public function __construct(
 		DashboardView $view,
@@ -45,8 +43,7 @@ class DashboardControl extends Control
 		\Twig\Environment $twig,
 		Sanitizer $sanitizerService,
 		ImageHelper $imageService,
-		QuizSessionGateway $quizSessionGateway,
-		TranslatorInterface $translator
+		QuizSessionGateway $quizSessionGateway
 	) {
 		$this->view = $view;
 		$this->dashboardGateway = $dashboardGateway;
@@ -60,7 +57,6 @@ class DashboardControl extends Control
 		$this->sanitizerService = $sanitizerService;
 		$this->imageService = $imageService;
 		$this->quizSessionGateway = $quizSessionGateway;
-		$this->translator = $translator;
 
 		parent::__construct();
 

src/Modules/Event/EventControl.php
@@ -6,27 +6,23 @@ use Foodsharing\Modules\Core\Control;
 use Foodsharing\Modules\Core\DBConstants\Event\EventType;
 use Foodsharing\Permissions\EventPermissions;
 use Foodsharing\Utility\DataHelper;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class EventControl extends Control
 {
 	private EventGateway $gateway;
 	private DataHelper $dataHelper;
 	private EventPermissions $eventPermissions;
-	private TranslatorInterface $translator;
 
 	public function __construct(
 		EventView $view,
 		EventGateway $gateway,
 		DataHelper $dataHelper,
-		EventPermissions $eventPermissions,
-		TranslatorInterface $translator
+		EventPermissions $eventPermissions
 	) {
 		$this->view = $view;
 		$this->gateway = $gateway;
 		$this->dataHelper = $dataHelper;
 		$this->eventPermissions = $eventPermissions;
-		$this->translator = $translator;
 
 		parent::__construct();
 	}

src/Modules/FoodSharePoint/FoodSharePointControl.php
@@ -12,7 +12,6 @@ use Foodsharing\Permissions\FoodSharePointPermissions;
 use Foodsharing\Utility\IdentificationHelper;
 use Foodsharing\Utility\Sanitizer;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class FoodSharePointControl extends Control
 {
@@ -27,7 +26,6 @@ class FoodSharePointControl extends Control
 	private $foodsaverGateway;
 	private $mailboxGateway;
 	private $sanitizerService;
-	private $translator;
 	private $identificationHelper;
 	private $foodSharePointPermissions;
 
@@ -38,7 +36,6 @@ class FoodSharePointControl extends Control
 		FoodsaverGateway $foodsaverGateway,
 		MailboxGateway $mailboxGateway,
 		Sanitizer $sanitizerService,
-		TranslatorInterface $translator,
 		IdentificationHelper $identificationHelper,
 		FoodSharePointPermissions $foodSharePointPermissions
 	) {
@@ -48,7 +45,6 @@ class FoodSharePointControl extends Control
 		$this->foodsaverGateway = $foodsaverGateway;
 		$this->mailboxGateway = $mailboxGateway;
 		$this->sanitizerService = $sanitizerService;
-		$this->translator = $translator;
 		$this->identificationHelper = $identificationHelper;
 		$this->foodSharePointPermissions = $foodSharePointPermissions;
 

src/Modules/Foodsaver/FoodsaverControl.php
@@ -12,7 +12,6 @@ use Foodsharing\Permissions\ProfilePermissions;
 use Foodsharing\Permissions\RegionPermissions;
 use Foodsharing\Utility\DataHelper;
 use Foodsharing\Utility\IdentificationHelper;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class FoodsaverControl extends Control
 {
@@ -26,7 +25,6 @@ class FoodsaverControl extends Control
 	private $regionPermissions;
 	private $dataHelper;
 	private $identificationHelper;
-	private $translator;
 
 	public function __construct(
 		FoodsaverView $view,
@@ -39,8 +37,7 @@ class FoodsaverControl extends Control
 		ProfilePermissions $profilePermissions,
 		RegionPermissions $regionPermissions,
 		DataHelper $dataHelper,
-		IdentificationHelper $identificationHelper,
-		TranslatorInterface $translator
+		IdentificationHelper $identificationHelper
 	) {
 		$this->view = $view;
 		$this->foodsaverGateway = $foodsaverGateway;
@@ -53,7 +50,6 @@ class FoodsaverControl extends Control
 		$this->regionPermissions = $regionPermissions;
 		$this->dataHelper = $dataHelper;
 		$this->identificationHelper = $identificationHelper;
-		$this->translator = $translator;
 
 		parent::__construct();
 	}

commit 544da1e34f3f9f10511dff8c3fa9b4f8846b9429

EventXhr: Initialize `translator` before accessing it
Accidentally broken by cad8747f6c2e27496aefdbea4229dd3d78229c15

src/Modules/Event/EventXhr.php
@@ -21,13 +21,14 @@ class EventXhr extends Control
 		$this->gateway = $gateway;
 		$this->responses = new XhrResponses();
 		$this->eventPermissions = $eventPermissions;
+
+		parent::__construct();
+
 		$this->responseOptions = [
 			InvitationStatus::ACCEPTED => 'pulseSuccess("' . $this->translator->trans('events.rsvp.yes') . '");',
 			InvitationStatus::MAYBE => 'pulseSuccess("' . $this->translator->trans('events.rsvp.maybe') . '");',
 			InvitationStatus::WONT_JOIN => 'pulseInfo("' . $this->translator->trans('events.rsvp.no') . '");',
 		];
-
-		parent::__construct();
 
 		if (isset($_GET['id'])) {
 			$this->event = $this->gateway->getEvent($_GET['id'], true);