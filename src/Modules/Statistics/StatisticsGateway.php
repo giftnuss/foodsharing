e257640f3ce920ba6a884ba6e4a6493efaa6b0e8
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-09-03 21:09:19 +0200

    &7 &22 Statistics cleanup and translations

--- src/Modules/Statistics/StatisticsGateway.php
+++ src/Modules/Statistics/StatisticsGateway.php
@@ -11,23 +11,23 @@ class StatisticsGateway extends BaseGateway
 	public function listTotalStat(): array
 	{
 		$stm = '
-				SELECT
-					SUM(`stat_fetchweight`) AS fetchweight,
-					SUM(`stat_fetchcount`) AS fetchcount,
-					SUM(`stat_korpcount`) AS cooperationscount,
-					SUM(`stat_botcount`) AS botcount,
-					SUM(`stat_fscount`) AS fscount,
-					SUM(`stat_fairteilercount`) AS fairteilercount
-				FROM
-					fs_bezirk
-				WHERE
-					`id` = :region_id
+			SELECT
+				SUM(`stat_fetchweight`) AS fetchweight,
+				SUM(`stat_fetchcount`) AS fetchcount,
+				SUM(`stat_korpcount`) AS cooperationscount,
+				SUM(`stat_botcount`) AS botcount,
+				SUM(`stat_fscount`) AS fscount,
+				SUM(`stat_fairteilercount`) AS fairteilercount
+			FROM
+				fs_bezirk
+			WHERE
+				`id` = :region_id
 		';
 
 		return $this->db->fetch($stm, [':region_id' => RegionIDs::EUROPE]);
 	}
 
-	public function listStatCities(): array
+	public function listStatRegions(): array
 	{
 		$stm = '
 			SELECT
@@ -75,15 +75,15 @@ class StatisticsGateway extends BaseGateway
 	{
 		// get number of all fetches within time range
 		$q = '
-	    SELECT
-	      COUNT(*) as fetchCount
-	    FROM
-	      fs_abholer
+			SELECT
+				COUNT(*) as fetchCount
+			FROM
+				fs_abholer
 			WHERE
 				CAST(`date` as date) > DATE_ADD(CURDATE(), INTERVAL -100 DAY) AND
 				CAST(`date` as date) < CURDATE()
 				AND fs_abholer.confirmed = 1
-	  ';
+		';
 		$fetchCount = (int)$this->db->fetch($q)['fetchCount'];
 		// time range to average over in days
 		$diffDays = 99;
@@ -97,7 +97,7 @@ class StatisticsGateway extends BaseGateway
 		return $this->db->count('fs_basket', []);
 	}
 
-	public function avgWeeklyBaskets($diffWeeks = 4): int
+	public function avgWeeklyBaskets(int $diffWeeks = 4): int
 	{
 		// query
 		$q = '
--- src/Modules/Statistics/StatisticsView.php
+++ src/Modules/Statistics/StatisticsView.php
@@ -6,178 +6,190 @@ use Foodsharing\Modules\Core\View;
 
 class StatisticsView extends View
 {
-	public function getStatCities($cities)
+	public function getStatRegions(array $regions): string
 	{
-		$out = '
-		<table class="stat_cities">
-		';
+		$out = '<table class="leaderboard stat_regions">';
 
-		if (is_array($cities)) {
-			$i = 0;
-			foreach ($cities as $c) {
-				++$i;
-				$out .= '
-				<tr>
-					<td style="width:5px;text-align:right;padding-right:5px;" valign="top">
-						<h4>' . $i . '.</h4>
-					</td>
-					<td class="city">
-						<h4>' . $c['name'] . '</h4>
-						<p>' . number_format($c['fetchweight'], 0, ',', '.') . '<span style="white-space:nowrap">&thinsp;</span>kg</p>
-						<p>' . number_format($c['fetchcount'], 0, ',', '.') . '<span style="white-space:nowrap">&thinsp;</span>x abgeholt</p>
-					</td>
-				</tr>';
-			}
+		$i = 0;
+		foreach ($regions as $r) {
+			++$i;
+			$out .= '
+			<tr>
+				<td class="rank">
+					<h4>' . $i . '.</h4>
+				</td>
+				<td class="name">
+					<h4>' . $r['name'] . '</h4>
+					<p class="fetchweight">'
+					. number_format($r['fetchweight'], 0, ',', '.')
+					. '<span style="white-space: nowrap;">&thinsp;</span>kg '
+					. $this->translator->trans('profile.stats.weight')
+					. '</p>
+					<p class="fetchcount">'
+					. number_format($r['fetchcount'], 0, ',', '.')
+					. '<span style="white-space: nowrap;">&thinsp;</span>x '
+					. $this->translator->trans('profile.stats.count')
+					. '</p>
+				</td>
+			</tr>';
 		}
-		$out .= '
-		</table>';
+		$out .= '</table>';
 
-		return $this->v_utils->v_field($out, $this->translationHelper->s('active_cities'), ['class' => 'ui-padding']);
+		return $this->v_utils->v_field(
+			$out,
+			$this->translator->trans('stats.leader.regions'),
+			['class' => 'ui-padding']
+		);
 	}
 
-	public function getStatTotal($stat, int $foodsharerCount, int $avgDailyFetchCount, int $foodSharePointsCount)
+	public function getStatTotal(array $stat, int $foodsharerCount, int $avgDailyFetchCount, int $foodSharePointsCount): string
 	{
-		/*
-		 *  fetchweight,
-		fetchcount,
-		cooperationscount,
-		botcount,
-		fscount,
-		fairteilercount
-		*/
 		return $this->v_utils->v_field('
-		<div id="stat_whole">
-			<div class="stat_item">
-					<div class="stat_badge">
-						<div class="stat_icon fetchweight">
-							<i class="fas fa-apple-alt"></i>
-						</div>
-					</div>
-					<div class="stat_text">
-						<h4>' . number_format($stat['fetchweight'], 0, ',', '.') . '<span style="white-space:nowrap">&thinsp;</span>kg</h4>
-						<p>Lebensmittel erfolgreich vor der Tonne gerettet.</p>
-					</div>
-			</div>
-			<div class="stat_item">
-					<div class="stat_badge">
-						<div class="stat_icon coorpcount">
-							<i class="fas fa-store-alt"></i>
-						</div>
-					</div>
-					<div class="stat_text">
-						<h4>' . number_format($stat['cooperationscount'], 0, ',', '.') . '</h4>
-						<p>Betriebe kooperieren kontinuierlich und zufrieden mit uns.</p>
-					</div>
-			</div><br />
-			<div class="stat_item">
-					<div class="stat_badge">
-						<div class="stat_icon fscount">
-							<i class="fas fa-user-check", style="margin-left: 30px"></i>
-						</div>
-					</div>
-					<div class="stat_text">
-						<h4>' . number_format($stat['fscount'], 0, ',', '.') . '</h4>
-						<p>Foodsaver engagieren sich ehrenamtlich für eine Welt ohne Lebensmittelverschwendung.</p>
-					</div>
-			</div>
-			<div class="stat_item">
-					<div class="stat_badge">
-						<div class="stat_icon fscount2">
-							<i class="fas fa-users"></i>
-						</div>
-					</div>
-					<div class="stat_text">
-						<h4>' . number_format($foodsharerCount, 0, ',', '.') . '</h4>
-						<p>Foodsharer sind derzeit auf foodsharing registriert und interessieren sich für unsere Arbeit.</p>
-					</div>
-			</div><br />
-			<div class="stat_item">
-					<div class="stat_badge">
-						<div class="stat_icon fetchcount">
-							<i class="fas fa-walking"></i>
-						</div>
-					</div>
-					<div class="stat_text">
-						<h4>' . number_format($stat['fetchcount'], 0, ',', '.') . '</h4>
-						<p>Rettungseinsätze haben unsere Foodsaver gemeistert.</p>
-					</div>
-			</div>
-			<div class="stat_item">
-					<div class="stat_badge">
-						<div class="stat_icon dailyfetchcount">
-							<i class="fas fa-people-carry"></i>
-						</div>
-					</div>
-					<div class="stat_text">
-						<h4>' . number_format($avgDailyFetchCount, 0, ',', '.') . '</h4>
-						<p>' . $this->translationHelper->s('average_daily_fetches') . '</p>
-					</div>
-			</div><br />
-			<div class="stat_item">
-				<div class="stat_badge">
-					<div class="stat_icon totalbaskets">
-						<i class="fas fa-shopping-basket"></i>
-					</div>
+	<div id="stat_whole">
+		<div class="stat_item">
+			<div class="stat_badge">
+				<div class="stat_icon fetchweight">
+					<i class="fas fa-apple-alt"></i>
 				</div>
-					<div class="stat_text">
-						<h4>' . number_format($stat['totalBaskets'], 0, ',', '.') . '</h4>
-						<p>' . $this->translationHelper->s('total_baskets') . '</p>
-					</div>
 			</div>
-			<div class="stat_item">
-				<div class="stat_badge">
-					<div class="stat_icon avgWeeklyBaskets">
-            <span class="fa-stack">
-              <i class="far fa-calendar fa-stack-2x"></i>
-              <i class="fas fa-shopping-basket fa-stack-1x fa-stack-sm"></i>
-            </span>
-          </div>
+			<div class="stat_text">
+				<h4>' . number_format($stat['fetchweight'], 0, ',', '.') . '<span style="white-space: nowrap;">&thinsp;</span>kg</h4>
+				<p>' . $this->translator->trans('stats.total.weight') . '</p>
+			</div>
+		</div>
+		<div class="stat_item">
+			<div class="stat_badge">
+				<div class="stat_icon coorpcount">
+					<i class="fas fa-store-alt"></i>
 				</div>
-					<div class="stat_text">
-						<h4>' . number_format($stat['avgWeeklyBaskets'], 0, ',', '.') . '</h4>
-						<p>' . $this->translationHelper->s('average_weekly_baskets') . '</p>
-					</div>
-			</div><br />
-			<div class="stat_item">
-					<div class="stat_badge">
-						<div class="stat_icon fetchweight">
-							<i class="fas fa-recycle"></i>
-						</div>
-					</div>
-					<div class="stat_text">
-						<h4>' . number_format($foodSharePointsCount, 0, ',', '.') . '</h4>
-						<p>FairTeiler werden zum Tausch von Lebensmitteln genutzt.</p>
-					</div>
 			</div>
-		</div>', $this->translationHelper->s('stat_whole'));
+			<div class="stat_text">
+				<h4>' . number_format($stat['cooperationscount'], 0, ',', '.') . '</h4>
+				<p>' . $this->translator->trans('stats.total.cooperations') . '</p>
+			</div>
+		</div>
+		<br>
+		<div class="stat_item">
+			<div class="stat_badge">
+				<div class="stat_icon fscount">
+					<i class="fas fa-user-check", style="margin-left: 30px"></i>
+				</div>
+			</div>
+			<div class="stat_text">
+				<h4>' . number_format($stat['fscount'], 0, ',', '.') . '</h4>
+				<p>' . $this->translator->trans('stats.total.foodsaver') . '</p>
+			</div>
+		</div>
+		<div class="stat_item">
+			<div class="stat_badge">
+				<div class="stat_icon fscount2">
+					<i class="fas fa-users"></i>
+				</div>
+			</div>
+			<div class="stat_text">
+				<h4>' . number_format($foodsharerCount, 0, ',', '.') . '</h4>
+				<p>' . $this->translator->trans('stats.total.foodsharer') . '</p>
+			</div>
+		</div>
+		<br>
+		<div class="stat_item">
+			<div class="stat_badge">
+				<div class="stat_icon fetchcount">
+					<i class="fas fa-walking"></i>
+				</div>
+			</div>
+			<div class="stat_text">
+				<h4>' . number_format($stat['fetchcount'], 0, ',', '.') . '</h4>
+				<p>' . $this->translator->trans('stats.total.pickups') . '</p>
+			</div>
+		</div>
+		<div class="stat_item">
+			<div class="stat_badge">
+				<div class="stat_icon dailyfetchcount">
+					<i class="fas fa-people-carry"></i>
+				</div>
+			</div>
+			<div class="stat_text">
+				<h4>' . number_format($avgDailyFetchCount, 0, ',', '.') . '</h4>
+				<p>' . $this->translator->trans('stats.avg.pickups') . '</p>
+			</div>
+		</div>
+		<br>
+		<div class="stat_item">
+			<div class="stat_badge">
+				<div class="stat_icon totalbaskets">
+					<i class="fas fa-shopping-basket"></i>
+				</div>
+			</div>
+			<div class="stat_text">
+				<h4>' . number_format($stat['totalBaskets'], 0, ',', '.') . '</h4>
+				<p>' . $this->translator->trans('stats.total.baskets') . '</p>
+			</div>
+		</div>
+		<div class="stat_item">
+			<div class="stat_badge">
+				<div class="stat_icon avgWeeklyBaskets">
+					<span class="fa-stack">
+						<i class="far fa-calendar fa-stack-2x"></i>
+						<i class="fas fa-shopping-basket fa-stack-1x fa-stack-sm"></i>
+					</span>
+				</div>
+			</div>
+			<div class="stat_text">
+				<h4>' . number_format($stat['avgWeeklyBaskets'], 0, ',', '.') . '</h4>
+				<p>' . $this->translator->trans('stats.avg.baskets') . '</p>
+			</div>
+		</div>
+		<br>
+		<div class="stat_item">
+			<div class="stat_badge">
+				<div class="stat_icon fetchweight">
+					<i class="fas fa-recycle"></i>
+				</div>
+			</div>
+			<div class="stat_text">
+				<h4>' . number_format($foodSharePointsCount, 0, ',', '.') . '</h4>
+				<p>' . $this->translator->trans('stats.total.fsp') . '</p>
+			</div>
+		</div>
+	</div>',
+			$this->translator->trans('stats.title')
+		);
 	}
 
-	public function getStatFoodsaver($foodsaver)
+	public function getStatFoodsaver(array $foodsaver): string
 	{
-		$out = '
-		<table class="stat_cities">
-		';
+		$out = '<table class="leaderboard stat_foodsaver">';
 
-		if (is_array($foodsaver)) {
-			$i = 0;
-			foreach ($foodsaver as $fs) {
-				++$i;
-				$out .= '
-				<tr>
-					<td style="width:5px;text-align:right;padding-right:5px;" valign="top">
-						<h4>' . $i . '.</h4>
-					</td>
-					<td class="city">
-						<h4>' . $fs['name'] . '</h4>
-						<p>' . number_format($fs['fetchweight'], 0, ',', '.') . '<span style="white-space:nowrap">&thinsp;</span>kg</p>
-						<p>' . number_format($fs['fetchcount'], 0, ',', '.') . '<span style="white-space:nowrap">&thinsp;</span>x abgeholt</p>
-					</td>
-				</tr>';
-			}
+		$i = 0;
+		foreach ($foodsaver as $fs) {
+			++$i;
+			$out .= '
+			<tr>
+				<td class="rank">
+					<h4>' . $i . '.</h4>
+				</td>
+				<td class="name">
+					<h4>' . $fs['name'] . '</h4>
+					<p class="fetchweight">'
+					. number_format($fs['fetchweight'], 0, ',', '.')
+					. '<span style="white-space: nowrap;">&thinsp;</span>kg '
+					. $this->translator->trans('profile.stats.weight')
+					. '</p>
+					<p class="fetchcount">'
+					. number_format($fs['fetchcount'], 0, ',', '.')
+					. '<span style="white-space: nowrap;">&thinsp;</span>x '
+					. $this->translator->trans('profile.stats.count')
+					. '</p>
+				</td>
+			</tr>';
 		}
-		$out .= '
-		</table>';
+		$out .= '</table>';
 
-		return $this->v_utils->v_field($out, $this->translationHelper->s('most_active_foodsavers'), ['class' => 'ui-padding']);
+		return $this->v_utils->v_field(
+			$out,
+			$this->translator->trans('stats.leader.users'),
+			['class' => 'ui-padding']
+		);
 	}
 }
