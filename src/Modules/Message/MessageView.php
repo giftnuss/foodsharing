5ca70e1614cf3dca11a3fdaa5134dc7a3ce09f48
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-09-03 20:09:13 +0200

    &22 Misc MessageView refactoring

--- src/Modules/Message/MessageView.php
+++ src/Modules/Message/MessageView.php
@@ -9,7 +9,10 @@ final class MessageView extends View
 	public function leftMenu(): string
 	{
 		return $this->menu([
-			['name' => $this->translationHelper->s('new_message'), 'click' => 'msg.compose();return false;']
+			[
+				'name' => $this->translationHelper->s('new_message'),
+				'click' => 'msg.compose(); return false;',
+			]
 		]);
 	}
 
@@ -22,63 +25,44 @@ final class MessageView extends View
 			$.ajax({
 				url: "/api/search/legacyindex",
 				dataType: "json",
-				success: function(json){
-
-					if(json.length > 0 && json[0] != undefined && json[0].key != undefined && json[0].key == "buddies")
-					{
-
-						for(y=0;y<json[0].result.length;y++)
-						{
-							localsource.push({id:json[0].result[y].id,value:`${json[0].result[y].name} (${json[0].result[y].id})`});
+				success: function (json) {
+					if (json.length > 0 && json[0] != undefined && json[0].key == "buddies") {
+						for (y = 0; y < json[0].result.length; y++) {
+							localsource.push({
+								id: json[0].result[y].id,
+								value:`${json[0].result[y].name} (${json[0].result[y].id})`
+							});
 						}
-
 					}
 				},
-				complete: function(){
+				complete: function () {
 					$("#' . $id . ' input.tag").tagedit({
-						autocompleteOptions: {
-							delay: 300,
-							source: function(request, response) {
-					            /* Remote results only if string > 3: */
-
-								if(request.term.length > 3)
-								{
-									$.ajax({
-						                url: "/api/search/user",
-										data: {q:request.term},
-						                dataType: "json",
-						                success: function(data) {
-											response(data);
-											// following doesn\'t work somehow => ignoring
-											// local = [];
-											// term = request.term.toLowerCase();
-											// for(i=0;i<localsource.length;i++)
-											// {
-											// 	if(localsource[i].value.indexOf(term) > 0)
-											// 	{
-											// 		local.push(localsource[i]);
-											// 	}
-											// }
-											// response(merge(local,data,"id"));
-						                }
-						            });
-								}
-								else
-								{
-									response(localsource);
-								}
-
-					        },
-							minLength: 1
-						},
 						allowEdit: false,
 						allowAdd: false,
-						animSpeed:1
+						animSpeed: 1,
+						autocompleteOptions: {
+							delay: 300,
+							minLength: 1,
+							source: function (request, response) {
+					            /* Remote results only if string > 3: */
+								if (request.term.length > 3) {
+									$.ajax({
+						                url: "/api/search/user",
+										data: {q: request.term},
+						                dataType: "json",
+						                success: function (data) {
+											response(data);
+						                }
+						            });
+								} else {
+									response(localsource);
+								}
+					        }
+						}
 					});
 				}
 			});
-
-				var localsource = [];
+			var localsource = [];
 		');
 
 		$input = '<input type="text" name="' . $id . '[]" value="" class="tag input text value" />';
@@ -92,9 +76,14 @@ final class MessageView extends View
 
 		$content .= $this->v_utils->v_form_textarea('compose_body');
 
-		$content .= $this->v_utils->v_input_wrapper(false, '<a class="button" id="compose_submit" href="#">' . $this->translationHelper->s('send') . '</a>');
+		$content .= $this->v_utils->v_input_wrapper(false,
+			'<a class="button" id="compose_submit" href="#">' . $this->translationHelper->s('send') . '</a>');
 
-		return '<div id="compose">' . $this->v_utils->v_field($content, $this->translationHelper->s('new_message'), ['class' => 'ui-padding']) . '</div>';
+		return '<div id="compose">' . $this->v_utils->v_field(
+			$content,
+			$this->translationHelper->s('new_message'),
+			['class' => 'ui-padding']
+		) . '</div>';
 	}
 
 	/**
@@ -102,46 +91,54 @@ final class MessageView extends View
 	 */
 	public function conversationList(array $conversations, array $profiles): string
 	{
+		if (empty($conversations)) {
+			return '<li class="noconv">'
+				. $this->v_utils->v_info($this->translationHelper->s('no_conversations'))
+				. '</li>';
+		}
+
 		$list = '';
 
-		if (!empty($conversations)) {
-			foreach ($conversations as $c) {
-				if (!$c->lastMessage) {
-					/* only show conversations with a message */
+		foreach ($conversations as $c) {
+			if (!$c->lastMessage) {
+				// only show conversations with a message
+				continue;
+			}
+			if (empty($c->members)) {
+				continue;
+			}
+
+			$pics = '';
+			$title = '';
+
+			$pictureWidth = 50;
+			$size = 'med';
+
+			if (count($c->members) > 2) {
+				$pictureWidth = 25;
+				$size = 'mini';
+			}
+
+			foreach ($c->members as $m) {
+				if ($m == $this->session->id()) {
 					continue;
 				}
-				$pics = '';
-				$title = '';
-
-				if (!empty($c->members)) {
-					$pictureWidth = 50;
-					$size = 'med';
-
-					if (count($c->members) > 2) {
-						$pictureWidth = 25;
-						$size = 'mini';
-//						shuffle($c->members);
-					}
-
-					foreach ($c->members as $m) {
-						if ($m == $this->session->id()) {
-							continue;
-						}
-						$pics .= '<img src="' . $this->imageService->img($profiles[$m]->avatar, $size) . '" width="' . $pictureWidth . '" />';
-						$title .= ', ' . $profiles[$m]->name;
-					}
-
-					if ($c->title === null) {
-						$title = substr($title, 2);
-					} else {
-						$title = $c->title;
-					}
-
-					$list .= '<li id="convlist-' . $c->id . '" class="unread-' . (int)$c->hasUnreadMessages . '"><a href="#" onclick="msg.loadConversation(' . $c->id . ');return false;"><span class="pics">' . $pics . '</span><span class="names">' . $this->sanitizerService->plainToHtml($title) . '</span><span class="msg">' . $this->sanitizerService->plainToHtml($c->lastMessage->body) . '</span><span class="time">' . $this->timeHelper->niceDate($c->lastMessage->sentAt->getTimestamp()) . '</span><span class="clear"></span></a></li>';
-				}
+				$pics .= '<img src="' . $this->imageService->img($profiles[$m]->avatar, $size) . '" width="' . $pictureWidth . '" />';
+				$title .= ', ' . $profiles[$m]->name;
 			}
-		} else {
-			$list = '<li class="noconv">' . $this->v_utils->v_info($this->translationHelper->s('no_conversations')) . '</li>';
+
+			$title = $c->title ?? substr($title, 2);
+			$msg = $this->sanitizerService->plainToHtml($c->lastMessage->body);
+			$time = $this->timeHelper->niceDate($c->lastMessage->sentAt->getTimestamp());
+
+			$list .= '<li id="convlist-' . $c->id . '" class="unread-' . intval($c->hasUnreadMessages) . '">'
+				. '<a href="#" onclick="msg.loadConversation(' . $c->id . '); return false;">'
+				. '<span class="pics">' . $pics . '</span>'
+				. '<span class="names">' . $this->sanitizerService->plainToHtml($title) . '</span>'
+				. '<span class="msg">' . $msg . '</span>'
+				. '<span class="time">' . $time . '</span>'
+				. '<span class="clear"></span>'
+				. '</a></li>';
 		}
 
 		return $list;
@@ -149,22 +146,35 @@ final class MessageView extends View
 
 	public function conversationListWrapper(string $list): string
 	{
-		return $this->v_utils->v_field('<div id="conversation-list"><ul class="linklist conversation-list">' . $list . '</ul></div>', $this->translationHelper->s('conversations'), [], 'fas fa-comments');
+		return $this->v_utils->v_field(
+			'<div id="conversation-list"><ul class="linklist conversation-list">' . $list . '</ul></div>',
+			$this->translationHelper->s('conversations'),
+			[],
+			'fas fa-comments'
+		);
 	}
 
 	public function conversation(): string
 	{
-		$out = '
-			<div id="msg-conversation" class="corner-all"><ul></ul><div class="loader" style="display:none;"><i class="fas fa-sync fa-spin"></i></div></div>
-		';
+		$out = '<div id="msg-conversation" class="corner-all">
+			<ul></ul>
+			<div class="loader" style="display:none;">
+				<i class="fas fa-sync fa-spin"></i>
+			</div>
+		</div>';
 
-		$out .= '
-			<div id="msg-control">
-				<form>
-					' . $this->v_utils->v_form_textarea('msg_answer', ['style' => 'width: 88%;', 'nolabel' => true, 'placeholder' => $this->translationHelper->s('write_something')]) . '<input id="conv_submit" type="submit" class="button" name="submit" value="&#xf0a9;" />
-				</form>
-			</div>';
+		$out .= '<div id="msg-control">
+			<form>'
+			. $this->v_utils->v_form_textarea('msg_answer', [
+				'style' => 'width: 88%;',
+				'nolabel' => true,
+				'placeholder' => $this->translationHelper->s('write_something'),
+			]) . '<input id="conv_submit" type="submit" class="button" name="submit" value="&#xf0a9;" />
+			</form>
+		</div>';
 
-		return '<div id="msg-conversation-wrapper" style="display:none;">' . $this->v_utils->v_field($out, '', ['class' => 'ui-padding'], 'fas fa-comment', 'msg-conversation-title') . '</div>';
+		return '<div id="msg-conversation-wrapper" style="display: none;">'
+			. $this->v_utils->v_field($out, '', ['class' => 'ui-padding'], 'fas fa-comment', 'msg-conversation-title')
+			. '</div>';
 	}
 }
