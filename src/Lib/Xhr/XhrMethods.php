d6a0f44676c13707d5466665a99c0206843bda97
Kristijan Miklobusec <kristijanmiklobusec@Kristijans-MacBook-Pro.local>
2020-09-10 08:34:24 +0200

    fix

--- src/Lib/Xhr/XhrMethods.php
+++ src/Lib/Xhr/XhrMethods.php
@@ -14,6 +14,7 @@ use Foodsharing\Modules\Core\DBConstants\Email\EmailStatus;
 use Foodsharing\Modules\Core\DBConstants\Region\Type;
 use Foodsharing\Modules\Core\DBConstants\Region\WorkgroupFunction;
 use Foodsharing\Modules\Core\DBConstants\Store\StoreLogAction;
+use Foodsharing\Modules\Core\DBConstants\Store\TeamStatus;
 use Foodsharing\Modules\Email\EmailGateway;
 use Foodsharing\Modules\Foodsaver\FoodsaverGateway;
 use Foodsharing\Modules\Mailbox\MailboxGateway;
