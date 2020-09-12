c30c098481b53795fc9e497f7998741b8cdeb8d8
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-09-13 00:32:27 +0200

    Brush up Xhr/Xhr and remove Xhr:keepAlive
    Gone since 9b5a04d120d587ab83911c0ec645c5c1fb1aa835
    
    Fixes:
    - Parameter #2 $newvalue of function ini_set expects string, int given.

--- src/Lib/Xhr/Xhr.php
+++ src/Lib/Xhr/Xhr.php
@@ -9,29 +9,20 @@ namespace Foodsharing\Lib\Xhr;
  */
 class Xhr
 {
-	/*
-	 * message array to display simple popup messages after the request
-	 */
-	private $messages;
+	// display simple popup messages after the request
+	private array $messages;
 
-	/*
-	 * additional script will be executed if the request success
-	 */
-	private $script;
+	// additional script that is executed if the request success
+	private string $script;
 
-	/*
-	 * status code has to be set default 1 then will be execure the response
-	 */
-	private $status = 1;
+	// status code 1 = will execute the response
+	private int $status;
 
-	/*
-	 * date it will be send to the client and is available under global ajax object (ajax.data)
-	 */
-	private $data;
+	// data that will be sent to the client and is available under global ajax object (ajax.data)
+	private array $data;
 
 	public function __construct()
 	{
-		// set default data
 		$this->messages = [];
 		$this->script = '';
 		$this->status = 1;
@@ -39,23 +30,28 @@ class Xhr
 	}
 
 	/**
-	 * MEthod to set the status code returned to the client.
+	 * Method to set the status code returned to the client.
 	 *
 	 * @param int $code
+	 *
+	 * @deprecated Only SettingsXhr is still using this, do not add new usage!
 	 */
-	public function setStatus($code)
+	public function setStatus($code): void
 	{
 		$this->status = (int)$code;
 	}
 
 	/**
-	 * Add data accecable by client side js code
+	 * Add data accessible by client side js code.
+	 *
 	 * example:
-	 *    $xhr->addData('tree_id',1);
-	 *    is accesable in global Javascript with:
+	 *    $xhr->addData('tree_id', 1);
+	 *    is accessible in global Javascript with:
 	 *    alert(ajax.data.tree_id); => output is "1".
+	 *
+	 * @deprecated Only ActivityXhr BasketXhr and BellXhr are still using this, do not add new usage!
 	 */
-	public function addData(string $key, $value)
+	public function addData(string $key, $value): void
 	{
 		$this->data[$key] = $value;
 	}
@@ -63,10 +59,9 @@ class Xhr
 	/**
 	 * Method to add an simple pop up message to the cloent, there are 3 types of messages info, error and success can be added as 2nd parameter, default is info.
 	 *
-	 * @param string $msg
-	 * @param string $type
+	 * @deprecated Only StoreXhr and TeamXhr are still using this, do not add new usage!
 	 */
-	public function addMessage($msg, $type = 'info')
+	public function addMessage(string $msg, string $type = 'info'): void
 	{
 		$this->messages[] = [
 			'type' => $type,
@@ -75,17 +70,17 @@ class Xhr
 	}
 
 	/**
-	 * Method to add javascript code programmaticly this will be executed on client side.
+	 * Method to programmatically add javascript code that will be executed on client side.
 	 *
-	 * @param string $js
+	 * @deprecated Only StoreXhr and TeamXhr are still using this, do not add new usage!
 	 */
-	public function addScript($js)
+	public function addScript(string $js): void
 	{
 		$this->script .= "\n" . $js;
 	}
 
 	/**
-	 * Method to send all the stuff right formattet to the client.
+	 * Method to send everything to the client in the expected format.
 	 */
 	public function send()
 	{
@@ -102,16 +97,5 @@ class Xhr
 
 		echo json_encode($out, JSON_PARTIAL_OUTPUT_ON_ERROR);
 		exit();
-	}
-
-	/**
-	 * Set the time limit for this ajax call.
-	 *
-	 * @param int $seconds
-	 */
-	public function keepAlive($seconds)
-	{
-		ini_set('max_execution_time', $seconds);
-		set_time_limit($seconds);
 	}
 }
