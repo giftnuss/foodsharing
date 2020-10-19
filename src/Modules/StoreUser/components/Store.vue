27f84e5b4e2b2e6186513bff6b4cbb7addbaa18d
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-10-19 22:44:49 +0200

    StoreWall: allow toggling post display
    particularly helpful for mobile users

--- src/Modules/StoreUser/components/Store.vue
+++ src/Modules/StoreUser/components/Store.vue
@@ -2,12 +2,21 @@
 <template>
   <div class="store-view bootstrap">
     <div class="card rounded mb-2">
-      <div class="card-header text-white bg-primary">
+      <div class="card-header text-white bg-primary d-flex justify-content-between">
         {{ $i18n('wall.name') }}
+
+        <a
+          class="px-1 text-light"
+          href="#"
+          @click.prevent="toggleWallDisplay"
+        >
+          <i :class="['fas fa-fw', `fa-chevron-${displayWall ? 'down' : 'left'}`]" />
+        </a>
       </div>
 
       <div class="card-body p-0">
         <StoreWall
+          v-show="displayWall"
           :store-id="storeId"
           :managers="storeManagers"
           :may-write-post="mayWritePost"
@@ -27,8 +36,18 @@ export default {
     storeId: { type: Number, required: true },
     storeManagers: { type: Array, default: () => [] },
     mayWritePost: { type: Boolean, required: true },
-    mayDeleteEverything: { type: Boolean, required: true }
-  }
+    mayDeleteEverything: { type: Boolean, required: true },
+  },
+  data () {
+    return {
+      displayWall: true,
+    }
+  },
+  methods: {
+    toggleWallDisplay () {
+      this.displayWall = !this.displayWall
+    },
+  },
 }
 </script>
 
