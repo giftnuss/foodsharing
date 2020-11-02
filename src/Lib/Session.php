4cfe02a2c84c6d12ae3be8085e154cf4cc10cbb9
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-11-02 23:45:59 +0100

    Simplify Session:listRegionIDs

--- src/Lib/Session.php
+++ src/Lib/Session.php
@@ -264,11 +264,10 @@ class Session
 
 	public function listRegionIDs(): array
 	{
+		$regions = $this->getRegions();
 		$out = [];
-		if (isset($_SESSION['client']['bezirke']) && is_array($_SESSION['client']['bezirke'])) {
-			foreach ($_SESSION['client']['bezirke'] as $region) {
-				$out[] = $region['id'];
-			}
+		foreach ($regions as $region) {
+			$out[] = $region['id'];
 		}
 
 		return $out;
