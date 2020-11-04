dd21b5fac0472e7ada634e4b7d28fe61e4d693ad
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-11-04 20:48:09 +0100

    Namespacing imports in ReportGatewayTest

--- tests/unit/ReportGatewayTest.php
+++ tests/unit/ReportGatewayTest.php
@@ -1,23 +1,18 @@
 <?php
 
+use Foodsharing\Modules\Report\ReportGateway;
+
 class ReportGatewayTest extends \Codeception\Test\Unit
 {
-	/**
-	 * @var \UnitTester
-	 */
-	protected $tester;
-
-	/**
-	 * @var \Foodsharing\Modules\Report\ReportGateway
-	 */
-	protected $gateway;
+	protected UnitTester $tester;
+	protected ReportGateway $gateway;
 	protected $region;
 	protected $childRegion;
 	protected $childChildRegion;
 
 	protected function _before()
 	{
-		$this->gateway = $this->tester->get(\Foodsharing\Modules\Report\ReportGateway::class);
+		$this->gateway = $this->tester->get(ReportGateway::class);
 		$this->region = $this->tester->createRegion('Computer');
 		$this->childRegion = $this->tester->createRegion('Motherboard', ['parent_id' => $this->region['id']]);
 		$this->childChildRegion = $this->tester->createRegion('CPU', ['parent_id' => $this->childRegion['id']]);
