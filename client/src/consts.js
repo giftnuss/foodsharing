40b9034c0b5555dddfd7ab9e5d3d6822057ac5ab
TheSoulT <flava-sava@hotmail.de>
2021-01-10 22:46:52 +0100

    add status constants in consts.js

--- client/src/consts.js
+++ client/src/consts.js
@@ -6,3 +6,7 @@ export const MAP_TILES_URL = 'https://maps.geoapify.com/v1/styles/klokantech-bas
 export const MAP_RASTER_TILES_URL = 'https://maps.geoapify.com/v1/tile/klokantech-basic/{z}/{x}/{y}.png?apiKey=' + API_KEY
 export const MAP_ATTRIBUTION = 'Powered by <a href="https://www.geoapify.com/">Geoapify</a> | <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>'
 export const MAP_GEOCODING_ATTRIBUTION = MAP_ATTRIBUTION + ' | Geocoding by <a href="https://photon.komoot.io">Komoot Photon</a>'
+
+export const NOT_ACTIVATED = 0
+export const OPEN = 1
+export const CLOSED = 2
