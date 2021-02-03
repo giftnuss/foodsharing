84d482eca718dfa8c5263497236eec060fa1557d
Alexander Simm <alexandersimm2@googlemail.com>
2021-02-03 11:03:16 +0100

    update nightly store team calculation

--- src/Modules/Stats/StatsGateway.php
+++ src/Modules/Stats/StatsGateway.php
@@ -31,19 +31,27 @@ class StatsGateway extends BaseGateway
 	public function updateStoreUsersData(int $storeId): void
 	{
 		$this->db->fetch('UPDATE fs_betrieb_team,
-    			(SELECT foodsaver_id,
-    			        COUNT(*) as fetchcount,
-    			        DATE_FORMAT(max(date),"%Y-%m-%d") as last_fetch,
-    			        DATE_FORMAT(min(date),"%Y-%m-%d") as first_fetch
-    			FROM fs_abholer
-    			WHERE betrieb_id = 1
-    			AND date < NOW()
-    			AND confirmed = 1
-    			GROUP BY foodsaver_id
-    		) AS storestats
-    		SET stat_fetchcount = storestats.fetchcount,
-    		    stat_first_fetch = storestats.first_fetch,
-    		    stat_last_fetch = storestats.last_fetch
+				(SELECT bt.foodsaver_id,
+				        IFNULL(innerstat.fetchcount, 0) as fetchcount,
+				        IFNULL(innerstat.last_fetch, null) as last_fetch,
+				        IFNULL(innerstat.first_fetch, null) as first_fetch
+				FROM fs_betrieb_team bt
+				LEFT OUTER JOIN (
+				    SELECT a.foodsaver_id,
+				           COUNT(*) as fetchcount,
+				           DATE_FORMAT(max(a.date),"%Y-%m-%d") as last_fetch,
+				           DATE_FORMAT(min(a.date),"%Y-%m-%d") as first_fetch
+				    FROM fs_abholer a
+				    WHERE a.betrieb_id = :storeId
+				    AND a.date < NOW()
+				    AND a.confirmed = 1
+				    GROUP BY a.foodsaver_id) innerstat
+				ON bt.foodsaver_id = innerstat.foodsaver_id
+				WHERE betrieb_id = :storeId
+			) AS storestats
+			SET stat_fetchcount = storestats.fetchcount,
+			    stat_first_fetch = storestats.first_fetch,
+			    stat_last_fetch = storestats.last_fetch
 			WHERE storestats.foodsaver_id = fs_betrieb_team.foodsaver_id
 			AND fs_betrieb_team.betrieb_id = :storeId', [
 			':storeId' => $storeId,
