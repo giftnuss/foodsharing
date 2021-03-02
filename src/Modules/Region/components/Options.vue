ff28bea299a55b72c06906113fcfb4ab57e4c34b
Alexander Simm <alexandersimm2@googlemail.com>
2021-03-02 09:25:13 +0100

    update layout and show loader when saving region options

--- src/Modules/Region/components/Options.vue
+++ src/Modules/Region/components/Options.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="bootstrap card">
-    <div class="card-header text-white bg-primary">
-      <div class="row m-1">
-        <h4 class="text-truncate">
-          {{ $i18n('regionOptions.header_page', {bezirk: regionName}) }}
+  <div class="bootstrap">
+    <div class="card rounded">
+      <div class="card-header text-white bg-primary">
+        <h4 class="m-1 text-truncate">
+          {{ $i18n('regionOptions.header_page', { bezirk: regionName }) }}
         </h4>
       </div>
-    </div>
-    <div class="card m-2">
-      <div>
+      <div class="rounded p-3">
         <b-form-checkbox
           id="enableReportButton"
           v-model="isReportButtonEnabled"
@@ -18,11 +16,12 @@
         <b-form-checkbox
           id="enableMediationButton"
           v-model="isMediationButtonEnabled"
+          class="mt-1"
         >
           {{ $i18n('regionOptions.enableMediationButton') }}
         </b-form-checkbox>
         <b-button
-          class="text-right"
+          class="text-right mt-2"
           variant="secondary"
           size="sm"
           @click="trySendOptions"
@@ -35,7 +34,7 @@
 </template>
 <script>
 import { setRegionOptions } from '@/api/regions'
-import { pulseError, pulseInfo } from '@/script'
+import { hideLoader, pulseError, pulseInfo, showLoader } from '@/script'
 import i18n from '@/i18n'
 
 export default {
@@ -50,6 +49,7 @@ export default {
   },
   methods: {
     async trySendOptions () {
+      showLoader()
       try {
         await setRegionOptions(this.regionId, this.isReportButtonEnabled, this.isMediationButtonEnabled)
         pulseInfo(i18n('regionOptions.success'))
@@ -57,6 +57,7 @@ export default {
         console.error(err)
         pulseError(i18n('error_unexpected'))
       }
+      hideLoader()
     },
   },
 }
