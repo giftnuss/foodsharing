45b7746255b77289c77c2d62d5dd45e98276db24
Kristijan Miklobusec <kristijanmiklobusec@Kristijans-MBP.fritz.box>
2021-03-15 09:33:26 +0100

    fix select

--- src/Modules/Stats/StatsGateway.php
+++ src/Modules/Stats/StatsGateway.php
@@ -42,19 +42,19 @@ class StatsGateway extends BaseGateway
 				           DATE_FORMAT(max(a.date),"%Y-%m-%d") as last_fetch,
 				           DATE_FORMAT(min(a.date),"%Y-%m-%d") as first_fetch
 				    FROM fs_abholer a
-				    WHERE a.betrieb_id = :storeId
+				    WHERE a.betrieb_id = :storeIdA
 				    AND a.date < NOW()
 				    AND a.confirmed = 1
 				    GROUP BY a.foodsaver_id) innerstat
 				ON bt.foodsaver_id = innerstat.foodsaver_id
-				WHERE betrieb_id = :storeId
+				WHERE betrieb_id = :storeIdB
 			) AS storestats
 			SET stat_fetchcount = storestats.fetchcount,
 			    stat_first_fetch = storestats.first_fetch,
 			    stat_last_fetch = storestats.last_fetch
 			WHERE storestats.foodsaver_id = fs_betrieb_team.foodsaver_id
-			AND fs_betrieb_team.betrieb_id = :storeId', [
-			':storeId' => $storeId,
+			AND fs_betrieb_team.betrieb_id = :storeIdC', [
+			':storeIdA' => $storeId, ':storeIdB' => $storeId, ':storeIdC' => $storeId
 		]);
 	}
 }
