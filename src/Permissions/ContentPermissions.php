6499d3c67d8cc7f13c70abeeb2e287632a332659
Alexander Simm <alexandersimm2@googlemail.com>
2021-04-26 17:08:14 +0200

    update list of content IDs in permissions

--- src/Permissions/ContentPermissions.php
+++ src/Permissions/ContentPermissions.php
@@ -11,55 +11,49 @@ final class ContentPermissions
 	private Session $session;
 
 	private array $PR_PARTNER_AND_TEAM_CONTENT_IDS = [
-		'id' => [
-			ContentId::PARTNER_PAGE_10,
-			ContentId::PARTNER_PAGE_AU_79,
-			ContentId::TEAM_HEADER_PAGE_39,
-			ContentId::TEAM_ACTIVE_PAGE_53,
-			ContentId::TEAM_FORMER_ACTIVE_PAGE_54,
-		]
+		ContentId::PARTNER_PAGE_10,
+		ContentId::PARTNER_PAGE_AU_79,
+		ContentId::TEAM_HEADER_PAGE_39,
+		ContentId::TEAM_ACTIVE_PAGE_53,
+		ContentId::TEAM_FORMER_ACTIVE_PAGE_54,
 	];
 
 	private array $QUIZ_CONTENT_IDS = [
-		'id' => [
-			ContentId::QUIZ_DESCRIPTION_PAGE_12,
-			ContentId::QUIZ_FAILED_PAGE_13,
-			ContentId::QUIZ_CONFIRM_FS_PAGE_14,
-			ContentId::QUIZ_CONFIRM_SM_PAGE_15,
-			ContentId::QUIZ_CONFIRM_AMB_PAGE_16,
-			ContentId::QUIZ_START_PAGE_17,
-			ContentId::QUIZ_POPUP_PAGE_18,
-			ContentId::QUIZ_FAILED_FS_TRY_1_PAGE_19,
-			ContentId::QUIZ_FAILED_FS_TRY_2_PAGE_20,
-			ContentId::QUIZ_FAILED_FS_TRY_3_PAGE_21,
-			ContentId::QUIZ_FAILED_SM_TRY_1_PAGE_22,
-			ContentId::QUIZ_FAILED_SM_TRY_2_PAGE_23,
-			ContentId::QUIZ_FAILED_SM_TRY_3_PAGE_24,
-			ContentId::QUIZ_FAILED_AMB_TRY_1_PAGE_25,
-			ContentId::QUIZ_FAILED_AMB_TRY_2_PAGE_26,
-			ContentId::QUIZ_FAILED_AMB_TRY_3_PAGE_27,
-			ContentId::QUIZ_REMARK_PAGE_33,
-			ContentId::QUIZ_POPUP_SM_PAGE_34,
-			ContentId::QUIZ_POPUP_AMB_PAGE_35,
-			ContentId::QUIZ_POPUP_AMB_LAST_PAGE_36,
-		]
+		ContentId::QUIZ_DESCRIPTION_PAGE_12,
+		ContentId::QUIZ_FAILED_PAGE_13,
+		ContentId::QUIZ_CONFIRM_FS_PAGE_14,
+		ContentId::QUIZ_CONFIRM_SM_PAGE_15,
+		ContentId::QUIZ_CONFIRM_AMB_PAGE_16,
+		ContentId::QUIZ_START_PAGE_17,
+		ContentId::QUIZ_POPUP_PAGE_18,
+		ContentId::QUIZ_FAILED_FS_TRY_1_PAGE_19,
+		ContentId::QUIZ_FAILED_FS_TRY_2_PAGE_20,
+		ContentId::QUIZ_FAILED_FS_TRY_3_PAGE_21,
+		ContentId::QUIZ_FAILED_SM_TRY_1_PAGE_22,
+		ContentId::QUIZ_FAILED_SM_TRY_2_PAGE_23,
+		ContentId::QUIZ_FAILED_SM_TRY_3_PAGE_24,
+		ContentId::QUIZ_FAILED_AMB_TRY_1_PAGE_25,
+		ContentId::QUIZ_FAILED_AMB_TRY_2_PAGE_26,
+		ContentId::QUIZ_FAILED_AMB_TRY_3_PAGE_27,
+		ContentId::QUIZ_REMARK_PAGE_33,
+		ContentId::QUIZ_POPUP_SM_PAGE_34,
+		ContentId::QUIZ_POPUP_AMB_PAGE_35,
+		ContentId::QUIZ_POPUP_AMB_LAST_PAGE_36,
 	];
 
 	private array $START_CONTENT_IDS = [
-		'id' => [
-			ContentId::STARTPAGE_BLOCK1_DE,
-			ContentId::STARTPAGE_BLOCK2_DE,
-			ContentId::STARTPAGE_BLOCK3_DE,
-			ContentId::STARTPAGE_BLOCK1_BETA,
-			ContentId::STARTPAGE_BLOCK2_BETA,
-			ContentId::STARTPAGE_BLOCK3_BETA,
-			ContentId::STARTPAGE_BLOCK1_AT,
-			ContentId::STARTPAGE_BLOCK2_AT,
-			ContentId::STARTPAGE_BLOCK3_AT,
-			ContentId::STARTPAGE_BLOCK1_CH,
-			ContentId::STARTPAGE_BLOCK2_CH,
-			ContentId::STARTPAGE_BLOCK3_CH,
-		]
+		ContentId::STARTPAGE_BLOCK1_DE,
+		ContentId::STARTPAGE_BLOCK2_DE,
+		ContentId::STARTPAGE_BLOCK3_DE,
+		ContentId::STARTPAGE_BLOCK1_BETA,
+		ContentId::STARTPAGE_BLOCK2_BETA,
+		ContentId::STARTPAGE_BLOCK3_BETA,
+		ContentId::STARTPAGE_BLOCK1_AT,
+		ContentId::STARTPAGE_BLOCK2_AT,
+		ContentId::STARTPAGE_BLOCK3_AT,
+		ContentId::STARTPAGE_BLOCK1_CH,
+		ContentId::STARTPAGE_BLOCK2_CH,
+		ContentId::STARTPAGE_BLOCK3_CH,
 	];
 
 	public function __construct(Session $session)
@@ -81,17 +75,18 @@ final class ContentPermissions
 			return [];
 		}
 
+		$ids = [];
 		if ($this->session->isAdminFor(RegionIDs::QUIZ_AND_REGISTRATION_WORK_GROUP)) {
-			return $this->QUIZ_CONTENT_IDS;
+			$ids = $this->QUIZ_CONTENT_IDS;
 		}
 		if ($this->session->isAdminFor(RegionIDs::PR_PARTNER_AND_TEAM_WORK_GROUP)) {
-			return $this->PR_PARTNER_AND_TEAM_CONTENT_IDS;
+			$ids = array_merge($ids, $this->PR_PARTNER_AND_TEAM_CONTENT_IDS);
 		}
 		if ($this->session->isAdminFor(RegionIDs::PR_START_PAGE)) {
-			return $this->START_CONTENT_IDS;
+			$ids = array_merge($ids, $this->START_CONTENT_IDS);
 		}
 
-		return ['id' => []];
+		return ['id' => $ids];
 	}
 
 	public function mayEditContentId(int $id): bool
