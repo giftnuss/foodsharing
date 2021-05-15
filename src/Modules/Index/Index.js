46a4430df7e44aa3a357f4f3cd00bf6cf24dd23a
Christian W <chris.walg@posteo.de>
2021-05-15 08:29:20 +0200

    startpage: Removed unused confirm dialog for external video

--- src/Modules/Index/Index.js
+++ src/Modules/Index/Index.js
@@ -1,6 +1,5 @@
 import '@/core'
 import '@/globals'
-import i18n from '@/i18n'
 import { vueApply, vueRegister } from '@/vue'
 import Index from './components/Index'
 
@@ -8,10 +7,3 @@ vueRegister({
   Index,
 })
 vueApply('#index')
-
-const video = document.querySelector('.vidlink')
-video.addEventListener('click', (event) => {
-  if (!confirm(i18n('index.confirm_open_video'))) {
-    event.preventDefault()
-  }
-})
